a1a224175101b1a8da6f58e52db9d2f52884d54b
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-19 19:58:05 -0300

    Update guess.js

--- comandos/Prefix /div/guess.js
+++ comandos/Prefix /div/guess.js
@@ -1,8 +1,7 @@
-module.exports = {
+module.exports = ({
 name: "adivinharnúmero"
 aliases: ['guessnumber', 'guessthenumber'],
-code: `
-$botTyping
+code: ` $botTyping
 $reply
 $argsCheck[1;<@$authorID> | Você é que tem que adivinhar qual número estou pensando, não eu adivinhar o seu! Utilize **pw!$randomText[adivinharnúmero;guessnumber;guessthenumber] <número de 1 a 10>
 $onlyIf[$message[1]==$random[1;10];<@$authorID> | Ops, você errou! Tente novamente :)]
@@ -10,4 +9,4 @@ $onlyIf[$message[1]==$random[1;10];<@$authorID> | Ops, você errou! Tente novame
 $setUserVar[cataventos;$sum[$getUserVar[cataventos];1]
 $cooldown[5m;<@authorID> | Espere *%time%** para executar este comando de novo, enquanto isso, pense qual número irei pensar na próxima!]
 `
-}
+})
