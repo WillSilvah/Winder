b1b796382401a739ab0e2bb2834cb888d0783f99
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-20 15:34:55 -0300

    Update novorank.js

--- comandos/Prefix /uti/novorank.js
+++ comandos/Prefix /uti/novorank.js
@@ -1,14 +1,14 @@
 module.exports = [{
 name: "novorank",
-code:`$awaitMessages[$channelId;$authorID;10s;adoros,ativos,catavento,premiados;adoros,ativos,catavento,premiados;deu erro]
+code:`$awaitMessages[$channelId;$authorID;10s;adoros,ativos,catavento,premiados;adoros,ativos,catavento,premiados;<@$authorID> | Eu não gostei dessa sua resposta, portanto irei dormir, boa noite.]
 $botTyping
-<@$authorID> | Qual ranking vocรช quer ver?
+<@$authorID> | Qual ranking você quer ver?
 
-> **โค๏ธ adoros = Ranking dos membros mais adorados do servidor**
-> **๐’ฌ ativos = Veja os membros mais ativos!**
-> **๐ผ catavento = Veja os membros com a maior quantidade de cataventos.**
-> **โญ premiados = Veja os membros mais ativos de cada mรชs!**
-> **๐บ Miau = Miau**
+> **❤️ adoros = Ranking dos membros mais adorados do servidor**
+> **💬 ativos = Veja os membros mais ativos!**
+> **🌼 cataventos = Veja os membros com a maior quantidade de cataventos.**
+> **⭐ premiados = Veja os membros mais ativos de cada mês!**
+> **😺 Miau = Miau**
 
 $cooldown[30s;<@$authorID> | Aguarde **%time%** para poder executar esse comando de novo!]
 `
@@ -21,16 +21,16 @@ $botTyping
 $author[1;Membros mais adorados da pinwheel]
 $color[1;RANDOM]
 $description[1;:heart: - $splitText[1]
-2ยบ $splitText[2]
-3ยบ $splitText[3]
-4ยบ $splitText[4]
-5ยบ $splitText[5]
-6ยบ $splitText[6]
-7ยบ $splitText[7]
-8ยบ $splitText[8]
-9ยบ $splitText[9]
-10ยบ $splitText[10]
-$textSplit[$userLeaderboard[$guildID;rep;asc;{tag} -แตแต {value}];\n]
+2º $splitText[2]
+3º $splitText[3]
+4º $splitText[4]
+5º $splitText[5]
+6º $splitText[6]
+7º $splitText[7]
+8º $splitText[8]
+9º $splitText[9]
+10º $splitText[10]
+$textSplit[$userLeaderboard[$guildID;rep;asc;{tag} - {value}];\n]
 $thumbnail[1;$getVar[thumbrank]]
 `
 },{
@@ -43,15 +43,15 @@ $color[1;RANDOM]
 $description[1;**TOP 1** - $splitText[1]
 **TOP 2** - $splitText[2]
 **TOP 3** - $splitText[3]
-4ยบ $splitText[4]
-5ยบ $splitText[5]
-6ยบ $splitText[6]
-7ยบ $splitText[7]
-8ยบ $splitText[8]
-9ยบ $splitText[9]
-10ยบ $splitText[10]
-$textSplit[$userLeaderboard[$guildID;mensagens;asc;{tag} -แตแต {value}];\n]
-$footer[1;Soma de mensagens enviadas: $getServerVar[allmensagens] | Vocรช: $getUserVar[mensagens]]
+4º $splitText[4]
+5º $splitText[5]
+6º $splitText[6]
+7º $splitText[7]
+8º $splitText[8]
+9º $splitText[9]
+10º $splitText[10]
+$textSplit[$userLeaderboard[$guildID;mensagens;asc;{tag} - {value}];\n]
+$footer[1;Soma de mensagens enviadas: $getServerVar[allmensagens] | Você: $getUserVar[mensagens]]
 $thumbnail[1;$getVar[thumbrank]]
  $botTyping
 `
@@ -65,22 +65,22 @@ $color[1;RANDOM]
 $description[1;**TOP 1** - $splitText[1]
 **TOP 2** - $splitText[2]
 **TOP 3** - $splitText[3]
-4ยบ $splitText[4]
-5ยบ $splitText[5]
-6ยบ $splitText[6]
-7ยบ $splitText[7]
-8ยบ $splitText[8]
-9ยบ $splitText[9]
-10ยบ $splitText[10]
-$textSplit[$userLeaderboard[$guildID;cataventos;asc;{tag} -แตแต {value}];\n]
+4º $splitText[4]
+5º $splitText[5]
+6º $splitText[6]
+7º $splitText[7]
+8º $splitText[8]
+9º $splitText[9]
+10º $splitText[10]
+$textSplit[$userLeaderboard[$guildID;cataventos;asc;{tag} - {value}];\n]
 $thumbnail[1;$getVar[thumbrank]]
 `
 },{
 name: "premiados",
 type:"awaited",
 code: `$reply
-<@$authorID> | โญ
-$title[1;Membros que foram os mais ativos de cada mรชs]
+<@$authorID> |
+$title[1;Membros que foram os mais ativos de cada mês]
 $description[1;
 $addField[1;Janeiro de 2022;Aelusi]
 $addField[1;Fevereiro de 2022;Fulano#0000]
@@ -92,6 +92,6 @@ $footer[1;Quer aparecer aqui? Tente ser os 3 top membros ativos!]
 name: "miau",
 type:"awaited",
 code: `$reply
-<@$authorID> | Miau! ๐ธ
+<@$authorID> | Miau!
 `
 }]
