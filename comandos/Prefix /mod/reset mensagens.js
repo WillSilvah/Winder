4aa025eb84610089fba5323ff1349fdf6d36591d
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-19 21:07:17 -0300

    Update reset mensagens.js

--- comandos/Prefix /mod/reset mensagens.js
+++ comandos/Prefix /mod/reset mensagens.js
@@ -4,6 +4,7 @@ code: `
 
 $resetServerVar[allmensagens]
 $resetUserVar[mensagens]
+$resetGlobalUserVar[mensagens]
 
 $onlyForIDs[494795833351602176;882913524291088384;<@$authorID> você não tem permissão para executar este comando!]
 <@$authorID> | Todas as mensagens foram resetadas! Agora estarei contando as novas mensagens :)
