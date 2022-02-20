ed022dd74ec72d9a7a99a4329ba350dd0e0afef4
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-20 15:03:34 -0300

    Update novorank.js

--- comandos/Prefix /uti/novorank.js
+++ comandos/Prefix /uti/novorank.js
@@ -1,6 +1,6 @@
 module.exports = ({
 name: "novorank",
-code: `$awaitMessages[$authorID;10s;adoros;ativos;catavento;premiados;deu erro]
+code: `$awaitMessages[$channelID;10s;adoros;ativos;catavento;premiados;deu erro]
 $botTyping
 <@$authorID> | Qual ranking você quer ver?
 
