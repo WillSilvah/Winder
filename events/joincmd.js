0c4409a4d44f477773eb89c2ac5ecf0e0083aa22
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-21 09:18:53 -0300

    Update joincmd.js

--- events/joincmd.js
+++ events/joincmd.js
@@ -6,11 +6,12 @@ bot.joinCommand({
 $title[1;<:pinw_yellow:929149041881788526> Seja Bem Vindo $username[$authorID] ]
 $description[1;Seja bem vindo a comunidade pinwheel <@$authorID> ! Espero que goste daqui e que faça  otimos amigos!
 
-**Respeite as diretrizes!**
-Leia o canal <#861333504297926677> para saber o que pode ou não pode!
+$addField[1;Respeite as diretrizes!;Leia o canal <#861333504297926677> para saber o que pode ou não pode!]
 
-**Registre-se e se colore-se**
-Visite o canal <#861829639444889620> e pegue cargos!]
+$addField[1;Registre-se e se colore-se;Visite o canal <#861829639444889620> e pegue cargos!]
+$addField[1;Ajude a pinwheel ser cada vez melhor!;Vote na pinwheel usando vt!vote | Dê sua opinião sobre ela indo em https://dyno.gg/form/316a9c99]
+
+]
 $color[1;RANDOM]
 $footer[1;Comunidade pinwheel | ID do usuário: $authorID]
 $thumbnail[1;$authorAvatar]
