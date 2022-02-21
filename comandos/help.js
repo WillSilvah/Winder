c75b927027174ae6c1f0ba0ba885bc33aed09e0d
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-21 08:59:07 -0300

    Update help.js

--- comandos/help.js
+++ comandos/help.js
@@ -6,7 +6,7 @@ $author[1;Manual do pinwBot]
 $description[1;
 Saiba de todos os comandos disponíveis que eu tenho!]
 $addField[1;Diversão;perguntar / adorar / adivinharnúmero]
-$addField[1;Moderação;ban / unban / kick / lock / unlock / clear / sos]
+$addField[1;Moderação;ban / unban / kick / lock / unlock / clear / sos / 'user mudar apelido']
 $addField[1;Utilidades;avatar / botinfo / sobremim / perfil / serstaff / checkperms / checklist /  mensagens / rank]
 $addField[1;Pesquisa;pw!pesquisar google/bing/duckduckgo/youtube]
 $addField[1;Comandos avançados;executar / reiniciar]
