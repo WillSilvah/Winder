9eda1f5ce59064216f2a9ff87d15289375e993c6
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-21 08:53:22 -0300

    Update reiniciar (copy).js

--- comandos/Prefix /bot/reiniciar (copy).js
+++ comandos/Prefix /bot/reiniciar (copy).js
@@ -1,8 +1,6 @@
 module.exports = {
 name: "reiniciar",
 code: `
-$reboot[index.js]
-
-$onlyForIDs[494795833351602176;882913524291088384;<@$authorID> | Você não tem permissão suficientes para executar este comando.]
+<@$authorID> | Este comando está em falhas constantes.]
 `
 }
