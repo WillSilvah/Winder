ce065c4efc8a870d30b2598b9b9b867df05f459f
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-21 08:35:16 -0300

    Update guess.js

--- comandos/Prefix /div/guess.js
+++ comandos/Prefix /div/guess.js
@@ -3,10 +3,7 @@ name: "adivinharnúmero",
 aliases: ['guessnumber', 'guessthenumber'],
 code: `$botTyping
 $reply
-$argsCheck[1;<@$authorID> | Você é que tem que adivinhar qual número estou pensando, não eu adivinhar o seu! Utilize **pw!$randomText[adivinharnúmero;guessnumber;guessthenumber] <número de 1 a 10>
-$onlyIf[$message[1]==$random[1;10];<@$authorID> | Ops, você errou! Tente novamente :)]
-<@$authorID> | Você acertou! Você ganhou um catavento :)
-$setUserVar[cataventos;$sum[$getUserVar[cataventos];1]
+<@$authorID> | Atualize sua pinwheel para a versão **$getVar[versão]** para poder usar este comando!
 $cooldown[5m;<@$authorID> | Espere **%time%** para executar este comando de novo, enquanto isso, pense qual número irei pensar na próxima!]
 `
 }
