0016448ecd3acf343e8fb63b6e46da23c67ca60d
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-21 11:07:36 -0300

    Update bump.js

--- comandos/Prefix /bot/bump.js
+++ comandos/Prefix /bot/bump.js
@@ -1,8 +1,17 @@
-module.exports = {
- name: "!d bump",
-  nonPrefixed: true,
+module.exports = [{
+name: "!d bump",
+nonPrefixed: true,
  code: `$setUserVar[bump;$sum[$getUserVar[bump];1]] $addCmdReactions[<:pwh_gato_like:930060681519173663>]
  $onlyForChannels[861422411915722774;]
  $globalCooldown[2h;]
 `
-} 
+} .{
+module.exports = [{
+name: "!d bump",
+nonPrefixed: true,
+code: `$useChannel[861422411915722774]
+<@&869508084404469831> | Bump disponível!
+> **__Digite !d bump para ajudar o servidor a crescer__**
+$wait[2h]
+$globalCooldown[2h;]
+}]
