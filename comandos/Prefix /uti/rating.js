7962a7125e5599887d464e330c0c4eee945da78e
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-21 13:05:09 -0300

    Update rating.js

--- comandos/Prefix /uti/rating.js
+++ comandos/Prefix /uti/rating.js
@@ -3,7 +3,6 @@ module.exports = {
  code: `$reply
 $setUserVar[cataventos;$sum[$getUserVar[cataventos];1]]
 $onlyIf[$randomText[no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;yes]==yes;]
-$cooldown[5s;]
 <@$authorID> | Sua mensagem foi tão incrível que você ganhou um catavento! Você tem **$sum[$getUserVar[cataventos];1]** cataventos!
 `
 }
