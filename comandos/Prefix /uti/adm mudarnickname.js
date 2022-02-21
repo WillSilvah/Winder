cb3e0b13510e58ed4f094f868f4c9412fe917726
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-21 08:58:01 -0300

    Create adm mudarnickname.js

---
+++ comandos/Prefix /uti/adm mudarnickname.js
@@ -0,0 +1,11 @@
+module.exports = {
+name: "usuário mudar apelido",
+aliases: ['user change nickname'],
+code: `
+  $reply
+<@$authorID> | Apelido de <@$mentioned[1] foi alterado para **$noMentionMessage**!
+$changeNickname[$mentioned[1];$noMentionMessage]
+$onlyPerms[managenicknames;<@$authorID> | Você não tem permissão de **gerenciar apelidos**]
+$botTyping
+$cooldown[10s;<@$authorID> | Aguarde %time% para poder executar este comando de novo!]
+`
