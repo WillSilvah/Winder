b5c279a565ebdb225fc20b537ea675120219ba4c
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-21 13:42:05 -0300

    Update on_ready (copy).js

--- events/on_ready (copy).js
+++ events/on_ready (copy).js
@@ -14,7 +14,7 @@ $description[1;
 > **Membros totais: \`$membersCount[861313067291115541]\`**
 > **Hospedado na SquareCloud https://squarecloud.app/**
 ]
-$deleteIn[2m]
+$deleteIn[1m]
 $color[1;RANDOM]
 $thumbnail[1;$userAvatar[861048815594504214]]
 `
