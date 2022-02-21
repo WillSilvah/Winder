8410fb1d8ba9e04d179bbcd2e462d02fb83b5dda
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-21 14:24:44 -0300

    Update perfil.js

--- comandos/Prefix /uti/perfil.js
+++ comandos/Prefix /uti/perfil.js
@@ -9,7 +9,7 @@ $description[1;
 $getUserVar[sobremim;$mentioned[1]]
 $addField[1;Última mensagem enviada;**$getUserVar[mensagens1;$mentioned[1;yes]]** | Horário: $getUserVar[mensagens2;$mentioned[1;yes]] | último Canal: $getUserVar[mensagens3;$mentioned[1;yes]]]
 
-$addField[1;Jornada de $username[$mentioned[1;yes]] na pinwheel;**$getUserVar[mensagens;$mentioned[1;yes]]** mensagens enviadas | **$getUserVar[rep;$mentioned[1;yes]]** adoros!]
+$addField[1;Jornada de $username[$mentioned[1;yes]] na pinwheel;**$getUserVar[mensagens;$mentioned[1;yes]]** mensagens enviadas | **$getUserVar[rep;$mentioned[1;yes]]** adoros! | **$getUserVar[cataventos;$mentioned[1]]** cataventos]
 
 $addField[1;ID do usuário;$mentioned[1]]
 
