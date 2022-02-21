6859c65a40f03e90b0fff81b11b947f187c176fe
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-21 14:14:39 -0300

    Update mudarnickname.js

--- comandos/Prefix /uti/mudarnickname.js
+++ comandos/Prefix /uti/mudarnickname.js
@@ -2,10 +2,9 @@ module.exports = {
 name: "mudarnickname",
 aliases: ['nickname','apelido','mudarapelido'],
 code: `$botTyping
-  $reply
+$reply
 <@$authorID> | Seu apelido foi alterado para **$message**!
 $setUserVar[nickname;$message;$authorID]
-$changeNickname[$authorID;$message]
 $argsCheck[1;<@$authorID> | Qual apelido você quer que eu chame você? **pw!$randomText[nickname;mudarapelido;apelido] <novo apelido>**]
 $cooldown[10s;<@$authorID> | Aguarde %time% para poder executar este comando de novo!]
 `
