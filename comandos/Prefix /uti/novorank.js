fecdccff828af7d640980a87dd1adefae0722c02
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-21 08:29:57 -0300

    Update novorank.js

--- comandos/Prefix /uti/novorank.js
+++ comandos/Prefix /uti/novorank.js
@@ -1,6 +1,6 @@
 module.exports = [{
-name: "novorank",
-code:`$awaitMessages[$channelID;$authorID;30s;adoros,ativos,catavento,premiados,miau;adoros,ativos,catavento,premiados,miau;<@$authorID> | Eu não gostei dessa sua resposta, portanto irei dormir, boa noite.]
+name: "rank",
+code:`$awaitMessages[$channelID;$authorID;30s;adoros,ativos,catavento,premiados,bump;adoros,ativos,catavento,premiados,bump;<@$authorID> | Eu não gostei dessa sua resposta, portanto irei dormir, boa noite.]
 $botTyping
 <@$authorID> | Qual ranking você quer ver?
 
@@ -8,7 +8,7 @@ $botTyping
 > **💬 ativos = Veja os membros mais ativos!**
 > **🌼 cataventos = Veja os membros com a maior quantidade de cataventos.**
 > **⭐ premiados = Veja os membros mais ativos de cada mês!**
-> **😺 Miau = Miau**
+> **😺 Bump = Veja os membros que mais bumparam a pinwheel!**
 *Digite no chat, a opção que deseja ver*
 
 $cooldown[10s;<@$authorID> | Aguarde **%time%** para poder executar esse comando de novo!]
@@ -83,16 +83,30 @@ code: `$reply
 <@$authorID> |
 $title[1;Membros que foram os mais ativos de cada mês]
 $description[1;
-$addField[1;Janeiro de 2022;Aelusi]
+$addField[1;Janeiro de 2022;aelusi#0099 (400785627165097985)]
 $addField[1;Fevereiro de 2022;Fulano#0000]
 ]
 $thumbnail[1;$getVar[thumbrank]]
 $footer[1;Quer aparecer aqui? Tente ser os 3 top membros ativos!]
 `
 },{
-name: "miau",
+name: "bump",
 type:"awaited",
 code: `$reply
-<@$authorID> | Miau!
+$botTyping
+$author[1;Membros que mais bumparam a pinwheel]
+$color[1;RANDOM]
+$description[1;**TOP 1** - $splitText[1]
+**TOP 2** - $splitText[2]
+**TOP 3** - $splitText[3]
+4º $splitText[4]
+5º $splitText[5]
+6º $splitText[6]
+7º $splitText[7]
+8º $splitText[8]
+9º $splitText[9]
+10º $splitText[10]
+$textSplit[$userLeaderboard[$guildID;bump;asc;{tag} -឵឵ {value}];\n]
+$thumbnail[1;$getVar[thumbrank]]
 `
 }]
