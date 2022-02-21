b0ed97ec34cb8ea131301761ba5202d4ec5f15d0
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-21 13:54:22 -0300

    Update mudarnickname.js

--- comandos/Prefix /uti/mudarnickname.js
+++ comandos/Prefix /uti/mudarnickname.js
@@ -1,12 +1,11 @@
 module.exports = {
 name: "mudarnickname",
 aliases: ['nickname','apelido','mudarapelido'],
-code: `
+code: `$botTyping
   $reply
 <@$authorID> | Seu apelido foi alterado para **$message**!
+$setUserVar[nickname;$message;$authorID]
 $changeNickname[$authorID;$message]
-$onlyForROles[905245073019641869;942775153530634280;<@$authorID> | Você precisa ter **pinwheel Ultra/Ultra+** para poder usar este comando!
-$botTyping
 $cooldown[10s;<@$authorID> | Aguarde %time% para poder executar este comando de novo!]
 `
 }
