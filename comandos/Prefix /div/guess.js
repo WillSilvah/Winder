3d78ab4d348c5a2b083298e52d86f0a6f5bc21b1
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-21 13:57:01 -0300

    Update guess.js

--- comandos/Prefix /div/guess.js
+++ comandos/Prefix /div/guess.js
@@ -6,14 +6,14 @@ code: `$botTyping
 $if[$getServerVar[guess]==$message[1]]
 <@$authorID> | Parabéns! Você adivinhou o número que estava pensando!
 Você ganhou um catavento!
-$changeNickname[$authorID;[LeitorDeMentes] - $username]
+$changeNickname[$authorID;[LeitorDeMentes] - $getUserVar[nickname]]
 $setServerVar[guess;$random[1;20]]
 $setUserVar[cataventos;$sum[$getUserVar[cataventos];1]]
 
 $elseIf[$getServerVar[guess]!=$message[1]]
 <@$authorID> | Parabéns! Você errou :joy:
 Tente novamente daqui a pouco
-$changeNickname[$authorID;[Burro] - $username]
+$changeNickname[$authorID;[Burro] - $getUserVar[nickname]]
 
 $endelseIf
 $endif
