f03ef46c511cb687b5694f4a705f77a7b6fd0b2b
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-21 12:49:56 -0300

    Update guess.js

--- comandos/Prefix /div/guess.js
+++ comandos/Prefix /div/guess.js
@@ -1,9 +1,18 @@
 module.exports = {
 name: "adivinharnúmero",
 aliases: ['guessnumber', 'guessthenumber','adivinharnumero'],
+$if: "v4",
 code: `$botTyping
-$reply
-<@$authorID> | Atualize sua pinwheel para a versão **$getVar[versão]** para poder usar este comando!
-$cooldown[5m;<@$authorID> | Espere **%time%** para executar este comando de novo, enquanto isso, pense qual número irei pensar na próxima!]
+$if[$getServerVar[guess]==$message[1]]
+<@$authorID> | Parabéns! Você adivinhou o número que estava pensando!
+$setServerVar[guess;$random[1;20]]
+
+$elseIf[$getServerVar[guess]!=$message[1]]
+<@$authorID> | Parabéns! Você errou :joy:
+Tente novamente daqui a pouco
+
+$endelseIf
+$endif
+$cooldown[5m;<@$authorID> | Espere **%time** para adivinhar novamente!]
 `
 }
