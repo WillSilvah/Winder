42feacc5ca5f40fb6257d28b3004f93a39e8a12a
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-21 08:44:04 -0300

    Update help.js

--- comandos/help.js
+++ comandos/help.js
@@ -1,16 +1,17 @@
 module.exports = [{
 name:'help',
-  aliases: ['ajuda'],
-  code: `$reply
-    $author[1;Manual do pinwBot]
-    $description[1;
-    Saiba de todos os comandos disponíveis que eu tenho!]
+aliases: ['ajuda'],
+code: `$reply
+$author[1;Manual do pinwBot]
+$description[1;
+Saiba de todos os comandos disponíveis que eu tenho!]
 $addField[1;Diversão;perguntar / adorar / adivinharnúmero]
 $addField[1;Moderação;ban / unban / kick / lock / unlock / clear / sos]
-$addField[1;Utilidades;avatar / botinfo / sobremim / perfil / serstaff / checkperms / checklist /  mensagens]
-$addField[1;Ranking;rank mensagens / rank bump / rank adoros / rank cataventos]
+$addField[1;Utilidades;avatar / botinfo / sobremim / perfil / serstaff / checkperms / checklist /  mensagens / rank]
 $addField[1;Pesquisa;pw!pesquisar google/bing/duckduckgo/youtube]
 $addField[1;Comandos avançados;executar / reiniciar]
+$thumbnail[1;$userAvatar[861048815594504214]]
+
 
 `
 }]
