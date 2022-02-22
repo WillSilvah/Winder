6fb6a553695e76c24ce6bc95518129bbf32547af
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-22 20:01:43 -0300

    Update status.js

--- handler/status.js
+++ handler/status.js
@@ -1,36 +1,36 @@
 module.exports = (bot) => {
   
 bot.status({
-  text: "pw! | $membersCount[861313067291115541;all;no] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
-  type: "PLAYING",
+  text: "$membersCount[861313067291115541] pinwheelers!",
+  type: "WATCHING",
 })
 
 bot.status({
-  text: "pw! | $membersCount[861313067291115541;all;no] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
+  text: "$randomText[Five Nights at Pinwheel's;Five Nights at Freddy's;Five Nights at Freddy's 2;Five Nights at Freddy's 3;Five Nights at Freddy's: The Final Chapter;Five Nights at Freddy's: Sister Location;Five Nights at Freddy's: Pizzeria Simulator;Five Nights at Freddy's: Ultimate Custom Night;Five Nights at Freddy's: Security Breach]",
+  type: "PLAYING",
+  time: 30
+})
+
+bot.status({
+  text: "FNAF com o $randomText[Anderson;Ycro;Volt] <3",
   type: "PLAYING",
   time: 15
 })
 
 bot.status({
-  text: "pw! | $membersCount[861313067291115541;all;no] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
-  type: "PLAYING",
-  time: 15
-})
-
-bot.status({
-  text: "pw! | $membersCount[861313067291115541;all;no] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
+  text: "Minecraft",
   type: "PLAYING",
   time: 15
 })
   
 bot.status({
-  text: "pw! | $membersCount[861313067291115541;all;no] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
-  type: "PLAYING",
+  text: "amor para todo mundo!",
+  type: "STREAMING",
   time: 15
 })
 
 bot.status({
-  text: "pw! | $membersCount[861313067291115541;all;no] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
+  text: "ping: $pingms | DB ping: $dpPingms",
   type: "PLAYING", 
 //PLAYING
 //WATCHING
