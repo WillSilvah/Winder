0982de12e9008ca39a665fa54688e10804350153
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-22 09:25:38 -0300

    Update rating.js

--- comandos/Prefix /uti/rating.js
+++ comandos/Prefix /uti/rating.js
@@ -1,6 +1,7 @@
 module.exports = {
- name: "$alwaysExecute",
- code: `$reply
+name: "$alwaysExecute",
+$if: "v4",
+code: `$reply
 $setUserVar[cataventos;$sum[$getUserVar[cataventos];1]]
 $if[$randomText[no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;yes]==yes]
 <@$authorID> | Sua mensagem foi tão incrível que você ganhou um catavento! Você tem **$sum[$getUserVar[cataventos];1]** cataventos!
