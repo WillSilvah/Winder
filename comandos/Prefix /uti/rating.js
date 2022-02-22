09d3973f1d470199c3b380c68a325ff3a72965be
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-22 09:06:25 -0300

    Update rating.js

--- comandos/Prefix /uti/rating.js
+++ comandos/Prefix /uti/rating.js
@@ -4,6 +4,7 @@ module.exports = {
 $setUserVar[cataventos;$sum[$getUserVar[cataventos];1]]
 $onlyIf[$randomText[no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;yes]==yes;]
 <@$authorID> | Sua mensagem foi tão incrível que você ganhou um catavento! Você tem **$sum[$getUserVar[cataventos];1]** cataventos!
+> **__Veja ela e outras mensagens incríveis em <#945355607421095936>__**
 
 $channelSendMessage[945355607421095936;<@$authorID> {newEmbed:$getUserVar[nickname] enviou uma mensagem incrível!} {description:$message} {footer:$username#$discriminator enviou $getUserVar[mensagens]}  }]
 `
