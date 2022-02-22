a913adf8f51f11150e2ad54be18527920425edad
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-22 17:59:32 -0300

    Update mudarnickname.js

--- comandos/Prefix /uti/mudarnickname.js
+++ comandos/Prefix /uti/mudarnickname.js
@@ -3,6 +3,7 @@ name: "mudarnickname",
 aliases: ['nickname','apelido','mudarapelido'],
 code: `$botTyping
 $reply
+$changeNickname[$authorID;$message]
 <@$authorID> | Seu apelido foi alterado para **$message**!
 $setUserVar[nickname;$message;$authorID]
 $argsCheck[1;<@$authorID> | Qual apelido você quer que eu chame você? **pw!$randomText[nickname;mudarapelido;apelido] <novo apelido>**]
