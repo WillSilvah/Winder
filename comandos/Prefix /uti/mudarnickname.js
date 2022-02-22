18fb38449c48b9f8c27b5760da2d69eca1cfb006
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-22 18:03:48 -0300

    Update mudarnickname.js

--- comandos/Prefix /uti/mudarnickname.js
+++ comandos/Prefix /uti/mudarnickname.js
@@ -6,7 +6,7 @@ $reply
 $changeNickname[$authorID;$message]
 <@$authorID> | Seu apelido foi alterado para **$message**!
 $setUserVar[nickname;$message;$authorID]
-$argsCheck[1;<@$authorID> | Qual apelido você quer que eu chame você? **pw!$randomText[nickname;mudarapelido;apelido] <novo apelido>**]
+$argsCheck[0;<@$authorID> | Qual apelido você quer que eu chame você? **pw!$randomText[nickname;mudarapelido;apelido] <novo apelido>**]
 $cooldown[10s;<@$authorID> | Aguarde %time% para poder executar este comando de novo!]
 `
 }
