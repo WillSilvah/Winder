b85c1738ac0e596d9be120badbbc9ed5c47d45ae
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-22 18:23:50 -0300

    Update rating.js

--- comandos/Prefix /uti/rating.js
+++ comandos/Prefix /uti/rating.js
@@ -3,11 +3,7 @@ name: "$alwaysExecute",
 $if: "v4",
 code: `$reply
 $if[$randomText[no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;yes]==yes]
-<@$authorID> | Sua mensagem foi tão incrível que você ganhou um catavento! Você tem **$sum[$getUserVar[cataventos];1]** cataventos!
 $setUserVar[cataventos;$sum[$getUserVar[cataventos];1]]
-> **__Veja ela e outras mensagens incríveis em <#945355607421095936>__**
 
-$channelSendMessage[945355607421095936;<@$authorID> {newEmbed: {author:$getUserVar[nickname] enviou uma mensagem incrível!} {description:$message} {footer:$username#$discriminator enviou $getUserVar[mensagens] mensagens}  }]
-$endIf
 `
 }
