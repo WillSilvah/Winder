fee0e8988e7f165d4e8ba3342ce70840411a9972
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-22 17:17:35 -0300

    Update guess.js

--- comandos/Prefix /div/guess.js
+++ comandos/Prefix /div/guess.js
@@ -3,7 +3,7 @@ name: "adivinharnúmero",
 aliases: ['guessnumber', 'guessthenumber','adivinharnumero', 'gtn'],
 $if: "v4",
 code: `$botTyping
-$argsCheck[1;<@$authorID> | É pra você adivinhar o número que eu estou pensando, não eu adivinhar o que você está pensando. **$getUserVar[prefixo]$randomText[adivinharnúmero;guessnumber;guessthenumber;adivinharnumero;gtn]**]
+$argsCheck[>1;<@$authorID> | É pra você adivinhar o número que eu estou pensando, não eu adivinhar o que você está pensando. **$getUserVar[prefixo]$randomText[adivinharnúmero;guessnumber;guessthenumber;adivinharnumero;gtn]**]
 $if[$getServerVar[guess]==$message[1]]
 $reply $botTyping
 <@$authorID> | Parabéns! Você adivinhou o número que estava pensando!
