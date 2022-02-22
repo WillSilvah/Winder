b567a8621f512234c4ea59079ab4d4cb75657d55
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-22 08:18:27 -0300

    Update leavecmd.js

--- events/leavecmd.js
+++ events/leavecmd.js
@@ -1,10 +1,10 @@
 module.exports = (bot) => {
-bot.leaveCommand({ 
-        channel: "861337787192836117", 
-  code: `
-$title[1;<:pinw_yellow:929149041881788526> **A gente se vê por ai! $username[$authorID]!**]
+bot.leaveCommand({
+channel: "861337787192836117", 
+code: `
+$author[1;A gente se vê por ai! $username[$authorID]!;https://cdn.discordapp.com/emojis/945622734484942849.png?size=2048]
 $description[1;Infelizmente o **$username[$authorID]** saiu da pinwheel :(]
-$footer[1;Comunidade pinwheel | Id do usuário: $authorID ]
+$footer[1;Comunidade pinwheel | Id do usuário: $authorID;$serverIcon]
 $color[1;RANDOM]
 $thumbnail[1;$authorAvatar]
 
