a20c66f0d1156ab151b147ed7cb63a704eaf122b
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-22 08:26:27 -0300

    Update status.js

--- handler/status.js
+++ handler/status.js
@@ -1,36 +1,36 @@
 module.exports = (bot) => {
   
 bot.status({
-  text: "pw! | $membersCount[861313067291115541] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
+  text: "pw! | $membersCount[861313067291115541;all;no] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
   type: "PLAYING",
 })
 
 bot.status({
-  text: "pw! | $membersCount[861313067291115541] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
-  type: "PLAYING",
-  time: 15
-})
-
-bot.status({
-  text: "pw! | $membersCount[861313067291115541] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
+  text: "pw! | $membersCount[861313067291115541;all;no] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
   type: "PLAYING",
   time: 15
 })
 
 bot.status({
-  text: "pw! | $membersCount[861313067291115541] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
+  text: "pw! | $membersCount[861313067291115541;all;no] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
+  type: "PLAYING",
+  time: 15
+})
+
+bot.status({
+  text: "pw! | $membersCount[861313067291115541;all;no] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
   type: "PLAYING",
   time: 15
 })
   
 bot.status({
-  text: "pw! | $membersCount[861313067291115541] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
+  text: "pw! | $membersCount[861313067291115541;all;no] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
   type: "PLAYING",
   time: 15
 })
 
 bot.status({
-  text: "pw! | $membersCount[861313067291115541] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
+  text: "pw! | $membersCount[861313067291115541;all;no] membros | Versão: $getVar[versão] | ping: $pingms | $uptime",
   type: "PLAYING", 
 //PLAYING
 //WATCHING
