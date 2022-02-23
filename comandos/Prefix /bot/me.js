bc3704d82d0bfa464d63c4e79b59e0f2e93b6690
Williams <49321188+WillSilvah@users.noreply.github.com>
2022-02-23 19:29:32 -0300

    Update me.js

--- comandos/Prefix /bot/me.js
+++ comandos/Prefix /bot/me.js
@@ -1,5 +1,6 @@
 module.exports = {
  name: "$alwaysExecute",
+$if: "v4",
  code: `
  $setUserVar[mensagens;$sum[$getUserVar[mensagens];1]]
  $setUserVar[totalmensagens;$sum[$getUserVar[mensagens];1]]
@@ -9,5 +10,38 @@ module.exports = {
  $setUserVar[mensagens3;<#$channelID>]
  $setServerVar[allmensagens;$sum[$getServerVar[allmensagens];1]]
 $timezone[America/Recife]
+
+$if[$gerUserVar[mensagens]==1000]
+**$username** | enviou 1,000 mensagens!
+
+$elseIf[$getUserVar[mensagens]==2000]
+**$username** | enviou 2,000 mensagens!
+
+$elseIf[$getUserVar[mensagens]==3000]
+**$username** | enviou 3,000 mensagens
+
+$elseIf[$getUserVar[mensagens]==4000]
+**$username** | enviou 4,000 mensagens
+
+$elseIf[$getUserVar[mensagens]==5000]
+**$username** | enviou 5,000 mensagens
+
+$elseIf[$getUserVar[mensagens]==6000]
+**$username** | enviou 6,000 mensagens
+
+$elseIf[$getUserVar[mensagens]==7000]
+**$username** | enviou 7,000 mensagens
+
+$elseIf[$getUserVar[mensagens]==8000]
+**$username** | enviou 8,000 mensagens
+
+$elseIf[$getUserVar[mensagens]==9000]
+**$username** | enviou 9,000 mensagens
+
+$elseIf[$getUserVar[mensagens]==10000]
+**$username** | enviou 10,000 mensagens
+
+$endelseIf
+$endif
 `
 } 
