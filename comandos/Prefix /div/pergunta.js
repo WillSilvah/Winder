6f648eda9a103a997b84bca1618d22b7cd06df3f
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-22 21:26:36 -0300

    Update pergunta.js

--- comandos/Prefix /div/pergunta.js
+++ comandos/Prefix /div/pergunta.js
@@ -6,6 +6,6 @@ code: `$reply
 $botTyping
 $onlyIf[$message!=;<@$authorID> | Faça alguma pergunta e eu tentarei responder ela da forma mais padronizada possível. **$getServerVar[prefixo]$randomText[perguntar;ask] <pergunta>**]
 <@$authorID> $randomText[Hmm, eu não sei nada sobre isso.;Claro!;Com certeza.;Absoluta;Óbvio;Não;Nunca;Pergunta no posto Ipiranga!;O google provavelmente sabe;Esqueça a minha existência]
-$cooldown[10s;<@$authorID> | Espere **%time** para executar este comando novamente]
+$cooldown[10s;<@$authorID> | Espere **%time%** para executar este comando novamente]
 `
 } 
