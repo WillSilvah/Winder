9e5b93d6cdd87b609714739e7d21c5ebb1adaf3a
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-22 21:04:37 -0300

    Update rating.js

--- comandos/Prefix /uti/rating.js
+++ comandos/Prefix /uti/rating.js
@@ -1,8 +1,10 @@
 module.exports = {
 name: "$alwaysExecute",
+$if: "v4",
 code: `$reply
-$onlyIf[$randomText[no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;yes]==yes;]
+$if[$randomText[no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;yes]==yes]
 $setUserVar[cataventos;$sum[$getUserVar[cataventos];1]]
 
+$endIf
 `
 }
