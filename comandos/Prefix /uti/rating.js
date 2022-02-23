22a31558d06699a251d16bd188a390ae8571f76b
SrGatinho <49321188+WillSilvah@users.noreply.github.com>
2022-02-22 21:02:16 -0300

    Update rating.js

--- comandos/Prefix /uti/rating.js
+++ comandos/Prefix /uti/rating.js
@@ -1,6 +1,5 @@
 module.exports = {
 name: "$alwaysExecute",
-$if: "v4",
 code: `$reply
 $onlyIf[$randomText[no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;yes]==yes;]
 $setUserVar[cataventos;$sum[$getUserVar[cataventos];1]]
