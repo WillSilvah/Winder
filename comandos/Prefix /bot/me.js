9f5dfd0cf4d451029e7b0636c369060897073033
Williams <49321188+WillSilvah@users.noreply.github.com>
2022-02-23 11:45:58 -0300

    Update me.js

--- comandos/Prefix /bot/me.js
+++ comandos/Prefix /bot/me.js
@@ -2,6 +2,8 @@ module.exports = {
  name: "$alwaysExecute",
  code: `
  $setUserVar[mensagens;$sum[$getUserVar[mensagens];1]]
+ $setUserVar[totalmensagens;$sum[$getUserVar[totalmensagens];1]]
+
  $setUserVar[mensagens2;$hour:$minute:$second - $day/$month/$year]
  $setUserVar[mensagens1;$message]
  $setUserVar[mensagens3;<#$channelID>]
