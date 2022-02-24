cbd7fb29646c7756af903e3265b33db1a997acf4
Williams <49321188+WillSilvah@users.noreply.github.com>
2022-02-24 19:27:08 -0300

    Update guess.js

--- comandos/Prefix /div/guess.js
+++ comandos/Prefix /div/guess.js
@@ -1,8 +1,19 @@
 module.exports = {
 name: "guessnumber",
+aliases: ['adivinharnumero','guessthenumber','gtn',],
 $if: "v4",
 code: `$botTyping
-$if[$getServerVar[guess]==$message[1]]
+$if[$message[1]!=]
+<@$authorID> | Adivinhe qual número estou pensando!
+$author[1;Como funciona?]
+$description[1;
+A cada 1 hora, eu atualizo uma variável colocando um número aleátorio de 1 até 20 e este mesmo número é igual a todos deste servidor.
+Os membros deverão pensar qual número estou pensando naquele momento, quando alguém acertar, eu irei sortear outro número e a brincadeira continua!
+> **__Você ganha 1 catavento cada vez que acerta :)__**
+]
+$footer[1;$getServerVar[prefixo]$randomText[guessnumber;adivinharnumero;guessthenumber;gtn] $random[1;20] | EXEMPLO]
+
+$elseIf[$getServerVar[guess]==$message[1]]
 $reply $botTyping
 <@$authorID> | Parabéns! Foi o número **$message[1]** que eu estava pensando mesmo! Você ganhou um catavento
 $addCmdReactions[<:pinw_branco:945614522385825862>]
