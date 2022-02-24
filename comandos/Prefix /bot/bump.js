8f1d043f4cb7e38a2ccc07a38fcaeca355542dcb
Williams <49321188+WillSilvah@users.noreply.github.com>
2022-02-24 10:54:45 -0300

    Update bump.js

--- comandos/Prefix /bot/bump.js
+++ comandos/Prefix /bot/bump.js
@@ -1,19 +1,8 @@
 module.exports = [{
 name: "!d bump",
-nonPrefixied: true,
-code: `
-$setTimeout[bump;$get[sc]s;{
-"cid":"861337787192836117",
-"prize":"10s",
-}],
-Codigo de quando usar !d bump`
-},{
- name: "bump",
- type: "timeout",
- code: `
-$setTimeout[bump;$get[sc]s;{
-"cid":"861337787192836117",
-"prize":"10s",
-}],
-$channelSendMessage[861337787192836117;Codigo de quando acabar o tempo, o bot enviar message]`
-}]
+nonPrefixed: true,
+ code: `$setUserVar[bump;$sum[$getUserVar[bump];1]] $addCmdReactions[:pwh_gato_like:]
+ $onlyForChannels[861422411915722774;]
+ $globalCooldown[2h;]
+`
+} 
