daa80b9594f3edc230ab5383ca50a12126365f57
Williams <49321188+WillSilvah@users.noreply.github.com>
2022-02-24 11:49:21 -0300

    Update rating.js

--- comandos/Prefix /uti/rating.js
+++ comandos/Prefix /uti/rating.js
@@ -4,7 +4,7 @@ $if: "v4",
 code: `$reply
 $if[$randomText[no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;yes]==yes]
 $setUserVar[cataventos;$sum[$getUserVar[cataventos];1]]
-$addCmdReactions[$randomText[<:pinw_roxo:945624151681560636>;<:pinw_rosa:945621147733590046>;<:pinw_ciano:945623416294240287>;<:pinw_branco:945614522385825862>;<:pinw_amarelo:945622734484942849>;<:pinw_vermelho:945615345526374460>;<:pinw_verde:945617632114130964>]]
+$addCmdReactions[<:pinw_branco:945614522385825862>]
 $endIf
 `
 }
