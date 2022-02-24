dd9846290a0c0592c37be62d2020f88b37423fb9
Williams <49321188+WillSilvah@users.noreply.github.com>
2022-02-24 11:55:57 -0300

    Update guess.js

--- comandos/Prefix /div/guess.js
+++ comandos/Prefix /div/guess.js
@@ -4,15 +4,14 @@ $if: "v4",
 code: `$botTyping
 $if[$getServerVar[guess]==$message[1]]
 $reply $botTyping
-<@$authorID> | Parabéns! Você adivinhou o número que estava pensando!
-Você ganhou um catavento!
+<@$authorID> | Parabéns! Foi o número **$message[1]** que eu estava pensando mesmo! Você ganhou um catavento
+$addCmdReactions[<:pinw_branco:945614522385825862>]
 $setServerVar[guess;$random[1;20]]
 $setUserVar[cataventos;$sum[$getUserVar[cataventos];1]]
 
 $elseIf[$getServerVar[guess]!=$message[1]]
 $reply $botTyping
-<@$authorID> | Parabéns! Você errou :joy:
-Tente novamente daqui a pouco
+<@$authorID> | Eu não pensei no número **$message[1]**, tente novamente daqui a 30 segundos!
 
 $endelseIf
 $endif
