012fb45d828f967a03b0a34a0ec76480fb5821b4
Williams <49321188+WillSilvah@users.noreply.github.com>
2022-02-25 10:49:38 -0300

    Create lite ativar.js

---
+++ comandos/Prefix /uti/lite ativar.js
@@ -0,0 +1,7 @@
+module.exports = {
+name: "lite ativar",
+code: `
+$giveRole[$authorID;946206255855312917]
+<@$authorID> | **pinwheel Lite** ativado! Você não terá acesso a canais considerados "não essenciais", terá pouca movimentação no seu Discord como minimas
+`
+}
