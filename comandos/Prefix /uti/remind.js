933f6ec4259a034f9fd4d905d4407caa18ea7d8a
Williams <49321188+WillSilvah@users.noreply.github.com>
2022-02-25 10:36:02 -0300

    Update remind.js

--- comandos/Prefix /uti/remind.js
+++ comandos/Prefix /uti/remind.js
@@ -1,12 +1,12 @@
-modules.exports = ({
-    name: "reminder", 
-    code: `Ich errinere dich an $messageSlice[1] nach $message[1].
+modules.exports = [{
+name: "reminder", 
+type: "timeout",
+code: `Ich errinere dich an $messageSlice[1] nach $message[1].
 $setTimeout[remind;$message[1];
 userID: $authorID,
 message: $messageSlice[1]] `
-})
-
-bot.timeoutCommand({
-name:"remind",
+},{
+name: "remind",
+type: "timeout",
 code: `<@!$timeoutData[userID]> reminder for $timeoutData[message]`
-})
+}]
