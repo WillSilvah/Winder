d53319e96023092371ab5ae2ca1fc852e9314c81
Williams <49321188+WillSilvah@users.noreply.github.com>
2022-02-25 10:55:29 -0300

    Update lite desativar.js

--- comandos/Prefix /uti/lite desativar.js
+++ comandos/Prefix /uti/lite desativar.js
@@ -1,7 +1,7 @@
 module.exports = {
 name: "lite desativar",
 code: `
-$takeRole[$authorID;946206255855312917]
+$takeRole[$guildID;$authorID;946206255855312917]
 <@$authorID> | **pinwheel Lite** desativados! Você terá acesso a todos os canais normalmente :)
 `
 }
