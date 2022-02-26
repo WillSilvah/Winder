79353b29fe5cd3a2d1dc11eb722fdaca40e57e3d
Williams <49321188+WillSilvah@users.noreply.github.com>
2022-02-26 12:53:10 -0300

    Update and rename lite ativar.js to lite.js

--- comandos/Prefix /uti/lite ativar.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-name: "lite ativar",
-code: `
-$giveRole[$guildID;$authorID;946206255855312917]
-<@$authorID> | **pinwheel Lite** ativado! Você não terá acesso a canais considerados "não essenciais", terá pouca movimentação no seu Discord como minimas
-`
-}
