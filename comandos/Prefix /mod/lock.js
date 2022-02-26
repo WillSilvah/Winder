70e69a58529e202c71c988e782e63ffb640e44fc
Williams <49321188+WillSilvah@users.noreply.github.com>
2022-02-26 15:18:17 -0300

    Update lock.js

--- comandos/Prefix /mod/lock.js
+++ comandos/Prefix /mod/lock.js
@@ -4,6 +4,6 @@ module.exports = {
   aliases: ['trancar','travar'],
   code: `$reply
   $modifyChannelPerms[$guildID;$channelID;-sendmessage]
-<@$authorID> | <#$channelID> foi bloqueado com sucesso! Por motivos de prioridade e qualidade de serviço, somente pinwheel Ultra+ pode conversar aqui mesmo com o canal bloqueado.
+<@$authorID> | <#$channelID> foi bloqueado com sucesso!
 $onlyPerms[managechannel;<@$authorID> | Você precisa ter a permissão de gerenciar canais para executar este comando!]`
 }
