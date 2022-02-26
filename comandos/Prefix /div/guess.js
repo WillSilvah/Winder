3c7b91c29c6d3a5bb5d839c625a641dc2c126a2b
Williams <49321188+WillSilvah@users.noreply.github.com>
2022-02-25 21:58:13 -0300

    Update guess.js

--- comandos/Prefix /div/guess.js
+++ comandos/Prefix /div/guess.js
@@ -3,15 +3,7 @@ name: "guessnumber",
 aliases: ['adivinharnumero','guessthenumber','gtn',],
 $if: "v4",
 code: `$botTyping
-$if[$message!=]
-<@$authorID> | Adivinhe qual número estou pensando!
-$author[1;Como funciona?]
-$description[1;
-A cada 1 hora, eu atualizo uma variável colocando um número aleátorio de 1 até 20 e este mesmo número é igual a todos deste servidor.
-Os membros deverão pensar qual número estou pensando naquele momento, quando alguém acertar, eu irei sortear outro número e a brincadeira continua!
-> **__Você ganha 1 catavento cada vez que acerta :)__**
-]
-$footer[1;$getServerVar[prefixo]$randomText[guessnumber;adivinharnumero;guessthenumber;gtn] $random[1;20] | EXEMPLO]
+$onlyIf[$message!=;<@$authorID> | Adivinhe qual número estou pensando!]
 
 $elseIf[$getServerVar[guess]==$message[1]]
 $reply $botTyping
@@ -24,7 +16,6 @@ $elseIf[$getServerVar[guess]!=$message[1]]
 $reply $botTyping
 <@$authorID> | Eu não pensei no número **$message[1]**, tente novamente daqui a 30 segundos!
 
-$endelseIf
 $endelseIf
 $endif
 $cooldown[30s;<@$authorID> | Espere **%time%** para adivinhar novamente!]
