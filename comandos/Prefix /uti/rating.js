3f7ff3ca090ebb90b465bbdd320b8c732de9d301
Williams <49321188+WillSilvah@users.noreply.github.com>
2022-02-26 14:52:30 -0300

    Update rating.js

--- comandos/Prefix /uti/rating.js
+++ comandos/Prefix /uti/rating.js
@@ -2,7 +2,8 @@ module.exports = {
 name: "$alwaysExecute",
 $if: "v4",
 code: `$reply
-$if[$randomText[no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;yes]==yes]
+$argsCheck[3;]
+$if[$randomText[no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;yes;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no]==yes]
 $setUserVar[cataventos;$sum[$getUserVar[cataventos];1]]
 $addCmdReactions[<:pinw_branco:945614522385825862>]
 $cooldown[5s;]
