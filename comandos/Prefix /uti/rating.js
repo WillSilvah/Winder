ce14142ddc2811887946eba702a4d36e923e3be4
Williams <49321188+WillSilvah@users.noreply.github.com>
2022-02-26 10:10:20 -0300

    Update rating.js

--- comandos/Prefix /uti/rating.js
+++ comandos/Prefix /uti/rating.js
@@ -5,6 +5,7 @@ code: `$reply
 $if[$randomText[no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;no;yes]==yes]
 $setUserVar[cataventos;$sum[$getUserVar[cataventos];1]]
 $addCmdReactions[<:pinw_branco:945614522385825862>]
+$cooldown[5s;]
 $endIf
 `
 }
