ca34a87650efaacf8879251e0fe2389b535e9fee
Williams <49321188+WillSilvah@users.noreply.github.com>
2026-01-25 14:44:37 -0300

    Delete express.js

--- express.js
+++
@@ -1,9 +0,0 @@
-const express = require('express'); // Conexão com o pacote 'express'
-const app = express(); // criando app
-app.get("/", (request, response) => { // Recebendo respostas do website.
-  const ping = new Date(); // Obtendo a data e hora atual.
-  ping.setHours(ping.getHours() - 3); // Reduzindo 3 horas por causa do fuso horário.
-console.log(`${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()} => Website ping recebido.`); // Enviando mensagem aos logs.
-  response.sendStatus(200); // Enviando resposta para o visitante do site.
-});
-app.listen(process.env.PORT);// Escutando a porta do servidor.
