267e4716bf8fcf7e19faac6fb6f41b993573995f
WillSilvah <williamfelipe235@gmail.com>
2026-01-26 02:43:13 -0300

    Adição de aviso no canal console ao reiniciar

--- comandos/WinderEventos/readyCommand.js
+++ comandos/WinderEventos/readyCommand.js
@@ -3,6 +3,17 @@ name: "readyEvent",
 type: "clientReady",
 channel: "1462224055884189781",
 code: `
+$channelSendMessage[1465218335389257802;{newEmbed:
+{author:$username[$clientID] está ligado!]}
+{description:
+{field:AOi.JS:$packageVersion:true}
+{field:Node.JS:$nodeVersion:true}
+{field:Versão do $username[$clientID]:$getVar[versão]:true}
+{color:Blue}
+}
+
+
+}
 
 $log[---------------
 $username[$clientID] está ligado! >w<
