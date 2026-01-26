e7fa7295a368c08fabfcf4aba97ed15abe88f106
WillSilvah <williamfelipe235@gmail.com>
2026-01-26 12:17:03 -0300

    Correção do gato.js

--- comandos/Interactions/Imagens/gato.js
+++ comandos/Interactions/Imagens/gato.js
@@ -6,9 +6,7 @@ code: `
 $interactionFollowUp[
 {attachment:winder-gato.png:$getObjectProperty[gato;image]}
 
-$createObject[gato;$getObjectProperty[api;images[0]]]
-
-$createObject[api;$exec[curl -X GET -H 'user-agent: nsei -v ' https://api.animality.xyz/all/cat]
+$createObject[gato;$exec[curl -X GET -H 'user-agent: nsei -v ' https://api.animality.xyz/all/cat]
 $reply
 $interactionDefer[false]
   `
