aef1a402dff3b02869c09b3a23257c5340519b38
WillSilvah <williamfelipe235@gmail.com>
2026-01-25 23:58:54 -0300

    Adição de automação de slash commands, /ping e correção do botinfo

---
+++ comandos/CriarSlashAuto/ping.js
@@ -0,0 +1,9 @@
+module.exports = [{
+name: "readyEvent",
+type: "ready",
+channel: "861337787192836117",
+code: `
+$createApplicationCommand[global;ping;Pong!;false;guild;guild,botdm,dm;slash]
+
+`
+}]
--- comandos/WinderEventos/readyCommand.js
+++ comandos/WinderEventos/readyCommand.js
@@ -4,6 +4,8 @@ type: "clientReady",
 channel: "1462224055884189781",
 code: `
 $deleteIn[1m]
+$author[1;$username[$clientID] está pronto!;$userAvatar[$clientID]]
+$color[1;Random]
 
 $log[---------------
 OwO $username[$clientID] está ligado! >w<
