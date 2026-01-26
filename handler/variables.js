bebd259b512d79123a20bf607af3acf5e3051937
WillSilvah <williamfelipe235@gmail.com>
2026-01-25 23:23:25 -0300

    Adição do prefixo nas variaveis

--- handler/variables.js
+++ handler/variables.js
@@ -2,6 +2,7 @@ module.exports = (client) => {
 client.variables({
 
   "versão": "Winder (old gen) 01",
+  "prefixo": "w!",
   "msgPerMinute": "0",
   "lastMessage": "",
   "lastMessageChannel": "",
