56953fdd39c578c5c9e83c32b5f02daa53b8db27
WillSilvah <williamfelipe235@gmail.com>
2026-01-26 23:22:17 -0300

    Alteração

--- comandos/WinderEventos/awaitWelcomeStaff.js
+++ comandos/WinderEventos/awaitWelcomeStaff.js
@@ -4,7 +4,7 @@ type: "memberUpdate",
 channel: "$getVar[staffChat]",
 code: `
 $channelSendMessage[$getVar[batePapo];{newEmbed:
-{author:$userTag entrou na Equipe Patinhas!:https://cdn.discordapp.com/emojis/1464435933729263657.png?size=2048}
+{author:@$userTag entrou na Equipe Patinhas!:https://cdn.discordapp.com/emojis/1464435933729263657.png?size=2048}
 {color:Orange}
 }]
 
