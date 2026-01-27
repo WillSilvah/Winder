5b919ebaeba275f068ae920e384d547146a5cb14
WillSilvah <williamfelipe235@gmail.com>
2026-01-26 23:26:59 -0300

    Alteração

---
+++ comandos/WinderEventos/resetMsg.js
@@ -0,0 +1,11 @@
+module.exports = [{
+name: "resetMsgs",
+type: "loop",
+channel: "$getVar[consoleChat]",
+executeOnStartup: "true",
+every: 65000,
+code: `
+$setGuildVar[msgPerMinute;0;$getVar[guildID]]
+
+`
+}]
