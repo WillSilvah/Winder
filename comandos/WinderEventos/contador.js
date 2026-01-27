7e6c3ac7a3138039e7f4337bf015a330f43906e8
WillSilvah <williamfelipe235@gmail.com>
2026-01-26 23:34:23 -0300

    Alteração

---
+++ comandos/WinderEventos/contador.js
@@ -0,0 +1,16 @@
+module.exports = [{
+name: "contador",
+type: "loop",
+channel: "$getVar[consoleChat",
+executeOnStartup: "true",
+every: 600000,
+code: `
+$editChannel[1463122813736063016;🏆│falta $sub[$getGuildVar[meta;$getVar[guildID]];$membersCount[$guildID;all;no]] peludos]
+
+$editChannel[1465535257297027214;🐾│$membersCount[$guildID;all;no] peludos]
+
+$editChannel[1465534760620392722;💬│$getGuildVar[guildMonthMessages;$getVar[guildID]] mensagens]
+
+$cacheMembers[$getVar[guildID];no]
+`
+}]
