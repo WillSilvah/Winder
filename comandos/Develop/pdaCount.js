65b46967959099d3fc5c5108590f9dc1b908c835
WillSilvah <williamfelipe235@gmail.com>
2026-01-27 17:21:28 -0300

    Adição do missions e verificação de membros migrada para o Winder

--- comandos/Develop/pdaCount.js
+++ comandos/Develop/pdaCount.js
@@ -31,6 +31,7 @@ $onlyIf[$hasRoles[$getVar[guildID];$authorID;$getVar[memberVerifiedRole]]==true;
 $onlyIf[$getGuildVar[pickStatus;$getVar[guildID]]==false;]
 $onlyIf[$checkContains[$channelID;1462546608758718515]==false;]
 $onlyIf[$checkContains[$channelCategoryID[$channelID];1462224055884189780;1463544077554552892]==true;]
+$onlyIf[$stringStartsWith[$message;+;=;w!+w+;/;m!]==false;]
 $onlyIf[$guildID==$getVar[guildID];]
 
 $cooldown[5s;]
