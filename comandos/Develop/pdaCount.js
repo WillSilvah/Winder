086040a417ad3b6f03d288c16ccd37118d0116a1
WillSilvah <williamfelipe235@gmail.com>
2026-01-26 21:30:00 -0300

    Correção de erro no PDACOUNT

---
+++ comandos/Develop/pdaCount.js
@@ -0,0 +1,47 @@
+module.exports = [{
+name: "$alwaysExecute",
+code: `
+
+$setGlobalUserVar[msgXP;$sum[$getGlobalUserVar[msgXP;$authorID];$get[pda]];$authorID]
+$setGlobalUserVar[msgXPtotal;$sum[$getGlobalUserVar[msgXPtotal;$authorID];$get[pda]];$authorID]
+
+$channelSendMessage[1465378100505219072;{newEmbed:
+{author:$userTag ($authorID):$userAvatar}
+{description:
+Contagem de caracteres: $charCount[$message]
+Argumentos: $argsCount
+PDA ganho: $get[pda]
+Link: https://discord.com/channels/$guildID/$channelID/$messageID
+}
+{footer:msgType0}
+{color:$dyColor[blue;light]}
+}]
+
+$let[pda;$truncate[$math[$charCount[$message]/6]]]
+
+$onlyIf[$if[$argsCount<=10;true;false]|$if[$isCaps[80%;$message]==true;true;false]==false|false;]
+$onlyIf[$charCount[$message]>=8;]
+$onlyIf[$checkContains[$message;lorem;gboard]==false;]
+$onlyIf[$hasRoles[1462224054676099094;$authorID;1462797987041513574]==true;]
+$onlyIf[$getGuildVar[pickStatus;1462224054676099094]==false;]
+$onlyIf[$guildID==1462224054676099094;]
+
+$cooldown[5s;]
+`
+},{
+//ESPECIAL
+name: "give-event",
+type: "awaited",
+code: `
+$addCmdReactions[🎁]
+
+$setGlobalUserVar[item;$sum[$getGlobalUserVar[item;$authorID;$get[item]];$authorID]
+
+$setGuildVar[itemTotal;$sum[$getGuildVar[itemTotal;1462224054676099094];$get[item]];1462224054676099094]
+
+$let[item;$random[1;3]]
+
+$cooldown[1m;]
+$onlyIf[$getGuildVar[eventItemStatus;1462224054676099094]==true;]
+`
+}]
