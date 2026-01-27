f1bb2f29584bb81f9b7a0a2380cc47568a8b36b1
WillSilvah <williamfelipe235@gmail.com>
2026-01-27 08:21:16 -0300

    Resolução do contador

--- comandos/WinderEventos/contador.js
+++ comandos/WinderEventos/contador.js
@@ -5,7 +5,8 @@ channel: "$getVar[consoleChat]",
 executeOnStartup: "true",
 every: 600000,
 code: `
-$djsEval[message.guild.channels.cache.get("1463122813736063016").setName(" | falta $sub[$getGuildVar[membersMeta;$getVar[guildID]];$membersCount[$guildID;all;false]] peludos")]
+
+$editChannel[1463122813736063016; | falta $sub[$getGuildVar[membersMeta;$getVar[guildID]];$membersCount[$guildID;all;false]] peludos]
 
 $djsEval[message.guild.channels.cache.get("1465535257297027214").setName(" | $membersCount[$guildID;all;false] peludos")]
 
