cfd503b52595e8d846a034bb2819bca566f1b89e
WillSilvah <williamfelipe235@gmail.com>
2026-01-27 07:42:01 -0300

    Utilização do Discord.js para o contador

--- comandos/WinderEventos/contador.js
+++ comandos/WinderEventos/contador.js
@@ -1,15 +1,15 @@
 module.exports = [{
 name: "contador",
 type: "loop",
-channel: "$getVar[consoleChat",
+channel: "$getVar[consoleChat]",
 executeOnStartup: "true",
 every: 600000,
 code: `
-$editChannel[1463122813736063016; | falta $sub[$membersCount[$guildID;all;no];$getGuildVar[membersMeta;$getVar[guildID]]] peludos]
+$djsEval[message.guild.channels.cache.get("1463122813736063016").setName(" | falta $sub[$membersCount[$guildID;all;no];$getGuildVar[membersMeta;$getVar[guildID]]] peludos")]
 
-$editChannel[1465535257297027214; | $membersCount[$guildID;all;no] peludos]
+$djsEval[message.guild.channels.cache.get("1465535257297027214").setName(" | $membersCount[$guildID;all;no] peludos")]
 
-$editChannel[1465534760620392722; | $getGuildVar[guildMonthMessages;$getVar[guildID]] mensagens]
+$djsEval[message.guild.channels.cache.get("1465534760620392722").setName(" | $getGuildVar[guildMonthMessages;$getVar[guildID]] mensagens")]
 
 $cacheMembers[$getVar[guildID];no]
 `
