6d470b2208a3c0e0eaf264cb16fb6b8189905850
WillSilvah <williamfelipe235@gmail.com>
2026-01-27 23:27:14 -0300

    adição do awaitMembersVerified

--- comandos/WinderEventos/awaitMemberVerified.js
+++ comandos/WinderEventos/awaitMemberVerified.js
@@ -1,27 +1,28 @@
 module.exports = [{
 name: "memberVerified",
 type: "memberUpdate",
-channel: "861337787192836117",
+channel: "$getVar[batePapo]",
 code: `
 $sendDm[{newEmbed:
-{title:<:gato_wow:1161731796459860018> Parabéns, você virou membro verificado na Pinwheel!}
+{title:<:pats_foxWoW:1463244909195821107> Parabéns, você virou furro verificado na Patinhas!}
 {url:https://www.bing.com/search?q=Folhas+de+verifica%C3%A7%C3%A3o&qs=n&form=QBRE&sp=-1&lq=0&pq=folhas+de+verifica%C3%A7%C3%A3o&sc=10-21&sk=&cvid=A51B72AD645C4C6F8B4B477A9279AD73&ghsh=0&ghacc=0&ghpl=}
 {description:
-Você enviou uma quantidade de mensagens e agora pode aproveitar a Pinwheel tendo permissões básicas!
+Você enviou uma quantidade de mensagens e agora pode aproveitar a Patinhas tendo permissões básicas!
 Veja mais o que você pode fazer:
 
 1. Enviar imagens, vídeos, arquivos e áudios em chats de texto (permitidos)
 2. Alterar o seu apelido, como também mudar o seu avatar exclusivamente neste servidor (Com nitro)
 3. Poder usar comandos do Winder mais livremente.
-4. Pode enviar links nos canais de texto.
-5. E outras coisas!
+4. Pode enviar links e mídia nos canais de texto.
+5. Começou a ganhar pontos de atividades.
+6. E outras coisas!
 
 }
 {footer:Espero que esteja gostando da Piwnheel. Se quiser, convide seus amigos para conhecer esta comunidade!}
-{color:$dyColor[green;light]}
-{thumbnail:https://i.ibb.co/q1tKKKj/IMG-20240530-092934.png}
+{color:Red}
+{thumbnail:https://cdn.discordapp.com/emojis/1463245277304459345.png?size=2048}
 };$authorID]
 
-$onlyIf[$newMember[addedRoles]==$roleName[1065319142271496212];]
+$onlyIf[$newMember[addedRoles]==$roleName[$getVar[memberVerifiedRole]];]
 `
 }]
