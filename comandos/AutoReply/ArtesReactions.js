93776ae9709c8ca6531ca9b940e45c004f16c679
Williams <49321188+WillSilvah@users.noreply.github.com>
2026-01-30 11:12:05 -0300

    Update ArtesReactions.js
    Agora, artistas que postar artes no canal de artes, precisam escrever #artes no final da mensagem.

--- comandos/AutoReply/ArtesReactions.js
+++ comandos/AutoReply/ArtesReactions.js
@@ -3,7 +3,9 @@ name: "$alwaysExecute",
 code: `
 $addCmdReactions[❤️]
 
+$onlyIf[$if[$messageAttachment!=;true;false];$checkContains[$stringEndsWith[$message;#artes] | $stringEndsWith[$message;#art];true]==true;true;]
 $onlyIf[$messageAttachment!=;]
 $onlyIf[$channelID==1462224055884189782;]
 `
 } 
+
