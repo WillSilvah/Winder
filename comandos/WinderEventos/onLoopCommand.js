2fec23b59cc55faa88be2b66745e8544be1961ee
WillSilvah <williamfelipe235@gmail.com>
2026-01-31 06:43:20 -0300

    Atualizações

--- comandos/WinderEventos/onLoopCommand.js
+++ comandos/WinderEventos/onLoopCommand.js
@@ -30,17 +30,16 @@ $channelSendMessage[1466734269572579399;{newEmbed:
 {author:$getObjectProperty[session;name]:https://abs.twimg.com/emoji/v2/72x72/1f4ac.png]
 {title:Top fofoqueiros ativos}
 {description:
-$userLeaderBoard[$guildID;msgXP;desc;{top}° **<@{id}>** -> ✨️{value};11;1]
+$userLeaderBoard[1462224054676099094;msgXP;desc;{top}° **<@{id}>** -> ✨️{value};11;1]
 
-$userLeaderBoard[$guildID;messageMonth;desc;{top}° **<@{id}>** -> 💬️{value};11;1]
+$userLeaderBoard[1462224054676099094;messageMonth;desc;{top}° **<@{id}>** -> 💬️{value};11;1]
 }
 {footer:Atualizado sempre às 22h | w!rank}
 {color:Green}
-]
 
 $createObject[session;$readFile[Recursos/session.json]]
 
-$clear[1466734269572579399;6]
+$clear[$getGuildVar[rankedChat;1462224054676099094];6]
 
 `
 },{
