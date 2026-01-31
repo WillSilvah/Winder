2be03bff50107a02a41749a6880a45a2be6fbe37
WillSilvah <williamfelipe235@gmail.com>
2026-01-31 06:47:00 -0300

    Atualizações

--- comandos/WinderEventos/onLoopCommand.js
+++ comandos/WinderEventos/onLoopCommand.js
@@ -35,7 +35,7 @@ $userLeaderBoard[1462224054676099094;msgXP;desc;{top}° **<@{id}>** -> ✨️{va
 $userLeaderBoard[1462224054676099094;messageMonth;desc;{top}° **<@{id}>** -> 💬️{value};11;1]
 }
 {footer:Atualizado sempre às 22h | w!rank}
-{color:Green}
+{color:Green}]
 
 $createObject[session;$readFile[Recursos/session.json]]
 
