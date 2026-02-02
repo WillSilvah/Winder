6b7a864eff8efbb7d3907878da7551867e0c8017
WillSilvah <williamfelipe235@gmail.com>
2026-02-01 22:27:40 -0300

    Correções

--- comandos/Develop/pdaCount.js
+++ comandos/Develop/pdaCount.js
@@ -29,16 +29,16 @@ $let[basicPDA;$if[$get[textDivisorPDA]>=15;15;$get[textDivisorPDA]]]
 
 $let[textDivisorPDA;$truncate[$math[$charCount[$message]/3]]]
 
-$onlyIf[$charCount[$message]>=8;]
+$onlyIf[$argsCheck[$message]>=3;]
 $onlyIf[$checkContains[$message;lorem;gboard]==false;]
 $onlyIf[$getGuildVar[pickStatus;$guildID]==false;]
 $onlyIf[$checkContains[$channelID;1462546608758718515]==false;]
-$onlyIf[$checkContains[$channelCategoryID[$channelID];1462224055884189780;1463544077554552892]==true;]
-$onlyIf[$stringStartsWith[$message;+;=;w!+w+;/;m!]==false;]
+$onlyIf[$checkContains[$channelCategoryID[$channelID];1462224055884189780]==true;]
+$onlyIf[$stringStartsWith[$message;+;=;w!+w+;/;m!]==false;] 
 $onlyIf[1462224054676099094==$guildID;]
 $onlyIf[$hasRoles[$guildID;$authorID;$getVar[memberVerifiedRole]]==true;]
 
-$cooldown[10s;]
+$cooldown[1m;]
 `
 },{
 //ESPECIAL
