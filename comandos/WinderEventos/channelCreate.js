afbf6c932431d7eff6ad3d0a35931489abb79978
WillSilvah <williamfelipe235@gmail.com>
2026-02-02 08:16:48 -0300

    Correções

--- comandos/WinderEventos/channelCreate.js
+++ comandos/WinderEventos/channelCreate.js
@@ -12,7 +12,7 @@ name: "threadCreate",
 type: "threadCreate",
 channel: "",
 code: `
-$channelSendMessage[$newChannel[id];$randomText[Meow! :3;Miaau! :3;Meeoow! :3;Primeiro! $customEmoji[justAFox];Olá! $customEmoji[foxKisses]]]
+$channelSendMessage[$newChannel[id];$randomText[Meow! :3;Miaau! :3;Meeoow! :3;Primeiro! $customEmoji[pats_justAFox];Olá! $customEmoji[pata_foxKiss]]]
 
 $onlyIf[$isTicket[$channelID]==false;]
 `
