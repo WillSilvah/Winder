14d5eb2300e567b6510048a3dab7e86f4804600a
WillSilvah <williamfelipe235@gmail.com>
2026-02-01 23:18:13 -0300

    Instalação doi AOi.Music

--- index.js
+++ index.js
@@ -36,22 +36,21 @@ const client = new AoiClient({
     });
 
 const voice = new AoiVoice(client, {
-   devOptions: {
-      debug: false, // Set to true for debugging purposes
-   },
-   searchOptions: {
-      soundcloudClientId: "",
-      youtubeAuth: false, // Options: TRUE or FALSE, default: TRUE
-      youtubegl: "US", // default: US
-      youtubeClient: "WEB", // Options: "WEB" | "ANDROID" | "YTMUSIC_ANDROID" | "YTMUSIC" | "YTSTUDIO_ANDROID" | "TV_EMBEDDED", default: WEB
-   },
-   requestOptions: {
-      offsetTimeout: 1500, // Timeout in milliseconds for searching and skipping, default: 500
-      soundcloudLikeTrackLimit: 10, // Limit the number of liked tracks from SoundCloud, default: -1
-      youtubePlaylistLimit: 20, // Limit the number of tracks in a YouTube playlist, default: -1
-      spotifyPlaylistLimit: 30, // Limit the number of tracks in a Spotify playlist, default: -1
-   },
+  searchOptions: {
+    //soundcloudClientId: "Soundcloud ID", // optional
+    youtubegl: "US",
+  },
+  requestOptions: {
+    offsetTimeout: 0,
+    soundcloudLikeTrackLimit: 200,
+  },
 });
+
+const loader = new LoadCommands(client);
+loader.load(voice.cmds, "./eventsmusic/");
+
+voice.addEvent(PlayerEvents.QueueEnd);
+voice.addEvent(PlayerEvents.TrackStart);
 
 voice.addPlugin(PluginName.Cacher, new Cacher("memory" /* or "disk" */));
 voice.addPlugin(PluginName.Filter, new Filter({
