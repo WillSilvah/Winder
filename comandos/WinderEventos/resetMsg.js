52ccdd284209356b96befc235dac0f566b735b92
Williams Felipe <49321188+WillSilvah@users.noreply.github.com>
2026-02-03 10:34:01 -0300

    Update resetMsg.js
    teste de minutagens

--- comandos/WinderEventos/resetMsg.js
+++ comandos/WinderEventos/resetMsg.js
@@ -1,11 +1,17 @@
 module.exports = [{
 name: "resetMsgs",
 type: "loop",
-channel: "$getVar[consoleChat]",
+channel: "1468237433337544877",
 executeOnStartup: "true",
 every: 65000,
 code: `
 $setGuildVar[msgPerMinute;0;$guildID]
 
+$sendMessage[{newEmbed:
+{author:Mensagens por minuto: $getGuildVar[msgPerMinute;$guildID]}
+{color:Random}
+}
+]
 `
+
 }]
