35429c4dbfe63f00077af3f5b28d90a5ca5798fb
WillSilvah <williamfelipe235@gmail.com>
2026-02-04 11:45:39 -0300

    autoBanTester

---
+++ comandos/WinderEventos/verifyAutoBanMember.js
@@ -0,0 +1,12 @@
+module.exports = [{
+name: "joinbanteste",
+//type: "",
+channel: "",
+code: `
+Banido KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
+
+$onlyIf[$fileExists[Recursos/autoBanimentos/$authorID.json]==true;]
+`
+}]
+//$channelSendMessage[1463226796974543114;<@&1462915937433948351>
+//> Vão dar boas vindas ao <@$authorID> ($authorID) no <#1462224055884189781>!]
