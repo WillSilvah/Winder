931810cdb1cfe3d3f77ee735b9f6120f5b2278fc
WillSilvah <williamfelipe235@gmail.com>
2026-02-04 11:17:44 -0300

    autoBanTester

--- comandos/Moderacao/winderban.js
+++ comandos/Moderacao/winderban.js
@@ -9,22 +9,18 @@ $sendMessage[<@$authorID> **$username[$get[userID]]** foi **$get[puniType]**!]
 
 $writeFile[Recursos/autoBanimentos/$get[userID].json;
 {
-    "usename": "$username[$get[userID]]",
+    "username": "$username[$get[userID]]",
     "motivo": "$get[reason]",
-    "userID": "$get[ID]"
+    "userID": "$get[userID]"
 };utf8]
 
 $onlyIf[$checkContains[$;
 $onlyIf[$message[1]!=$authorID;<@$authorID> | Você está colocando seu próprio ID.]
 
-
-$onlyIf[$argsCount>0;]
-$ifAwaited[$argsCount==0;{execute:cmdinfo-await}]
-$let[cmdName;$commandName]
 $let[staffID;$authorID]
 $let[userID;$message[1]]
 $let[reason;$if[$messageSlice[1]==;Sei lá qual foi o motivo, só sei que recebeu!;$messageSlice[1]]]
-$let[puniType;Banido Automáticamente]
+$let[puniType;Banido Automaticamente]
 $clientTyping
 
 $onlyIf[$hasRoles[1462224054676099094;$authorID;1462547405466636384]==true;<@$authorID> | Você precisa ser **$username[$clientID]** Developer para poder executar esse comando.]
