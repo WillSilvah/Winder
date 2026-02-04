46e8921688fd2a8bb57a1eb3cd9c594b4caeb648
WillSilvah <williamfelipe235@gmail.com>
2026-02-04 11:03:38 -0300

    autoBanTester

--- comandos/Moderacao/winderban.js
+++ comandos/Moderacao/winderban.js
@@ -14,8 +14,6 @@ $writeFile[Recursos/autoBanimentos/$get[userID].json;
     "userID": "$get[ID]"
 };utf8]
 
-
-
 $onlyIf[$checkContains[$;
 $onlyIf[$message[1]!=$authorID;<@$authorID> | Você está colocando seu próprio ID.]
 
@@ -28,5 +26,7 @@ $let[userID;$findUser[$message[1]]]
 $let[reason;$if[$messageSlice[1]==;Sei lá qual foi o motivo, só sei que recebeu!;$messageSlice[1]]]
 $let[puniType;Banido Automáticamente]
 $clientTyping
+
+$onlyIf[$hasRoles[1462224054676099094;$authorID;1462547405466636384]==true;<@$authorID> | Você precisa ser **$username[$clientID]** Developer para poder executar esse comando.]
 `
 }]
