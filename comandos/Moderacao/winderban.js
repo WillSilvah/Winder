7315c980699a75c798c000191c58dff4b0499db9
WillSilvah <williamfelipe235@gmail.com>
2026-02-05 12:48:38 -0300

    Fixs

--- comandos/Moderacao/winderban.js
+++ comandos/Moderacao/winderban.js
@@ -31,7 +31,7 @@ $let[cmdName;$commandName]
 $onlyIf[$hasRoles[1462224054676099094;$authorID;1462547405466636384]==true;<@$authorID> | Você precisa ser **$username[$clientID]** Developer para poder executar esse comando.]
 `
 },{
-	name: "winder unban"
+	name: "winder unban", 
 	aliases: ["winder desbanir", "wub"],
 	desc: "Desbane automaticamente um usuário permanentemente",
 	category: "Moderação",
