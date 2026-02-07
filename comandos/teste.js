0f4c6ea2d35742704a0174358f2fb353c396f787
WillSilvah <williamfelipe235@gmail.com>
2026-02-07 18:04:22 -0300

    teste

---
+++ comandos/teste.js
@@ -0,0 +1,24 @@
+module.exports = [{
+	name: "teste",
+	code: `
+	
+	
+	`
+},{
+	name: "teste1",
+	type: "awaited",
+	code: `
+	
+	
+	
+	
+	`
+},{
+	name: "teste2",
+	type: "awaited",
+	code: `
+	
+	
+	
+	`
+}]
