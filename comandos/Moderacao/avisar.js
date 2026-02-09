d072958753dc8b5953c4f85b83f09db51aa69a85
WillSilvah <williamfelipe235@gmail.com>
2026-02-08 21:43:01 -0300

    w+avisar

--- comandos/Moderacao/avisar.js
+++ comandos/Moderacao/avisar.js
@@ -9,7 +9,7 @@ $sendMessage[<@$authorID> **$username[$get[userID]]** foi **$get[puniType]**!]
 
 $setUserVar[lastWarn;{"staffID": "$get[staffID]", "motivo": "$get[reason]"};$get[userID];$guildID]
 	
-$setUserVar[warnsTotal;$sum[$getUserVar[warnTotal;$get[userID];$guildID];1];$get[userID];$guildID]
+$setUserVar[warnsTotal;$sum[$getUserVar[warnsTotal;$get[userID];$guildID];1];$get[userID];$guildID]
 
 $onlyPerms[kickmembers;<@$authorID> {newEmbed: {author:Você não tem as permissões necessárias} {description:Por segurança, você precisa ter as permissões de **expulsar membros**.}  {footer:Que tal você entrar para a staff? $getGuildVar[prefixo]serstaff} {color:#6F03FC}}]
 $onlyIf[$get[userID]!=$authorID;<@$authorID> | Você está se automencionando, por favor, **@mencione/ID** o usuário]
