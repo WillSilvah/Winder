1a78e0d9228bdbe7e02aa60e9de3f33add78defc
WillSilvah <williamfelipe235@gmail.com>
2026-02-09 19:13:19 -0300

    teete

--- comandos/WinderEventos/awaitMemberUpdate.js
+++ comandos/WinderEventos/awaitMemberUpdate.js
@@ -3,6 +3,8 @@ name: "awaitMemberUpdate",
 type: "memberUpdate",
 channel: "$getVar[consoleChat]",
 code: `
+$getGlobalUserVar[lastMemberUpdateTime]
+$getGlobalUserVar[lastMemberUpdateStatus]
 
 $setGlobalUserVar[lastMemberUpdateStatus;$userStatus;$authorID]
 $setGlobalUserVar[lastMemberUpdateTime;$hour:$minute:$second - $formatDate[$dateStamp;DD/MM/YYYY];$authorID]
