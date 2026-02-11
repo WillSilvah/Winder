8b7b82ad507588b7345e2ae3cf2aef92e50ebd1c
WillSilvah <williamfelipe235@gmail.com>
2026-02-10 21:00:59 -0300

    w+avisar

--- comandos/WinderEventos/pdaPunishments.js
+++ comandos/WinderEventos/pdaPunishments.js
@@ -2,17 +2,6 @@ module.exports = [{
 	name: "warn-actions",
 	type: "awaited",
 	code: `
-
-$ifAwaited[$getUserVar[warnsTotal;$ifAwaited[$findUser[$message[1]]==;$message[1];$findUser[$message[1]];$guildID]==1;{execute:pdapunish-1warn}]
-
-
-`
-},{
-	name: "pdapunish-1warn",
-	type: "awaited",
-	code: `
-	
-
 $sendMessage[Removido $get[removepda] PDAs de <@$get[userID1]>]
 $setUserVar[msgXP;$sub[$getUserVar[msgXP;$ifAwaited[$findUser[$message[1]]==;$message[1];$findUser[$message[1]];$guildID];$get[removepda]];$ifAwaited[$findUser[$message[1]]==;$message[1];$findUser[$message[1]];$guildID]
 $setUserVar[msgXPtotal;$sub[$getUserVar[msgXPtotal;$ifAwaited[$findUser[$message[1]]==;$message[1];$findUser[$message[1]];$guildID];$get[removepda]];$ifAwaited[$findUser[$message[1]]==;$message[1];$findUser[$message[1]];$guildID]
