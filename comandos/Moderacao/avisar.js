677e81967e1f954729d0e029be202930f4a9ffff
WillSilvah <williamfelipe235@gmail.com>
2026-02-10 21:22:11 -0300

    w+avisar

--- comandos/Moderacao/avisar.js
+++ comandos/Moderacao/avisar.js
@@ -7,8 +7,6 @@ module.exports = [{
 	code: `
 $sendMessage[<@$authorID> **$username[$ifAwaited[$findUser[$message[1]]==;$message[1];$findUser[$message[1]]]** foi **$get[puniType]**!]
 
-$setUserVar[warnsTotal;$sum[$getUserVar[warnsTotal;$ifAwaited[$findUser[$message[1]]==;$message[1];$findUser[$message[1]];$guildID];1];$ifAwaited[$findUser[$message[1]]==;$message[1];$findUser[$message[1]];$guildID]
-
 $onlyPerms[kickmembers;<@$authorID> {newEmbed: {author:Você não tem as permissões necessárias} {description:Por segurança, você precisa ter as permissões de **expulsar membros**.}  {footer:Que tal você entrar para a staff? $getGuildVar[prefixo]serstaff} {color:#6F03FC}}]
 $onlyIf[$ifAwaited[$findUser[$message[1]]==;$message[1];$findUser[$message[1]]!=$authorID;<@$authorID> | Você está se automencionando, por favor, **@mencione/ID** o usuário]
 $onlyIf[$argsCount>0;]
