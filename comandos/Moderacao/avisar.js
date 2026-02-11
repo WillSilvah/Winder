3bad942e4a92bbc6394f6ad923afee7ac86ae7a3
WillSilvah <williamfelipe235@gmail.com>
2026-02-11 11:48:52 -0300

    w+avisar

--- comandos/Moderacao/avisar.js
+++ comandos/Moderacao/avisar.js
@@ -19,15 +19,15 @@ $setUserVar[warnsTotal;$sum[$getUserVar[warnsTotal;$findUser[$get[userID]];$guil
 
 $onlyPerms[kickmembers;<@$authorID> {newEmbed: {author:Você não tem as permissões necessárias} {description:Por segurança, você precisa ter as permissões de **expulsar membros**.}  {footer:Que tal você entrar para a staff? $getGuildVar[prefixo]serstaff} {color:#6F03FC}}]
 $onlyIf[$get[userID]!=$authorID;<@$authorID> | Você está se automencionando, por favor, **@mencione/ID** o usuário]
-$onlyIf[$argsCount>0;]
-$ifAwaited[$argsCount==0;{execute:cmdinfo-await}]
-$let[cmdName;$commandName]
 
 $let[staffID;$authorID]
 $let[userID;$ifAwaited[$findUser[$message[1]]==;$message[1];$findUser[$message[1]]]
 $let[reason;$if[$messageSlice[1]==;Sei lá qual foi o motivo, só sei que recebeu!;$messageSlice[1]]]
 $let[puniType;advertido]
 
+$onlyIf[$argsCount>0;]
+$ifAwaited[$argsCount==0;{execute:cmdinfo-await}]
+$let[cmdName;$commandName]
 $clientTyping	
 `
 }]
--- comandos/Moderacao/desavisar.js
+++ comandos/Moderacao/desavisar.js
@@ -17,15 +17,15 @@ $setUserVar[warnsTotal;$sub[$getUserVar[warnsTotal;$findUser[$get[userID]];$guil
 $onlyIf[$getUserVar[warnsTotal;$findUser[$get[userID]];$guildID]>0;<@$authorID> O usuário não tem nenhuma advertência.]
 $onlyPerms[kickmembers;<@$authorID> {newEmbed: {author:Você não tem as permissões necessárias} {description:Por segurança, você precisa ter as permissões de **expulsar membros**.}  {footer:Que tal você entrar para a staff? $getGuildVar[prefixo]serstaff} {color:#6F03FC}}]
 $onlyIf[$findUser[$get[userID]]!=$authorID;<@$authorID> | Você está se automencionando, por favor, **@mencione/ID** o usuário]
-$onlyIf[$argsCount>0;]
-$ifAwaited[$argsCount==0;{execute:cmdinfo-await}]
-$let[cmdName;$commandName]
 
 $let[staffID;$authorID]
 $let[userID;$ifAwaited[$findUser[$message[1]]==;$message[1];$findUser[$message[1]]]
 $let[reason;$if[$messageSlice[1]==;Sei lá qual foi o motivo, só sei que recebeu!;$messageSlice[1]]]
-$let[puniType;desavisado]
+$let[puniType;desavisado
 
+$onlyIf[$argsCount>0;]
+$ifAwaited[$argsCount==0;{execute:cmdinfo-await}]
+$let[cmdName;$commandName]
 $clientTyping	
 `
 }]
