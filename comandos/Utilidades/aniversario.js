e72414dcff7434a3dee9b76a34b9f95f4ee94750
WillSilvah <williamfelipe235@gmail.com>
2026-02-12 17:12:17 -0300

    aniversário

--- comandos/Utilidades/aniversario.js
+++ comandos/Utilidades/aniversario.js
@@ -2,11 +2,12 @@ module.exports = ({
     name: "aniversario",
     aliases: ["set-niver", "setniver"],
     desc: "Não deixe que seu aniversário seja esquecido!",
-	category: "Interativo",
-	usage: "aniversario 17/01",
+    category: "Interativo",
+    usage: "aniversario 17/01",
     code: `
 <@$authorID> | Sucesso! Lembrarei o seu aniversário para o dia **$message[1]**.
 $setUserVar[birthday;$message[1];$authorID;$guildID]
+
 $onlyIf[$isValidDate[$splitText[2]/$splitText[1]/2026]==true;<@$authorID> | Data inválida! Use o formato \`DD/MM\` (Ex: 21/03).]
 $textSplit[$message[1];/]
 
