dde041d3410ef1bdbf2a7a939f0ffd9de2b2d069
WillSilvah <williamfelipe235@gmail.com>
2026-02-13 18:51:52 -0300

    Perfil

--- comandos/Utilidades/perfil.js
+++ comandos/Utilidades/perfil.js
@@ -5,23 +5,23 @@ module.exports = [{
 	category: "Utilidades",
 	usage: "perfil @user?",
 	code: `
-$title[1;📃 Perfil de $username[$get[userID]]]
+$title[1;📃 Perfil de $username[$findUser[$get[userID]]]]
 $description[1;
 
-$addField[1;🎮 Minigames ganhos;$getUserVar[minigameMonthWins;$get[userID]] (Mês) | $getUserVar[minigameTotalWins;$get[userID]] (total)]
+$addField[1;🎮 Minigames ganhos;$getUserVar[minigameMonthWins;$findUser[$get[userID]]] (Mês) | $getUserVar[minigameTotalWins;$findUser[$get[userID]]] (total)]
 
-$addField[1;Votos dados pelo Discords.com;$getUserVar[votesTotal;$get[userID]] (Mês) | $getUserVar[votesMonth;$get[userID]] (Total)]
+$addField[1;Votos dados pelo Discords.com;$getUserVar[votesTotal;$findUser[$get[userID]]] (Mês) | $getUserVar[votesMonth;$findUser[$get[userID]]] (Total)]
 
-$addField[1;Atividade do membro (histórico);✨ $getUserVar[msgXPtotal;$get[userID]] | 💬 $getUserVar[messageTotal;$get[userID]]
+$addField[1;Atividade do membro (histórico);✨ $getUserVar[msgXPtotal;$findUser[$get[userID]]] | 💬 $getUserVar[messageTotal;$findUser[$get[userID]]]
 
-$addField[1;Atividade do membro (temporada);✨ $getUserVar[msgXP;$get[userID]] | 💬 $getUserVar[messageMonth;$get[userID]]
+$addField[1;Atividade do membro (temporada);✨ $getUserVar[msgXP;$findUser[$get[userID]]] | 💬 $getUserVar[messageMonth;$findUser[$get[userID]]]
 
 ]
 $footer[1;$get[badges]]
-$thumbnail[1;$userAvatar[$get[userID]]]
+$thumbnail[1;$userAvatar[$findUser[$get[userID]]]]
 $color[1;Random]
 
-$let[badges;$if[$hasRoles[$guildID;$get[userID];$getGuildVar[memberSupporterRole]]==true;Apoiador | ;]$if[$hasRoles[$guildID;$get[userID];$getGuildVar[memberVerifiedRole]]==true;Verificado | ;]$if[$hasRoles[$guildID;$get[userID];$getGuildVar[allStaffRole]]==true;Equipe Patinhas | ;]$if[$hasRoles[$guildID;$get[userID];$getGuildVar[memberActiveRole]]==true;Membro Ativo;]]
+$let[badges;$if[$hasRoles[$guildID;$findUser[$get[userID]];$getGuildVar[memberSupporterRole]]==true;Apoiador | ;]$if[$hasRoles[$guildID;$findUser[$get[userID]];$getGuildVar[memberVerifiedRole]]==true;Verificado | ;]$if[$hasRoles[$guildID;$findUser[$get[userID]];$getGuildVar[allStaffRole]]==true;Equipe Patinhas | ;]$if[$hasRoles[$guildID;$findUser[$get[userID]];$getGuildVar[memberActiveRole]]==true;Membro Ativo;]]
 
 $let[userID;$ifAwaited[$findUser[$message[1]]==;$message[1];$findUser[$message[1]]]
 `
