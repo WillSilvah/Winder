8161f232e44d638cf72fda19de0e4b5b2d35b1b3
WillSilvah <williamfelipe235@gmail.com>
2026-02-12 21:45:14 -0300

    aniversário

--- comandos/Utilidades/aniversario.js
+++ comandos/Utilidades/aniversario.js
@@ -1,35 +1,44 @@
 module.exports = [{
     name: "aniversario",
-    aliases: ["set-niver", "setniver"],
+    aliases: ["set-niver", "setniver", "aniversário"],
     desc: "Não deixe que seu aniversário seja esquecido!",
     category: "Interativo",
     usage: "aniversario 17/01",
     code: `
-$sendMessage[<@$authorID> | Sucesso! Lembrarei o seu aniversário para o dia **$formatDate[$message[1];D/M]**.]
+$sendMessage[<@$authorID> | Sucesso! Lembrarei o seu aniversário para o dia **$formatDate[$message[1];DD/MM]**.]
 $clientTyping
 
-$setUserVar[birthday;$formatDate[$message[1];D/M];$authorID;$guildID]
+$setUserVar[birthday;$formatDate[$message[1];DD/MM];$authorID;$guildID]
+
 
 $onlyIf[$isNumber[$splitText[1]]$isNumber[$splitText[2]]==truetrue;<@$authorID> | Data inválida! Use apenas números no formato \`DD/MM\`.]
-$onlyIf[$charCount[$message[1]]==5;<@$authorID> | Formato incorreto! Use \`DD/MM\` (Ex: 04/09).]
+$onlyIf[$charCount[$message[1]]==5;<@$authorID> | Formato incorreto! Use \`DD/MM\` (Ex: 17/01).]
 $textSplit[$message[1];/]
+
+$timezone[America/Recife]
 
 $onlyIf[$argsCount>0;]
 $ifAwaited[$argsCount==0;{execute:cmdinfo-await}]
 $let[cmdName;$commandName]
 `
 },{
-    name: "aniversarios",
-    aliases: ["proximos-niver", "niverlista"],
+    name: "aniversários",
+    aliases: ["proximos-niver", "niverlista", "aniversarios"],
     desc: "Veja a lista de todos os aniversários registrados!",
     category: "Interativo",
     usage: "aniversarios",
     code: `
-$editMessage[$get[msgId];{newEmbed:{title:🗓️ Lista de Aniversários}{description:$if[$getVar[temp_lista]==;❌ Nenhum aniversário registrado.;$getVar[temp_lista]]}{color:#FFC0CB}{footer:Sistema de Aniversários do Winder}}]
+$editMessage[$get[msgId];<@$authorID> {newEmbed:
+{title:🗓️ Lista de Aniversários}
+{description:$if[$getVar[temp_lista]==;❌ Nenhum aniversário registrado.;$getVar[temp_lista]]}
+{color:Red}
+{footer:Utilize $getGuildVar[prefixo]aniversário DD/MM para adicionar o seu aniversário!}}]
 $wait[2s]
 $forEachUser[1;{};returnAniversarios;]
 $setVar[temp_lista;]
-$let[msgId;$sendMessage[🔍 Vasculhando registros... Aguarde.;true]]
+$let[msgId;$sendMessage[<@$authorID> 🔍 Procurando aniversários... Aguarde.;true]]
+
+$clientTyping
 
 $onlyIf[$argsCount==0;]
 $ifAwaited[$argsCount!=0;{execute:cmdinfo-await}]
