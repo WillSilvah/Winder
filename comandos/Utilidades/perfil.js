8ebcff083947acd40f9b589a4464a1b91519caba
WillSilvah <williamfelipe235@gmail.com>
2026-02-13 18:13:21 -0300

    Perfil

--- comandos/Utilidades/perfil.js
+++ comandos/Utilidades/perfil.js
@@ -9,12 +9,16 @@ $title[1;📃 Perfil de $username[$get[userID]]]
 $description[1;
 $get[badges]
 
-$addField[1;Atividade do membro (temporada);✨]
+$addField[1;Votos dados pelo [Discords.com](https://discords.com/servers/$guildID/upvote);✨ $getUserVar[msgXPtotal;$get[userID]] | 💬 $getUserVar[messageTotal;$get[userID]]
+
+$addField[1;Atividade do membro (histórico);✨ $getUserVar[msgXPtotal;$get[userID]] | 💬 $getUserVar[messageTotal;$get[userID]]
+
+$addField[1;Atividade do membro (temporada);✨ $getUserVar[msgXP;$get[userID]] | 💬 $getUserVar[messageMonth;$get[userID]]
 
 
 ]
 
-$let[badges;$if[$hasRoles[$guildID;$get[userID];$getGuildVar[memberSupporterRole]]==true;:handshake: **Apoiador** ;]$if[$hasRoles[$guildID;$get[userID];$getGuildVar[memberVerifiedRole]]==true;$customEmoji[pats_foxThumbsUp] **Verificado** ;]$if[$hasRoles[$guildID;$get[userID];$getGuildVar[allStaffRole]]==true;:shield: **Equipe Patinhas** ;]]
+$let[badges;$if[$hasRoles[$guildID;$get[userID];$getGuildVar[memberSupporterRole]]==true;🤝 **Apoiador** ;]$if[$hasRoles[$guildID;$get[userID];$getGuildVar[memberVerifiedRole]]==true;$customEmoji[pats_foxThumbsUp] **Verificado** ;]$if[$hasRoles[$guildID;$get[userID];$getGuildVar[allStaffRole]]==true;🛡 **Equipe Patinhas** ;]$if[$hasRoles[$guildID;$get[userID];$getGuildVar[memberActiveRole]]==true;💬 **Membro ativo** ;]]
 $let[userID;$ifAwaited[$findUser[$message[1]]==;$message[1];$findUser[$message[1]]]
 `
 
