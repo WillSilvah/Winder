a06ad8a5fba33fce3476c3c93c3e07d50b13a368
WillSilvah <williamfelipe235@gmail.com>
2026-02-13 07:07:40 -0300

    média

---
+++ comandos/Desenvolvedor/media.js
@@ -0,0 +1,24 @@
+module.exports = [{
+name: "pda media",
+aliases: ['media pda'],
+desc: "Veja a média de PDA!",
+category: "Desenvolvedor",
+usage: "eval",
+code: `
+$sendMessage[<@$authorID> | Temporada: **$get[temppda]** (PDA)
+<@$authorID> | Temporada: **$get[tempmsgsmedia]** (Mensagens)
+]
+
+
+$let[tempmsgsmedia;$truncate[$math[$get[tempsomamsgs]/$membersCount[$guildID;all;false]]]]
+
+$let[tempsomamsgs;$math[$replaceText[$replaceText[$userLeaderBoard[$guildID;messageMonth;desc;{value},;$membersCount[$guildID;all;false];1;main];,;+];+
+;+]0]]
+
+
+$let[temppda;$truncate[$math[$get[temppdasoma]/$membersCount[$guildID;all;false]]]]
+
+$let[temppdasoma;$math[$replaceText[$replaceText[$userLeaderBoard[$guildID;msgXP;desc;{value},;$membersCount[$guildID;all;false];1;main];,;+];+
+;+]0]]
+`
+}]
