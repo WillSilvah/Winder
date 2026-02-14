899e687fd20be5628666bd7dbababc135712d510
WillSilvah <williamfelipe235@gmail.com>
2026-02-13 22:17:54 -0300

    Aniversario no perfil

--- comandos/Utilidades/perfil.js
+++ comandos/Utilidades/perfil.js
@@ -5,25 +5,24 @@ module.exports = [{
 	category: "Utilidades",
 	usage: "perfil @user?",
 	code: `
-$title[1;📃 Perfil de $username[$findUser[$get[userID];true]]]
+$title[1;📃 Perfil de $username[$mentioned[1]]]
 $description[1;
-$if[$getUserVar[birthday;$findUser[$get[userID];true];$guildID]!=;🎂 **$getUserVar[birthday;$findUser[$get[userID];true];$guildID]/$year**;🎂 Utilize **$getGuildVar[prefixo]aniversário** para registrar uma data!]
+$if[$getUserVar[birthday;$mentioned[1];$guildID]!=;🎂 **$getUserVar[birthday;$mentioned[1];$guildID]/$year**;🎂 Utilize **$getGuildVar[prefixo]aniversário** para registrar uma data!]
 
-$addField[1;🎮 Minigames ganhos;$getUserVar[minigameMonthWins;$findUser[$get[userID];true]] (Mês) | $getUserVar[minigameTotalWins;$findUser[$get[userID];true]] (total)]
+$addField[1;🎮 Minigames ganhos;$getUserVar[minigameMonthWins;$mentioned[1]] (Mês) | $getUserVar[minigameTotalWins;$mentioned[1]] (total)]
 
-$addField[1;Votos dados pelo Discords.com;$getUserVar[votesTotal;$findUser[$get[userID];true]] (Mês) | $getUserVar[votesMonth;$findUser[$get[userID];true]] (Total)]
+$addField[1;Votos dados pelo Discords.com;$getUserVar[votesTotal;$mentioned[1]] (Mês) | $getUserVar[votesMonth;$mentioned[1]] (Total)]
 
-$addField[1;Atividade do membro (histórico);✨ $getUserVar[msgXPtotal;$findUser[$get[userID];true]] | 💬 $getUserVar[messageTotal;$findUser[$get[userID];true]]
+$addField[1;Atividade do membro (histórico);✨ $getUserVar[msgXPtotal;$mentioned[1]] | 💬 $getUserVar[messageTotal;$mentioned[1]]
 
-$addField[1;Atividade do membro (temporada);✨ $getUserVar[msgXP;$findUser[$get[userID];true]] | 💬 $getUserVar[messageMonth;$findUser[$get[userID];true]]
+$addField[1;Atividade do membro (temporada);✨ $getUserVar[msgXP;$mentioned[1]] | 💬 $getUserVar[messageMonth;$mentioned[1]]
 
 ]
 $footer[1;$replaceText[$get[badges]END; | END;;1]]
-$thumbnail[1;$userAvatar[$findUser[$get[userID];true]]]
+$thumbnail[1;$userAvatar[$mentioned[1]]]
 $color[1;Random]
 
-$let[badges;$if[$hasRoles[$guildID;$get[userID];$getGuildVar[memberSupporterRole]]==true;🤝 Apoiador | ;]$if[$hasRoles[$guildID;$get[userID];$getGuildVar[memberVerifiedRole]]==true;✅ Verificado | ;]$if[$hasRoles[$guildID;$get[userID];$getGuildVar[allStaffRole]]==true;🛡️ Equipe Patinhas | ;]$if[$hasRoles[$guildID;$get[userID];$getGuildVar[memberActiveRole]]==true;💬 Membro Ativo | ;]]
-$let[userID;$findUser[$message[1];true]]
+$let[badges;$if[$hasRoles[$guildID;$mentioned[1];$getGuildVar[memberSupporterRole]]==true;🤝 Apoiador | ;]$if[$hasRoles[$guildID;$mentioned[1];$getGuildVar[memberVerifiedRole]]==true;✅ Verificado | ;]$if[$hasRoles[$guildID;$mentioned[1];$getGuildVar[allStaffRole]]==true;🛡️ Equipe Patinhas | ;]$if[$hasRoles[$guildID;$mentioned[1];$getGuildVar[memberActiveRole]]==true;💬 Membro Ativo | ;]]
 
 $timezone[America/Recife]
 `
