8038e310b52fc3c63341a88bc8785a91527bd2ad
WillSilvah <williamfelipe235@gmail.com>
2026-02-14 08:03:36 -0300

    Aniversario

---
+++ comandos/WinderEventos/Rotinas/birthday.js
@@ -0,0 +1,45 @@
+module.exports = [{
+    name: "niver-loop",
+    type: "loop",
+    every: "999",
+    executeOnStartup: "true",
+    code: `
+$forEachUser[5;{};verificar_aniversariantes;]
+
+$onlyIf[$hour:$minute:$second==07:00:00;] 
+$onlyIf[$getGuildVar[birthdayChannel]!=;]
+$timezone[America/Recife]
+`
+},{
+	name: "verificar_aniversariantes",
+    type: "awaited",
+    code: `
+$channelSendMessage[$getGuildVar[batePapo;$guildID];🎂 | **PARABÉNS!** <@$authorID>, hoje é seu dia! Muitas felicidades! 🎉]
+
+$messagePublish[$get[birthMSG];$getGuildVar[birthdayChannel;$guildID]]
+
+$let[birthMSG;$channelSendMessage[$getGuildVar[birthdayChannel;$guildID];{newEmbed:
+{title:🎉 FELIZ ANIVERSÁRIO!}
+{description:
+Hoje é o aniversário do <@$authorID>! Vamos dar parabéns a ele? 
+}
+{footer:Adicione o seu aniversário com o comando $getGuildVar[prefixo]aniversário}
+{thumbnail:$userAvatar[$authorID]}
+{color:Random}
+
+}]]
+
+
+$onlyIf[$formatDate[$getUserVar[birthday;$authorID;$guildID];DD/MM]==$formatDate[$dateStamp;DD/MM];]
+$onlyIf[$isBot==false;]
+$timezone[America/Recife]
+
+`
+},{
+	name: "onDeleteBirthday",
+	type: "memberLeave",
+    code: `
+$deleteVar[birthday;$authorID;main]
+$log[Registro de aniversário deletado: Usuário "$username" ($authorID) saiu.]
+`
+}]
--- comandos/WinderEventos/onDeleteBirthday.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({
-    type: "memberLeave",
-    code: `
-$deleteVar[birthday;$authorID;main]
-$log[Registro de aniversário deletado: Usuário "$username" ($authorID) saiu.]
-`
-})
