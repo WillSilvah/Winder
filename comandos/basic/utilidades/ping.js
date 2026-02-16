9e9ad31a271b6a885e90168db34213b51d85926e
WillSilvah <williamfelipe235@gmail.com>
2026-02-15 23:34:33 -0300

    Ajustando nomes de pastas para minúsculo

---
+++ comandos/basic/utilidades/ping.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "ping",
+    desc: "Quanto tempo o bot responde?",
+category: "Informação",
+usage: "ping",
+    type: "messageCreate",
+    code: `
+<@$authorID> | $username[$clientID] Ping: **$pingms**
+`
+}
