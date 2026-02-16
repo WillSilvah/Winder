37fc850e1b2ca7f4f238340e0328347cbeebf940
WillSilvah <williamfelipe235@gmail.com>
2026-02-15 22:58:32 -0300

    Primeiros comandos

--- comandos/slash/Utilidades/ping.js
+++ comandos/slash/Utilidades/ping.js
@@ -3,14 +3,8 @@ module.exports = {
     Ping: \`$pingMS\` | Uptime: <t:$round[$math[$math[$getTimestamp-$uptime]/1000];0]:R>
   `,
 data: {
-    "type": 1,
-    "name": "ping",
-    "description": "Quanto tempo o Winder responde?",
-    "integration_types": [
-    0
-    ],
-    "contexts": [
-    0
-    ]
-  },
+  "type": 1,
+  "name": "ping",
+  "description": "Quanto tempo o Winder responde?"
+}
 }
