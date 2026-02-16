b067785913177efcb71acb7ef60bfa12c2a3c8dd
Williams Felipe <49321188+WillSilvah@users.noreply.github.com>
2026-02-15 23:20:08 -0300

    Delete comandos directory

--- comandos/basic/Utilidades/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "ping",
-    desc: "Quanto tempo o bot responde?",
-category: "Informação",
-usage: "ping",
-    type: "messageCreate",
-    code: `
-<@$authorID> | $username[$clientID] Ping: **$pingms**`
-}
