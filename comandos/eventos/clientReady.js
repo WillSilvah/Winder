a30c156f8f37034deee0ce968bcd4a22d2f3c2dc
WillSilvah <williamfelipe235@gmail.com>
2026-02-16 20:35:55 -0300

    ClientReady and ForgeCron

--- comandos/eventos/clientReady.js
+++ comandos/eventos/clientReady.js
@@ -3,10 +3,10 @@ module.exports = {
     code: `
 $setGlobalVar[version;$username[$clientID] $day/$month]
 $sendMessage[$getGlobalVar[consoleChat];
-$author[1;$username[$clientID] está ligado!]
+$author[$username[$clientID] está ligado!]
 $description[
-$addField[1;Versão do ForgeScript;$version;true]
-$addField[1;Versão do NodeJS;$nodeVersion;true]
+$addField[Versão do ForgeScript;$version;true]
+$addField[Versão do NodeJS;$nodeVersion;true]
 $addField[Versão do $username[$clientID];$getGlobalVar[version]
 ]
 $thumbnail[$userAvatar[$clientID]]
