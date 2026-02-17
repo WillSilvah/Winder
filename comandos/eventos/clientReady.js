f71e6501ce93b447ede0b31d92bcb0715af0ab62
WillSilvah <williamfelipe235@gmail.com>
2026-02-16 21:06:12 -0300

    ClientReady and ForgeCron

--- comandos/eventos/clientReady.js
+++ comandos/eventos/clientReady.js
@@ -1,7 +1,7 @@
 module.exports = {
     type: "clientReady",
     code: `
-$setGlobalVar[version;$username[$clientID] $day/$month]
+$setGlobalVar[version;$username[$clientID] $day.$month]
 $sendMessage[$getGlobalVar[consoleChat];
 $author[$username[$clientID] está ligado!]
 $description[
