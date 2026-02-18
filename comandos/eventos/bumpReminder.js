95d24ca6c7c8cc91201ee89ff0f6a2652e8469e3
WillSilvah <williamfelipe235@gmail.com>
2026-02-18 19:34:20 -0300

    testes

--- comandos/eventos/bumpReminder.js
+++ comandos/eventos/bumpReminder.js
@@ -3,8 +3,20 @@ type: "messageCreate",
 allowBots: true,
 code: `
 $onlyIf[$authorID==302050872383242240;]
-$onlyIf[$checkContains[$message;Bump done!]]
 $onlyIf[$channelID==1462796515578876079;]
+$onlyIf[$checkContains[$getEmbeds[1462796515578876079;$messageID;0;description];Bump done!]]
+
+$setTimeout[
+$sendMessage[1462796515578876079;<@&1462953076091785370>
+$title[🎉 Hora de dar /bump!]
+$description[
+Hey! Tá na hora de dar boost na Disboard!
+Usa o comando </bump:947088344167366698>
+]
+$color[Green]
+]
+;2h;BumpReminder]
+
 
 `
 }
