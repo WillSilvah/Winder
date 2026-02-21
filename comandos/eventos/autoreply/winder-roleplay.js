4853de9d0ec1341d6833b5653e121142ba7b5940
WillSilvah <williamfelipe235@gmail.com>
2026-02-21 19:28:41 -0300

    Roleplay

--- comandos/eventos/autoreply/winder-roleplay.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    desc: "Quanto tempo o bot responde?",
-category: "Informação",
-usage: "ping",
-    type: "messageCreate",
-    code: `
-<@$authorID> | $username[$clientID] Ping: **$pingms**
-`
-}
