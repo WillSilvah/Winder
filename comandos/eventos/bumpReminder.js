541020fbd84fe70520ae00c7f6dc1773a8eb39bb
WillSilvah <williamfelipe235@gmail.com>
2026-02-21 08:30:10 -0300

    BumpReminder

--- comandos/eventos/bumpReminder.js
+++ comandos/eventos/bumpReminder.js
@@ -6,6 +6,8 @@ $onlyIf[$authorID==302050872383242240;]
 $onlyIf[$channelID==1462796515578876079;]
 $onlyIf[$checkContains[$getEmbeds[1462796515578876079;$messageID;0;description];Bump done!]]
 
+$sendMessage[1462796515578876079;Obrigado por ajudar!]
+
 $setTimeout[
 $sendMessage[1462796515578876079;<@&1462953076091785370>
 $title[🎉 Hora de dar /bump!]
