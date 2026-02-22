77475843dfb535d01f74863558653e28ee430761
WillSilvah <williamfelipe235@gmail.com>
2026-02-22 18:48:58 -0300

    WelcomeMember

--- comandos/eventos/welcomeMember.js
+++ comandos/eventos/welcomeMember.js
@@ -10,7 +10,7 @@ $description[
 $if[$isBot[$authorID]==false;Espero que goste da Patinhas e que você se divirta mais do que eu com uma bola de lã. 🐱 Se liga nas nossas diretrizes e aproveite tudo! 😉;Um novo aplicativo foi adicionado na Patinhas *ruídos de robô pode ser ouvido* Epa! Ele disse alguma coisa? 🤔
 ]
 ]
-$footer[Veio pelo: @$username[$inviterID[$guildID;$authorID]] | ID: $authorID]
+$footer[Veio pelo: $if[$inviterID[$guildID;$authorID]!=;@$username[$inviterID[$guildID;$authorID]];@Fantasma] | ID: $authorID]
 $thumbnail[$userAvatar]
 $color[#020afc]
 ]
