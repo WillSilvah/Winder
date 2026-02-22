081244a140a104b68aeb744e377b9e21c969c83a
WillSilvah <williamfelipe235@gmail.com>
2026-02-22 11:16:37 -0300

    AFK

---
+++ comandos/eventos/afk.js
@@ -0,0 +1,40 @@
+module.exports = [{
+    type: "messageCreate",
+    code: `
+$onlyIf[$mentioned[0]!=$authorID;]
+$onlyIf[$getUserVar[afkStatus;$mentioned[0]]==true;]
+
+$sendMessage[$channelID;<@$authorID> **$userTag[$mentioned[1]]** está afk!
+$author[$getUserVar[afkReason;$mentioned[0]]]
+$color[Red]
+]
+
+$if[$getUserVar[afkNotify;$mentioned[0]]==true;
+$sendDM[$mentioned[0];
+$title[@$username[$authorID] mencionou você enquanto AFK!]
+$description[
+$message
+]
+$footer[Servidor: $guildName[$guildID] | Canal: $channelName[$channelID]]
+$color[Red]
+
+$addActionRow
+$addButton[$messageLink[$channelID;$messageID];Ir para a mensagem;Link;💬;false]
+
+]
+]
+
+`
+},{
+    type: "messageCreate",
+    code: `
+$onlyIf[$getUserVar[afkStatus;$authorID]==true;]
+
+$sendMessage[$channelID;<@$authorID> | Olá, que bom que você voltou! ❤️]
+
+$setUserVar[afkStatus;false;$authorID]
+$setUserVar[afkReason;;$authorID]
+
+    
+`
+}]
