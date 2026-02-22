934c5b0ff6f252a88e9b472568d906ffc24910a0
WillSilvah <williamfelipe235@gmail.com>
2026-02-22 06:57:06 -0300

    LinuxRAM

--- comandos/custom/linuxRAM.js
+++ comandos/custom/linuxRAM.js
@@ -1,4 +1,4 @@
 module.exports = [{
     name: "linuxRam",
-   code: `$exec[]pm2 jlist | jq '.[] | select(.name=="winder-canary") | .monit.memory / 1024 / 1024 | floor' | tr -d '\\n']`
+   code: `$exec[pm2 jlist | jq '.[] | select(.name=="winder-canary") | .monit.memory / 1024 / 1024 | floor' | tr -d '\\n']`
 }]
