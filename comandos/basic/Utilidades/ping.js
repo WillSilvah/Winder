e531f07c04ea4070defd2c3774bed401e85df2a3
WillSilvah <williamfelipe235@gmail.com>
2026-02-22 16:49:04 -0300

    cmdinfo

--- comandos/basic/Utilidades/ping.js
+++ comandos/basic/Utilidades/ping.js
@@ -1,8 +1,9 @@
 module.exports = {
     name: "ping",
     desc: "Quanto tempo o bot responde?",
-category: "Informação",
-usage: "ping",
+    category: "Informação",
+    usage: "ping",
+    slash: "winder ping",
     type: "messageCreate",
     code: `
 <@$authorID> | $username[$clientID] Ping: **$pingms**
--- comandos/basic/desenvolvedor/eval.js
+++ comandos/basic/desenvolvedor/eval.js
@@ -3,7 +3,6 @@ module.exports = {
     desc: "Permite o desenvolvedor executar funções a nível superior",
     category: "Desenvolvedor",
 	usage: "eval code",
-    slash: "winder ping",
     type: "messageCreate",
     code: `
 $onlyIf[$hasRoles[1462224054676099094;$authorID;1462547405466636384]==true;<@$authorID> | Você precisa ser **$username[$clientID]** Developer para poder executar esse comando.]
