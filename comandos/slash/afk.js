6e0b6d9ba991aea5ec3d57dab8e7958d39729267
WillSilvah <williamfelipe235@gmail.com>
2026-02-22 13:13:20 -0300

    Afk

--- comandos/slash/afk.js
+++ comandos/slash/afk.js
@@ -8,5 +8,31 @@ $interactionReply[<@$authorID> Modo AFK ativado!
 > $bold[$if[$option[notificardm]==;Não irei avisar as menções na DM.;Irei avisar as menções nas DM!]]
 ]
   `,
-data: ,
+data: {
+  "type": 1,
+  "description": "Ative o modo afk para que os outros sejam avisados ao mencionar você.",
+  "name": "afk",
+  "options": [
+    {
+      "type": 3,
+      "description": "Motivo que você vai ficar AFK.",
+      "name": "motivo"
+    },
+    {
+      "type": 3,
+      "description": "Deseja que o Winder envie menções recebidas na DM?",
+      "name": "notificardm",
+      "choices": [
+        {
+          "value": "true",
+          "name": "Avisar menções na DM"
+        },
+        {
+          "name": "Não avisar menções na DM",
+          "value": "false"
+        }
+      ]
+    }
+  ]
+},
 }
