c8943fe9ac0bff1b93dfe191982b02312299ea78
WillSilvah <williamfelipe235@gmail.com>
2026-02-23 17:59:21 -0300

    Minigames

--- comandos/custom/minigame.js
+++ comandos/custom/minigame.js
@@ -25,8 +25,37 @@ $description[
 $footer[$env[frase;autor]]
 $color[Green]
 
-$setGuildVar[minigameType;fraseRepeat;$guildID]
+$setGuildVar[minigameType;$env[tipo];$guildID]
 $setGuildVar[minigameStatus;true;$guildID]
 
 ]
+
+$if[$env[tipo]==questions;
+
+$let[id;$randomNumber[1;$exec[ls -1 Recursos/WinderMinigames/Questions/asks/ | wc -l]]]
+
+
+$jsonLoad[ask;$readFile[Recursos/WinderMinigames/Questions/asks/$get[id].txt]]
+$jsonLoad[alt;$env[ask;alts]]
+
+
+$author[Evento de chat: Perguntas;https://abs.twimg.com/emoji/v2/72x72/1f389.png]
+$description[
+### $toUpperCase[$env[ask;pergunta] ($env[ask;difficulty])]
+$if[$env[alt;a]!=undefined;A: $env[alt;a]]
+$if[$env[alt;b]!=undefined;B: $env[alt;b]]
+$if[$env[alt;c]!=undefined;C: $env[alt;c]]
+$if[$env[alt;d]!=undefined;D: $env[alt;d]]
+$if[$env[alt;e]!=undefined;E: $env[alt;e]]
+]
+$footer[Responda apenas com a letra da resposta! | ID: $get[id]]
+$image[$if[$env[ask;image]==;https://cdn.discordapp.com/attachments/785632865709981756/1465469785813942272/IMG_20260126_191316.png?ex=69793898&is=6977e718&hm=559e450ba67a6a132a64f475bb1781cce2aa4ebe4f046a39c5fc3dd303382591&;$env[ask;image]]]
+$color[Green]
+
+$setGuildVar[minigameWord;$env[ask;correct];$guildID]
+
+$setGuildVar[minigameType;$env[tipo];$guildID]
+$setGuildVar[minigameStatus;true;$guildID]
+]
+
 `}]
