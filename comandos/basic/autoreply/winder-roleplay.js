5afc58de8ae79a0c38a5fdfa8f572bea2c6d335d
WillSilvah <williamfelipe235@gmail.com>
2026-02-23 17:59:39 -0300

    Minigames

--- comandos/basic/autoreply/winder-roleplay.js
+++ comandos/basic/autoreply/winder-roleplay.js
@@ -2,7 +2,7 @@ module.exports = {
     type: "messageCreate",
     code: `
 $cooldown[$guildID;10m]
-$onlyIf[$randomNumber[1;10]==5;]
+$onlyIf[$randomNumber[1;15]==5;]
 $reply
 $randomText[$randomText[Miau;Meow;Meooww;Miaauu]! :3;*lambe você* >w<;*abraça você* >w<;*sniff sniff* 👃 $randomText[Vai tomar banho!!; Está de banho tomado, parabéns!;Que cheiro é esse?];*se esfregando em suas pernas e ronrona* UwU;*boop! OwO*;*morde você* >:3;*pat pat* OwO;*cafuné* OwO;*morde você* UwU;*boop* $emoji[$emojiID[pats_foxShy]]]
 
---
+++ comandos/custom/minigame.js
@@ -0,0 +1,53 @@
+module.exports = [{
+    type: 'clientReady',
+    code: `
+$setInterval[
+$onlyIf[$getGuildVar[msgPerMinute;$guildID]>=6;]
+
+$minigame[fraseRepeat]
+
+$wait[1m]
+$onlyIf[$getGuildVar[minigameStatus;$guildID]==true;]
+
+$setGuildVar[minigameStatus;false;$guildID]
+$setGuildVar[minigameWord;;$guildID]
+
+$setGuildVar[minigameXPmin;0;$guildID]
+$setGuildVar[minigameXPmax;0;$guildID]
+$setChannelSlowmode[$getGuildVar[batePapo];0]
+$sendMessage[### ⛔️ EVENTO DE CHAT FOI CANCELADO!]
+
+;15m;WinderMinigame]
+`
+},{
+    type: 'messageCreate',
+    code: `
+$onlyIf[$channelID==$getGuildVar[batePapo];]
+$onlyIf[$getGuildVar[minigameStatus;$guildID]==true;]
+$onlyIf[$message==$getGuildVar[minigameWord;$guildID];]
+$startTyping[$channelID]
+$setGuildVar[minigameStatus;false;$guildID]
+$setGuildVar[minigameWord;;$guildID]
+
+$let[xpDrop;$if[$hasRoles[$guildID;$authorID;$getGuildVar[memberVerifiedRole]]==true;$randomNumber[$getGuildVar[minigameXPmin;$guildID];$getGuildVar[minigameXPmax;$guildID]];0]]
+
+$setMemberVar[pdaMonth;$sum[$getMemberVar[pdaMonth;$authorID];$get[xpDrop]];$authorID]
+$setMemberVar[pdaTotal;$sum[$getMemberVar[pdaTotal;$authorID];$get[xpDrop]];$authorID]
+
+$!addMessageReactions[$channelID;$messageID;🏆]
+
+$setMemberVar[minigameMonthWins;$sum[$getMemberVar[minigameMonthWins;$authorID];1];$authorID]
+$setMemberVar[minigameTotalWins;$sum[$getMemberVar[minigameTotalWins;$authorID];1];$authorID]
+
+$sendMessage[$channelID;<@$authorID> | Parabéns, você ganhou ✨**+$get[xpDrop] Pontos de atividade!**!
+> **Ganhou 🏆$getMemberVar[minigameMonthWins;$authorID] vezes neste mês!**
+]
+
+$!memberRemoveRoles[$guildID;$getGuildVar[minigameLastUser;$guildID];$getGuildVar[lastWinMemberRole]]
+$wait[1s]
+$!memberAddRoles[$guildID;$authorID;$getGuildVar[lastWinMemberRole]]
+$setGuildVar[minigameLastUser;$authorID;$guildID]
+
+$setGuildVar[minigameEmit;false;$guildID]
+`
+}]
