a2dac93a6bec7244621cc587854b51d0ca2da21b
WillSilvah <williamfelipe235@gmail.com>
2026-02-23 11:38:14 -0300

    Minigame

---
+++ comandos/custom/minigame.js
@@ -0,0 +1,27 @@
+module.exports = [{
+    name: "minigame",
+    params: [
+    {
+      name: 'tipo',
+      description: "Tipo do minigame a ser executado",
+      required: true
+    }
+    ],
+    code: `
+$if[$env[tipo]==fraseRepeat;
+$let[frase_id;$randomNumber[1;$exec[ls -1 Recursos/WinderMinigames/FraseRepeat/Frases/ | wc -l]]]
+
+$jsonLoad[frase;$readFile[Recursos/WinderMinigames/FraseRepeat/Frases/$get[frase_id].txt]]
+
+$author[QUEM ESCREVER PRIMEIRO GANHA!;https://abs.twimg.com/emoji/v2/72x72/1f389.png]
+$description[
+### $replaceText[$env[frase;texto]; ;‌‌‌‌‌]
+‌
+]
+$footer[$env[frase;autor]]
+$color[Green]
+
+$setGuildVar[minigameWord;$env[frase;texto];$guildID]
+
+]
+`}]
