601d2c755ef0b8da651332f5367563066a6fc657
WillSilvah <williamfelipe235@gmail.com>
2026-02-24 20:12:53 -0300

    PdaPorCall

---
+++ comandos/eventos/pdaCallCount.js
@@ -0,0 +1,48 @@
+module.exports = [{
+    type: 'clientReady',
+    code: `
+$setInterval[
+$arrayLoad[teste;, ;$channelVoiceMemberIDs[1462603953077358791]]
+$arrayForEach[teste;id;
+$onlyIf[$hasRoles[1462224054676099094;$authorID;$getGuildVar[memberVerifiedRole]]==true;]
+$onlyIf[$channelVoiceMemberCount[$voiceID[$guildID;$env[id]]]>1;]
+
+$let[pda;$if[$isMuted[$guildID;$env[id]]==true;0;$randomNumber[1;5]]]
+
+$setMemberVar[pdaMonth;$sum[$getMemberVar[pdaMonth;$env[id]];$get[pda]];$env[id]]
+$setMemberVar[pdaTotal;$sum[$getMemberVar[pdaTotal;$env[id]];$get[pda]];$env[id]]
+
+
+
+];5m;PDA_musica]
+
+$setInterval[
+$arrayLoad[teste;, ;$channelVoiceMemberIDs[1462615093153108001]]
+$arrayForEach[teste;id;
+$onlyIf[$hasRoles[1462224054676099094;$authorID;$getGuildVar[memberVerifiedRole]]==true;]
+$onlyIf[$channelVoiceMemberCount[$voiceID[$guildID;$env[id]]]>1;]
+
+$let[pda;$if[$isMuted[$guildID;$env[id]]==true;0;$randomNumber[1;5]]]
+
+$setMemberVar[pdaMonth;$sum[$getMemberVar[pdaMonth;$env[id]];$get[pda]];$env[id]]
+$setMemberVar[pdaTotal;$sum[$getMemberVar[pdaTotal;$env[id]];$get[pda]];$env[id]]
+
+];5m;PDA_jogos]
+
+$setInterval[
+$arrayLoad[teste;, ;$channelVoiceMemberIDs[1462224055884189785]]
+$arrayForEach[teste;id;
+$onlyIf[$hasRoles[1462224054676099094;$authorID;$getGuildVar[memberVerifiedRole]]==true;]
+$onlyIf[$channelVoiceMemberCount[$voiceID[$guildID;$env[id]]]>1;]
+
+$let[pda;$if[$isMuted[$guildID;$env[id]]==true;0;$randomNumber[1;5]]]
+
+$setMemberVar[pdaMonth;$sum[$getMemberVar[pdaMonth;$env[id]];$get[pda]];$env[id]]
+$setMemberVar[pdaTotal;$sum[$getMemberVar[pdaTotal;$env[id]];$get[pda]];$env[id]]
+
+
+];5m;PDA_batePapo]
+    
+    
+`
+}]
