b6c495a3cfd996aae872e534a0695269f0a46ad9
Williams Felipe <49321188+WillSilvah@users.noreply.github.com>
2026-02-24 10:09:49 -0300

    Update clientReady.js

--- comandos/eventos/clientReady.js
+++ comandos/eventos/clientReady.js
@@ -17,10 +17,10 @@ $username[$clientID] está ligado! >w<
 ---------------
 ]
 
-$setStatus[Idle;Playing;$username[$clientID] esta ligado! >w<]
+$setStatus[Online;Playing;$username[$clientID] esta ligado! >w<]
 
 $setInterval[
-$setStatus[Idle;Playing;$parseInt[$ram] MB | $cpu% | $parseDigital[$uptime] | $pingms | $getGlobalVar[version]]
+$setStatus[Online;Playing;$parseInt[$ram] MB | $cpu% | $parseDigital[$uptime] | $pingms | $getGlobalVar[version]]
 
 ;15s;ClientStatus]
 `,
