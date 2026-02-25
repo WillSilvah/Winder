80e3869d4eeb38919847b89f2ed98bc57d6b9225
Williams Felipe <49321188+WillSilvah@users.noreply.github.com>
2026-02-25 11:24:14 -0300

    Update pdaCallCount.js

--- comandos/eventos/pdaCallCount.js
+++ comandos/eventos/pdaCallCount.js
@@ -2,9 +2,12 @@ module.exports = {
     type: "clientReady",
     code: `
     $log[PDAPORCALL EXECUTADO]
-    
-$cron[$arrayLoad[teste;, ;$channelVoiceMemberIDs[1462603953077358791]]
-$arrayForEach[teste;id;
+$arrayLoad[musica;, ;$channelVoiceMemberIDs[1462603953077358791]]
+$arrayLoad[jogos;, ;$channelVoiceMemberIDs[1462615093153108001]]
+$arrayLoad[batepapo;, ;$channelVoiceMemberIDs[1462224055884189785]]
+
+$cron[
+$arrayForEach[musica;id;
 $onlyIf[$hasRoles[1462224054676099094;$authorID;$getGuildVar[memberVerifiedRole]]==true;$log[$username[$env[id]] não tem verificado]]
 $onlyIf[$channelVoiceMemberCount[$voiceID[$guildID;$env[id]]]>1;]
 
@@ -17,8 +20,8 @@ $log[$username[$env[id]]: Ganhou $get[pda] no $channelName[$voiceID[$guildID;$en
 
 ];* * * * *]
 
-$cron[$arrayLoad[teste;, ;$channelVoiceMemberIDs[1462615093153108001]]
-$arrayForEach[teste;id;
+$cron[
+$arrayForEach[jogos;id;
 $onlyIf[$hasRoles[1462224054676099094;$authorID;$getGuildVar[memberVerifiedRole]]==true;$log[$username[$env[id]] não tem verificado]]
 $onlyIf[$channelVoiceMemberCount[$voiceID[$guildID;$env[id]]]>1;]
 
@@ -31,8 +34,8 @@ $log[$username[$env[id]]: Ganhou $get[pda] no $channelName[$voiceID[$guildID;$en
 
 ];* * * * *]
 
-$cron[$arrayLoad[teste;, ;$channelVoiceMemberIDs[1462224055884189785]]
-$arrayForEach[teste;id;
+$cron[
+$arrayForEach[batepapo;id;
 $onlyIf[$hasRoles[1462224054676099094;$authorID;$getGuildVar[memberVerifiedRole]]==true;$log[$username[$env[id]] não tem verificado]]
 $onlyIf[$channelVoiceMemberCount[$voiceID[$guildID;$env[id]]]>1;]
 
