ef21a4ad8e1711ad81991af9671c98d58629efdd
WillSilvah <williamfelipe235@gmail.com>
2026-02-25 22:11:59 -0300

    perfil

--- comandos/slash/perfil.js
+++ comandos/slash/perfil.js
@@ -11,11 +11,17 @@ $if[$getMemberVar[birthday;$get[userID];$guildID]!=;🎂 **$getMemberVar[birthda
 
 $addField[🎮 Minigames ganhos;$getMemberVar[minigameMonthWins;$get[userID]] (Mês) | $getMemberVar[minigameTotalWins;$get[userID]] (total)]
 
-$addField[Votos dados pelo Discords.com;$getMemberVar[votesTotal;$get[userID]] (Mês) | $getMemberVar[votesMonth;$get[userID]] (Total)]
+$addField[⭐ Votos dados pelo Discords.com;$getMemberVar[votesTotal;$get[userID]] (Mês) | $getMemberVar[votesMonth;$get[userID]] (Total)]
 
-$addField[Atividade do membro (histórico);✨ $getMemberVar[pdaTotal;$get[userID]] | 💬 $getMemberVar[messageTotal;$get[userID]]]
+$addField[📃 Atividade do membro;✨ $getMemberVar[pdaMonth;$get[userID]]
+💬 $getMemberVar[messageMonth;$get[userID]] (Mês)
+💬 $getMemberVar[messageWeekly;$get[userID]] (Semana)
+💬 $getMemberVar[messageToday;$get[userID]] (Hoje)
+]
 
-$addField[Atividade do membro (temporada);✨ $getMemberVar[pdaMonth;$get[userID]] | 💬 $getMemberVar[messageMonth;$get[userID]]]
+$addField[Historicamente;
+✨ $getMemberVar[pdaTotal;$get[userID]] (Total) | 💬 $getMemberVar[messageTotal;$get[userID]] (Total)
+]
 
 ]
 $footer[$replaceText[$get[badges]END; | END;;1]]
