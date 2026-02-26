7accbea27d37ec73ea3eca36a47f25f1585d0154
WillSilvah <williamfelipe235@gmail.com>
2026-02-25 21:55:06 -0300

    demograficos

--- comandos/eventos/messageCount.js
+++ comandos/eventos/messageCount.js
@@ -6,6 +6,8 @@ $onlyIf[$isBot==false;]
 $timezone[America/Recife]
 $setGuildVar[guildMonthMessages;$sum[$getGuildVar[guildMonthMessages];1]]
 $setGuildVar[guildTotalMessages;$sum[$getGuildVar[guildTotalMessages];1]]
+$setGuildVar[guildWeeklyMessages;$sum[$getGuildVar[guildWeeklyMessages];1]]
+$setGuildVar[guildTodayMessages;$sum[$getGuildVar[guildTodayMessages];1]]
 
 $setGuildVar[lastUserSendMessage;$authorID]
 $setMemberVar[lastMessage;$message;$authorID]
@@ -13,12 +15,13 @@ $setMemberVar[lastMessageTime;$hour:$minute:$second - $day/$month/$year]
 $setMemberVar[lastMessageChannel;$channelID]
 
 $setMemberVar[messageMonth;$sum[$getMemberVar[messageMonth;$authorID];1]]
+$setMemberVar[messageWeekly;$sum[$getMemberVar[messageWeekly;$authorID];1]]
+$setMemberVar[messageToday;$sum[$getMemberVar[messageToday;$authorID];1]]
 $setMemberVar[messageTotal;$sum[$getMemberVar[messageTotal;$authorID];1]]
 
 $if[$channelID==1462224055884189781;
 $setGuildVar[msgPerMinute;$sum[$getGuildVar[msgPerMinute];1]]
 ]
 
-`,
-nonprefixed: true
+`
 }
--- comandos/eventos/moments.js
+++ comandos/eventos/moments.js
@@ -10,8 +10,17 @@ $deleteMemberVar[msgXP]
 $deleteGuildVar[membersJoinedMonth]
 $deleteGuildVar[guildMonthMessages]
 ;0 0 1 * *;America/Recife]
- 
- `
+
+$cron[
+$deleteGuildVar[guildWeeklyMessage]
+$deleteMemberVar[messageWeekly]
+;0 0 * * 0]
+
+$cron[
+$deleteGuildVar[guildTodayMessage]
+$deleteMemberVar[messageToday]
+;0 0 * * *]
+`
 },{
     type: 'clientReady',
     code: `
