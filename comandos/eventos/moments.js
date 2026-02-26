f8d550ec2ecb98904307eb9bdf7b2b3d49662edb
WillSilvah <williamfelipe235@gmail.com>
2026-02-25 21:54:51 -0300

    demograficos

--- comandos/eventos/moments.js
+++ comandos/eventos/moments.js
@@ -1,8 +1,8 @@
 module.exports = {
- type: 'clientReady',
- code: `
- $cron[
- $deleteMemberVar[metaXP]
+    type: 'clientReady',
+    code: `
+$cron[
+$deleteMemberVar[metaXP]
 $deleteMemberVar[messageMonth]
 $deleteMemberVar[votesMonth]
 $deleteMemberVar[msgXP]
@@ -10,6 +10,16 @@ $deleteMemberVar[msgXP]
 $deleteGuildVar[membersJoinedMonth]
 $deleteGuildVar[guildMonthMessages]
 ;0 0 1 * *;America/Recife]
+
+$cron[
+$deleteGuildVar[guildWeeklyMessage]
+$deleteMemberVar[messageWeekly]
+;0 0 * * 0]
+
+$cron[
+$deleteGuildVar[guildTodayMessage]
+$deleteMemberVar[messageToday]
+;0 0 * * *]
  
  `
  }
