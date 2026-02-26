414069714c025f0a6d2a287c141c2fa273996220
WillSilvah <williamfelipe235@gmail.com>
2026-02-26 15:45:26 -0300

    Loadbot

--- comandos/basic/desenvolvedor/loadbot.js
+++ comandos/basic/desenvolvedor/loadbot.js
@@ -6,7 +6,7 @@ module.exports = {
     type: "messageCreate",
     code: `
 $onlyIf[$hasRoles[1462224054676099094;$authorID;1462547405466636384]==true;<@$authorID> | Você precisa ser **$username[$clientID]** Developer para poder executar esse comando.]
-$startTyping[$clientTyping]
+$startTyping[$channelID]
 
 $updateApplicationCommands
 $updateCommands
