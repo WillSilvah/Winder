2a6b84bb67f7fa44a5bcc579e6fcdab707c2f1a9
WillSilvah <williamfelipe235@gmail.com>
2026-02-26 20:11:28 -0300

    Rank

--- comandos/slash/rank/historico.js
+++ comandos/slash/rank/historico.js
@@ -5,13 +5,10 @@ $jsonLoad[session;$readFile[Recursos/session.json]]
 $author[$env[session;name];https://abs.twimg.com/emoji/v2/72x72/1f4ac.png]
 $title[Top fofoqueiros históricos | $if[$option[pagina]!=;$option[pagina];1]° Página]
 $description[
-$memberLeaderboard[pdaTotal;$guildID;desc;10;1;\n;data;pos;
-  $return[$env[pos]° $bold[$username[$env[data;id]]] -> ✨$env[data;value]]
-]
-
-$memberLeaderboard[messageTotal;$guildID;desc;10;1;\n;data;pos;
-  $return[$env[pos]° $bold[$username[$env[data;id]]] -> 💬$env[data;value]]
-]
+$memberLeaderboard[messageTotal;$guildID;desc;10;$if[$option[pagina]!=;$option[pagina];1];\n;data;pos;
+$return[$env[pos]° $bold[$username[$env[data;id]]]
+💬$env[data;value] / ✨️ $getMemberVar[pdaTotal;$env[data;id];$get[guildID]] / ⬆️ $getMemberVar[votesTotal;$env[data;id];$get[guildID]]
+]]
 ]
 $footer[Você: 💬$getMemberVar[messageTotal;$authorID] ✨️$getMemberVar[pdaTotal;$authorID] | Servidor: 💬$getGuildVar[guildTotalMessages;$guildID]]
 $color[Blue]
