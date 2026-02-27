f7b628b79ecab1442a27428a7b7caf16132d18bd
Williams Felipe <49321188+WillSilvah@users.noreply.github.com>
2026-02-27 11:14:50 -0300

    Update banir.js

--- comandos/slash/banir.js
+++ comandos/slash/banir.js
@@ -10,7 +10,8 @@ $punishmentlog[$get[userID];$get[staffID];$get[reason];$get[type];]
 
 $ban[$guildID;$get[userID];$get[reason];0]
 
-$interactionReply[<@$authorID> **$username[$get[userID]]** foi **$get[type]**!]
+$interactionReply[<@$authorID> **$username[$get[userID]]** foi **$get[type]**!
+> $bold[$get[reason]]]
 
   `,
 data: {
