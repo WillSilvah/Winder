9641ec0a8de96ded0b7b71ca2cb950c490cb91b5
Williams Felipe <49321188+WillSilvah@users.noreply.github.com>
2026-02-27 10:57:19 -0300

    Create avisar.js

---
+++ comandos/slash/avisar.js
@@ -0,0 +1,41 @@
+module.exports = {
+  code: `
+$let[userID;$findUser[$option[user]]]
+$let[reason;$if[$option[motivo]==;Sei lá qual foi o motivo, só sei que recebeu!;$option[motivo]]]
+$let[staffID;$authorID]
+$let[type;advertido]
+
+$sendDMPunishment[$get[userID];$get[staffID];$get[reason];$get[type];]
+$punishmentlog[$get[userID];$get[staffID];$get[reason];$get[type];]
+
+$setMemberVar[lastWarn;{"staffID": "$findUser[$get[staffID]]","reason": "$get[reason]"};$findUser[$get[userID]]]
+$setMemberVar[warnsTotal;$sum[$getMemberVar[warnsTotal;$findUser[$get[userID]];$guildID;0];1];$findUser[$get[userID]]]
+
+$writeFile[Recursos/avisos/$guildID/$findUser[$get[userID]]/aviso_$getUserVar[warnsTotal;$findUser[$get[userID]];$guildID;0].txt;{
+"staffID": "$get[staffID]", 
+"motivo": "$get[reason]"
+};utf8]
+
+$interactionReply[<@$authorID> **$username[$get[userID]]** foi **$get[type]**!]
+
+  `,
+data: {
+  "type": 1,
+  "name": "avisar",
+  "description": "Aplique uma advertência em um membro",
+  "default_member_permissions": "4",
+  "options": [
+    {
+      "type": 6,
+      "description": "ID, menção ou nome de usuário",
+      "name": "user",
+      "required": true
+    },
+    {
+      "type": 3,
+      "description": "Motivo do usuário a ser punido",
+      "name": "motivo"
+    }
+  ]
+},
+}
