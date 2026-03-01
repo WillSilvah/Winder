ce01b4b9480d577ebba815bb9c1b8ef8355caf56
WillSilvah <williamfelipe235@gmail.com>
2026-03-01 11:34:09 -0300

    Media

--- comandos/eventos/missions.js
+++ comandos/eventos/missions.js
@@ -11,6 +11,11 @@ $sendMessage[$channelID;<@$authorID> | Você agora é um **Peludo Verificado**! 
 $!memberAddRoles[$guildID;$authorID;$getGuildVar[memberVerifiedRole;$guildID]]
 ]
 
+$if[$getGuildVar[guildActivityMedia]>=$getMemberVar[messageMonth;$authorID];
+
+$!memberAddRoles[$guildID;$authorID;$getGuildVar[memberActiveRole;$guildID]]
+]
+
 $if[$getGuildVar[guildMonthMessages;$guildID]==$getGuildVar[guildMonthMessagesMeta;$guildID];
 
 $setGuildVar[guildMonthMessagesMeta;$math[$getGuildVar[guildMonthMessagesMeta;$guildID]+10000];$guildID]
@@ -26,6 +31,5 @@ $color[Green]
 $if[$getUserVar[messageTotal;$authorID]<=6;
 $!memberAddRoles[$guildID;$authorID;$getGuildVar[allMemberRole;$guildID]]
 ]
-`,
-nonprefixed: true
+`
 }
