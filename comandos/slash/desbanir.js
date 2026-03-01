51b0f4a91a2702eb688acd56d0a1ae39ced14377
WillSilvah <williamfelipe235@gmail.com>
2026-02-28 22:16:27 -0300

    winder unban

--- comandos/slash/desbanir.js
+++ comandos/slash/desbanir.js
@@ -7,7 +7,7 @@ $let[type;desbanido]
 
 $punishmentlog[$get[userID];$get[staffID];$get[reason];$get[type];]
 
-$unban[$guildID;$get[userID];$get[reason];0]
+$unban[$guildID;$get[userID];$get[reason]]
 
 $interactionReply[<@$authorID> **$username[$get[userID]]** foi **$get[type]**!
 > $bold[$get[reason]]]
