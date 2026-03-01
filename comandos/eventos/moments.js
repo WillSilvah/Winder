72dfe3141a37389245b04fcdb9aac83791accc5b
WillSilvah <williamfelipe235@gmail.com>
2026-03-01 08:24:54 -0300

    Readme

--- comandos/eventos/moments.js
+++ comandos/eventos/moments.js
@@ -2,23 +2,23 @@ module.exports = {
     type: 'clientReady',
     code: `
 $cron[
-$deleteMemberVar[metaXP]
-$deleteMemberVar[messageMonth]
-$deleteMemberVar[votesMonth]
-$deleteMemberVar[msgXP]
+$deleteRecords[metaXP]
+$deleteRecords[messageMonth]
+$deleteRecords[votesMonth]
+$deleteRecords[msgXP]
 
-$deleteGuildVar[membersJoinedMonth]
-$deleteGuildVar[guildMonthMessages]
+$deleteRecords[membersJoinedMonth]
+$deleteRecords[guildMonthMessages]
 ;0 0 1 * *;America/Recife]
 
 $cron[
-$deleteGuildVar[guildWeeklyMessage]
-$deleteMemberVar[messageWeekly]
+$deleteRecords[guildWeeklyMessage]
+$deleteRecords[messageWeekly]
 ;0 0 * * 0]
 
 $cron[
-$deleteGuildVar[guildTodayMessage]
-$deleteMemberVar[messageToday]
+$deleteRecords[guildTodayMessage]
+$deleteRecords[messageToday]
 ;0 0 * * *]
  
  `
