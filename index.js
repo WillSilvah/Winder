a1ec0e1cd191f8ddddebe1bafd0142e87d9bf938
WillSilvah <williamfelipe235@gmail.com>
2026-03-02 17:10:23 -0300

    Music

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ const music = new ForgeMusic({
             "queueCreate",
             "queueDelete",
             "volumeChange"
-        ]
+        ],
+      includeExtractors: DefaultExtractors
 });
 
 const client = new ForgeClient({
