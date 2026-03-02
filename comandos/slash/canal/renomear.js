c6bd722c1a62b5ee46d1aa88c33cd3126f9082d8
WillSilvah <williamfelipe235@gmail.com>
2026-03-02 18:41:27 -0300

    Canal

---
+++ comandos/slash/canal/renomear.js
@@ -0,0 +1,33 @@
+module.exports = {
+  code: `
+$let[channelID;$option[canal]]
+$let[name;$option[nome]]
+  
+$setChannelName[$get[channelID];$replaceText[$get[nome];|;│]]
+
+$interactionReply[<@$authorID> | Canal renomeado!]
+  `,
+data: {
+  "type": 1,
+  "name_localizations": {
+    "pt-BR": "trabalhar"
+  },
+  "name": "renomear",
+  "description": "Renomeie o nome de um canal",
+  "default_member_permissions": "16",
+  "options": [
+    {
+      "type": 7,
+      "description": "Canal",
+      "required": true,
+      "name": "canal"
+    },
+    {
+      "type": 3,
+      "description": "Novo nome.",
+      "name": "nome",
+      "required": true
+    }
+  ]
+},
+}
