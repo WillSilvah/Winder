c8a09aa79a396005ac4a0ce73dae0cfa86e20453
WillSilvah <williamfelipe235@gmail.com>
2026-03-02 09:04:40 -0300

    file

---
+++ comandos/basic/desenvolvedor/database.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: "database baixar",
+    desc: "Pegue todos os dados catalogados pelo Winder de um usuário",
+    category: "Desenvolvedor",
+	usage: "database baixar userID",
+    type: "messageCreate",
+    code: `
+$onlyIf[$hasRoles[1462224054676099094;$authorID;1462547405466636384]==true;<@$authorID> | Você precisa ser **$username[$clientID]** Developer para poder executar esse comando.]
+
+$let[userID;$findUser[$message[1]]]
+
+$mkdir[Recursos/database]
+$writeFile[Recursos/database/WinderDB_$username[$get[userID]].json;
+$searchDB[;$get[userID];;;]
+;utf8]
+
+$attachment[Recursos/database/WinderDB_$username[$get[userID]].json;WinderDB_$username[$get[userID]].json;false;utf8;]
+`
+}
