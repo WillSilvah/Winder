e272cf413da66b807b3cd42def8bf20eee8b8830
WillSilvah <williamfelipe235@gmail.com>
2026-03-02 09:37:36 -0300

    file

--- comandos/basic/desenvolvedor/database.js
+++ comandos/basic/desenvolvedor/database.js
@@ -7,7 +7,6 @@ module.exports = {
     code: `
 $onlyIf[$hasRoles[1462224054676099094;$authorID;1462547405466636384]==true;<@$authorID> | Você precisa ser **$username[$clientID]** Developer para poder executar esse comando.]
 
-$mkdir[Recursos/database]
 $writeFile[Recursos/database/WinderDB_$username[$findUser[$message[0]]].json;
 $searchDB[;$findUser[$message[0]];;;]
 ;utf8]
