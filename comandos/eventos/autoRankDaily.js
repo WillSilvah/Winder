8133f0f047610d1716a87d0e2752b478297d5918
WillSilvah <williamfelipe235@gmail.com>
2026-03-03 06:40:15 -0300

    autoRankDaily

---
+++ comandos/eventos/autoRankDaily.js
@@ -0,0 +1,25 @@
+module.exports = {
+    type: 'clientReady',
+    code: `
+$jsonLoad[session;$readFile[Recursos/session.json]]
+$let[guildID;$getGlobalVar[guildID]]
+
+$setInterval[
+$clearMessages[$getGuildVar[rankedChat;$get[guildID]];6]
+$sendMessage[$getGuildVar[rankedChat;$get[guildID]];
+$disableAllMentions
+$addContainer[
+$addSection[
+$addTextDisplay[## $toUpperCase[TEMPORADA $env[session;number]: $env[session;name]]]
+$addThumbnail[$guildIcon[$get[guildID]]]
+]
+$addSeparator[Large;true]
+$addTextDisplay[
+$memberLeaderboard[messageMonth;$get[guildID];desc;11;1;\n;data;pos;$return[$env[pos]° <@$env[data;id]>
+💬$env[data;value] | ✨️ $getMemberVar[pdaMonth;$env[data;id];$get[guildID]] | ⬆️ $getMemberVar[votesMonth;$env[data;id];$get[guildID]]]]
+]
+;Green]
+];30m;autoRankDaily]
+ 
+`
+}
