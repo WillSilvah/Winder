d4121852971054ea07a519b91ec87777deb74340
WillSilvah <williamfelipe235@gmail.com>
2026-03-04 16:48:58 -0300

    Defensor

--- comandos/basic/defensor/NewMemberSharedLink.js
+++ comandos/basic/defensor/NewMemberSharedLink.js
@@ -6,6 +6,8 @@ $jsonLoad[rawData;$messageRawData[$channelID;$messageID]]
 
 $onlyIf[$env[rawData;embeds;0;url]!=;]
 $onlyIf[$getMemberVar[messageTotal;$authorID;$guildID;0]<=6]
+$onlyIf[$includes[$env[rawData;embeds;0;url];tenor;giphy]==true;]
+
 
 $let[userID;$authorID]
 $let[reason;Envio de link com a idéia de divulgar algum produto, um programa malicioso ou qualquer outra intenção.]
