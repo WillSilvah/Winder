c6b79e41905e3a1799d2c7f99677bbdff58aab52
WillSilvah <williamfelipe235@gmail.com>
2026-03-04 16:46:52 -0300

    Defensor

--- comandos/basic/defensor/NewMemberSharedLink.js
+++ comandos/basic/defensor/NewMemberSharedLink.js
@@ -5,10 +5,22 @@ $wait[2s]
 $jsonLoad[rawData;$messageRawData[$channelID;$messageID]]
 
 $onlyIf[$env[rawData;embeds;0;url]!=;]
+$onlyIf[$getMemberVar[messageTotal;$authorID;$guildID;0]<=6]
 
-$sendMessage[1472742861526142996;
-$env[rawData;embeds;0;url]
+$let[userID;$authorID]
+$let[reason;Envio de link com a idéia de divulgar algum produto, um programa malicioso ou qualquer outra intenção.]
+$let[staffID;$clientID]
+$let[type;banido]
 
-]
+$sendMessage[$channelID;w!ban <@$authorID> $get[reason]]
+
+$if[$memberExists[$guildID;$env[userID]]==true;$sendDMPunishment[$get[userID];$get[staffID];$get[reason];$get[type];]]
+$punishmentlog[$get[userID];$get[staffID];$get[reason];$get[type];]
+
+$!ban[$guildID;$get[userID];$get[reason];7]
+
+$sendMessage[$channelID;<@$clientID> **$username[$get[userID]]** foi **$get[type]**!
+> $bold[$get[reason]]]
+
 `
 }
