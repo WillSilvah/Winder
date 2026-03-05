8326a97c88f5f76cb49591385017f8f818734bf2
WillSilvah <williamfelipe235@gmail.com>
2026-03-05 15:42:27 -0300

    Timeout

--- comandos/eventos/clientReady/timeoutsHandler.js
+++ comandos/eventos/clientReady/timeoutsHandler.js
@@ -5,10 +5,10 @@ module.exports = {
     $jsonLoad[keys;$jsonKeys[timeouts]]
     $loop[$arrayLength[keys];
       $let[id;$arrayAt[keys;$math[$env[i] - 1]]]
-      $let[code;$env[timeouts;$get[id];code]]
       $let[endTime;$env[timeouts;$get[id];endTime]]
+      $let[code;$env[timeouts;$get[id];code]]
 
-      $log[1### id: $get[id] | end time: $get[endTime] | ends in: $math[$get[endTime] - $getTimestamp]ms]
+      $log[id: $get[id] | end time: $get[endTime]]
 
       $setTimeout[
         $jsonLoad[timeouts;$getGlobalVar[timeouts;{}]]
@@ -17,7 +17,6 @@ module.exports = {
           $setGlobalVar[timeouts;$env[timeouts]]
         ]
         $try[$eval[$get[code];false]]
-        $log[2### id: $get[id] | $get[endTime] - $getTimestamp = $math[$get[endTime] - $getTimestamp]]
       ;$max[500;$math[$get[endTime] - $getTimestamp]];$get[id]]
     ;i;true]
   `
