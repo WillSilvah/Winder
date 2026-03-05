21df6fe1620f57b1e842c0eea1c0b4f5e2834ea2
WillSilvah <williamfelipe235@gmail.com>
2026-03-05 16:17:10 -0300

    BumpReminder

--- comandos/eventos/messageCreate/bumpReminder.js
+++ comandos/eventos/messageCreate/bumpReminder.js
@@ -11,15 +11,15 @@ $onlyIf[$checkContains[$env[data;embeds;0;description];Bump done!]]
 
 $sendMessage[1462796515578876079;<@$env[data;interaction;user]> Obrigado por ajudar!]
 
-$let[text;Hey! Tá na hora de dar boost na Disboard!
-Usa o comando </bump:947088344167366698>]
+$let[text;Hey! Tá na hora de dar boost na Disboard!\nUsa o comando </bump:947088344167366698>]
 $advancedTimeout[$esc[
-  $sendMessage[{0};{1}
-  $title[🎉 Hora de dar /bump!]
-  $description[{2}]
-  $color[Green]
+  $sendMessage[{0};
+    {1}
+    $title[🎉 Hora de dar /bump!]
+    $description[{2}]
+    $color[Green]
   ]
-];2h;BumpReminder;$channelID;<@&1462953076091785370>;$get[text]]
+];1m;BumpReminder_TEST;$channelID;<@&1462953076091785370>;$get[text]]
 
 `
 }
