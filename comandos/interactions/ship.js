a85f300335927399f262a50da12aa5f4d024d496
WillSilvah <williamfelipe235@gmail.com>
2026-03-06 12:32:34 -0300

    Ship

--- comandos/interactions/ship.js
+++ comandos/interactions/ship.js
@@ -7,6 +7,7 @@ $let[user1;$option[user1]]
 $let[user2;$findUser[$option[user2];true]]
 
 $setGlobalVar[ship_$get[user1]_$get[user2];$get[shipCalc]]
+$setGlobalVar[ship_$get[user2]_$get[user1];$get[shipCalc]]
 
 $interactionReply[$addContainer[
 $addSection[
@@ -17,12 +18,15 @@ $addSection[
 $addTextDisplay[### ...E A DE @$userTag[$get[user2]]...]
 $addThumbnail[$userAvatar[$get[user2]]]
 ]
-$addTextDisplay[## = ❤️ $get[shipCalc]%]
+$addTextDisplay[## = ❤️ $getGlobalVar[ship_$get[user1]_$get[user2]]%]
 ;Red]]
 
 $let[varShip;ship_$get[user1]_$get[user2]]
+$let[varShip2;ship_$get[user2]_$get[user1]]
 
-$advancedTimeout[$deleteRecords[{0}];24h;DELETE-ship_$get[user1]_$get[user2];$get[varShip]]
+
+$advancedTimeout[$deleteRecords[{0}];24h;DELETE-ship_$get[user1]_$get[user2];$get[varShip]
+$deleteRecords[{0}];24h;DELETE-ship_$get[user2]_$get[user3];$get[varShip2]]
   `,
 data: {
   "type": 1,
