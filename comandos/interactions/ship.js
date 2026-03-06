86773fd6911443b24fb9c6a1fb943d107e2ab80c
WillSilvah <williamfelipe235@gmail.com>
2026-03-06 15:27:07 -0300

    Ship

--- comandos/interactions/ship.js
+++ comandos/interactions/ship.js
@@ -12,12 +12,10 @@ $setGlobalVar[ship_$get[user2]_$get[user1];$get[shipCalc]]
 ]
 
 $interactionReply[$addContainer[
-$addSection[
 $addTextDisplay[### @$userDisplayName[$get[user1]] É $getGlobalVar[ship_$get[user1]_$get[user2]]% PELO @$userDisplayName[$get[user2]]]
 $addMediaGallery[
 $addMediaItem[$userAvatar[$get[user1]]]
 $addMediaItem[$userAvatar[$get[user2]]]
-]
 ]
 
 
