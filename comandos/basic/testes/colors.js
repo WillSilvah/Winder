be34b2e2f6d317fca669f7511d7102fcd4c7d13b
WillSilvah <williamfelipe235@gmail.com>
2026-03-10 15:35:02 -0300

    colors.js

--- comandos/basic/testes/colors.js
+++ comandos/basic/testes/colors.js
@@ -3,6 +3,7 @@ module.exports = [{
     type: "messageCreate",
     code: `
 $onlyIf[$hasRoles[1462224054676099094;$authorID;1462547405466636384]==true;<@$authorID> | Você precisa ser **$username[$clientID]** Developer para poder executar esse comando.]
+$deleteCommand
 
 $addTextDisplay[## SIMPLESMENTE CORES!
 Escolha uma cor que mais combina com você!]
