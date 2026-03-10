9d2f30aa27eb6e431ba55d53608da9602c610e69
Williams Felipe <49321188+WillSilvah@users.noreply.github.com>
2026-03-10 10:56:35 -0300

    Update contador.js

--- comandos/eventos/clientReady/contador.js
+++ comandos/eventos/clientReady/contador.js
@@ -6,12 +6,12 @@ $!setChannelName[1465535257297027214;🐾 | $separateNumber[$guildMemberCount[14
 
 $!setChannelName[1463122813736063016;⭐️ | falta $sub[$getGuildVar[membersMeta;1462224054676099094];$guildMemberCount[1462224054676099094;;false]] peludos]
 
-$!setChannelName[1465534760620392722;💬 | $separateNumber[$getGuildVar[guildMonthMessages;1462224054676099094];.] mensagens]
+$!setChannelName[1465534760620392722;💬 | $separateNumber[$getGuildVar[guildMonthMessages;1462224054676099094];.] (👤$getMemberLeaderboardLength[messageMonth;1462224054676099094]) mensagens]
 
 ;10m;Voice_Statdocks]
 
 $setInterval[
-$!setChannelTopic[$getGuildVar[batePapo;1462224054676099094];(👤💬 $getMemberLeaderboardLength[messageToday;$guildID]) Onde a fauna se encontra.
+$!setChannelTopic[$getGuildVar[batePapo;1462224054676099094];(👤💬 $getMemberLeaderboardLength[messageToday;1462224054676099094]) Onde a fauna se encontra.
 Leia as #👋│diretrizes para ficar de acordo com a ordem! :dizzy:]
 ;10m;BatePapo_Stats]
 `
