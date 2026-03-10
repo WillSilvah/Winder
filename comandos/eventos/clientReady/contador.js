317b3f8d07c8746cdc727766b4f27f1571cec801
WillSilvah <williamfelipe235@gmail.com>
2026-03-10 09:11:10 -0300

    contador.js

--- comandos/eventos/clientReady/contador.js
+++ comandos/eventos/clientReady/contador.js
@@ -2,7 +2,7 @@ module.exports = {
  type: 'clientReady',
  code: `
 $setInterval[
-$!setChannelName[1465535257297027214;🐾 | $separateNumber[$guildMemberCount[1462224054676099094;;false];.] peludos]
+$!setChannelName[1465535257297027214;🐾 | $separateNumber[$guildMemberCount[1462224054676099094;;false];.] (🟢$math[$guildMemberCount[1462224054676099094;online;false]+$guildMemberCount[1462224054676099094;dnd;false]+$guildMemberCount[1462224054676099094;idle;false]]) peludos]
 
 $!setChannelName[1463122813736063016;⭐️ | falta $sub[$getGuildVar[membersMeta;1462224054676099094];$guildMemberCount[1462224054676099094;;false]] peludos]
 
