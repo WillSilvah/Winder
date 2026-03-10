7bbe6394b6b5bf91adb330eb4b2aef4928c8344c
WillSilvah <williamfelipe235@gmail.com>
2026-03-10 15:36:39 -0300

    colors.js

--- comandos/basic/testes/colors.js
+++ comandos/basic/testes/colors.js
@@ -33,7 +33,7 @@ $jsonLoad[cores;{
 }
 ]
 
-$!memberRemoveRoles[$guildID;$authorID;$jsonValues[cores;,]]
+$!memberRemoveRoles[$guildID;$authorID;$jsonValues[cores;$esc[;]]]
 
 $!memberAddRoles[$guildID;$authorID;$env[cores;$selectMenuValues]]
 
