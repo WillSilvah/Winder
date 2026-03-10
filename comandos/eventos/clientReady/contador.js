09fab30f584f78b90e729bbaa419753442def66e
WillSilvah <williamfelipe235@gmail.com>
2026-03-10 14:12:15 -0300

    contador.js

--- comandos/eventos/clientReady/contador.js
+++ comandos/eventos/clientReady/contador.js
@@ -2,11 +2,11 @@ module.exports = {
  type: 'clientReady',
  code: `
 $setInterval[
-$!setChannelName[1465535257297027214;🐾 | $separateNumber[$guildMemberCount[1462224054676099094;;false];.] (🟢$math[$guildMemberCount[1462224054676099094;online;false]+$guildMemberCount[1462224054676099094;dnd;false]+$guildMemberCount[1462224054676099094;idle;false]]) peludos]
+$!setChannelName[1465535257297027214;🐾 | $separateNumber[$guildMemberCount[1462224054676099094;;false];.] (🟢$math[$guildMemberCount[1462224054676099094;online;false]+$guildMemberCount[1462224054676099094;dnd;false]+$guildMemberCount[1462224054676099094;idle;false]])]
 
 $!setChannelName[1463122813736063016;⭐️ | falta $sub[$getGuildVar[membersMeta;1462224054676099094];$guildMemberCount[1462224054676099094;;false]] peludos]
 
-$!setChannelName[1465534760620392722;💬 | $separateNumber[$getGuildVar[guildMonthMessages;1462224054676099094];.] (👤$getMemberLeaderboardLength[messageMonth;1462224054676099094]) mensagens]
+$!setChannelName[1465534760620392722;💬 | $separateNumber[$getGuildVar[guildMonthMessages;1462224054676099094];.] (👤$getMemberLeaderboardLength[messageMonth;1462224054676099094]) ]
 
 ;10m;Voice_Statdocks]
 
