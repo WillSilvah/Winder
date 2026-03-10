93333cacf692802f034b2efd7b1f9bdf42fd1b46
WillSilvah <williamfelipe235@gmail.com>
2026-03-10 00:05:10 -0300

    info.js

--- comandos/interactions/servidor/info.js
+++ comandos/interactions/servidor/info.js
@@ -1,13 +1,20 @@
 module.exports = {
   code: `
+$jsonLoad[session;$readFile[Recursos/session.json]]
 $addContainer[
 $addSection[
 $addTextDisplay[## $toUpperCase[$guildName]
-**🐾 $separateNumber[$guildMemberCount[1462224054676099094;;false];.] | 🟢 $separateNumber[$guildMemberCount[1462224054676099094;online;false];.] | 🚀 $guildBoostCount[$guildID]**
+**👤 $separateNumber[$guildMemberCount[1462224054676099094;;false];.] | 🟢 $separateNumber[$guildMemberCount[1462224054676099094;online;false];.] | 🚀 $guildBoostCount[$guildID]**
 ]
 $addThumbnail[$guildIcon]
 ]
 $addSeparator[Large;true]
+$addTextDisplay[
+### $toUpperCase[TEMPORADA $env[session;number]: $env[session;name]]
+💬 **$separateNumber[$getGuildVar[guildMonthMessages;$get[guildID];0];.]** mensagens foram enviadas por **$getMemberLeaderboardLength[messageMonth;$get[guildID]]** membros.
+💬 A média de mensagens do mês anterior é **$getGuildVar[guildActivityMedia]** e atualmente é 
+
+]
 ;$getGuildVar[color;$guildID;Red]]
 
   `,
