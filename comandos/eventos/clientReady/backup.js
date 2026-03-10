7c9574c0d9b17b1b5975fcd386f4118cde115456
WillSilvah <williamfelipe235@gmail.com>
2026-03-09 21:53:01 -0300

    backup.js

--- comandos/eventos/clientReady/backup.js
+++ comandos/eventos/clientReady/backup.js
@@ -1,7 +1,7 @@
 module.exports = [{
     type: "clientReady",
-    code: `
-    WinderDB_$day[TwoDigit]-$month[TwoDigit]_$hour[TwoDigit]$minute[TwoDigit]
+    code: `$timezone[America/Recife]
+WinderDB_$day[TwoDigit]-$month[TwoDigit]_$hour[TwoDigit]$minute[TwoDigit]
 $sendMessage[788131713418002463;
 WinderDB_$day[TwoDigit]-$month[TwoDigit]_$hour[TwoDigit]$minute[TwoDigit]
 $attachment[database/forge.db;forge.db]
