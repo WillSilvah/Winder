3b0a0d8f767ddb8fd0ff49dace53322d59d08208
WillSilvah <williamfelipe235@gmail.com>
2026-03-10 00:24:09 -0300

    info.js

---
+++ comandos/interactions/servidor/info.js
@@ -0,0 +1,35 @@
+module.exports = {
+  code: `
+$jsonLoad[session;$readFile[Recursos/session.json]]
+$addContainer[
+$addSection[
+$addTextDisplay[## $toUpperCase[$guildName]
+**👤 $separateNumber[$guildMemberCount[1462224054676099094;;false];.] | 🟢 $separateNumber[$guildMemberCount[1462224054676099094;online;false];.] | 🚀 $guildBoostCount[$guildID]**
+⭐️ $discordTimestamp[$guildCreatedAt[$guildID];RelativeTime]
+]
+$addThumbnail[$guildIcon]
+]
+$addSeparator[Large;true]
+$addTextDisplay[
+### $toUpperCase[TEMPORADA $env[session;number]: $env[session;name]]
+💬 **$getMemberLeaderboardLength[messageMonth;$get[guildID]]** membros enviaram **$separateNumber[$getGuildVar[guildMonthMessages;$get[guildID];0];.]** mensagens neste mês.
+💬 **$getMemberLeaderboardLength[messageWeekly;$guildID]** membros enviaram **$separateNumber[$getGuildVar[guildWeeklyMessages;$guildID;0];.]** mensagens nessa semana.
+💬 **$getMemberLeaderboardLength[messageToday;$guildID]** membros enviaram **$separateNumber[$getGuildVar[guildTodayMessages;$guildID;0];.]** mensagens hoje.
+💬 A média de mensagens do mês anterior é **$getGuildVar[guildActivityMedia;$guildID;0]** e atualmente é **$truncate[$math[$media[messageMonth;$guildMemberCount[1462224054676099094;;false]]/3]]**.
+]
+$addSeparator[Large;true]
+$addTextDisplay[
+🎮 $discordTimestamp[$getGuildVar[minigameNextTime];RelativeTime] pode acontecer um minigame.
+🎮 **$getGuildVar[minigameExecuteCount;$guildID;0]** minigames executados.
+🎮 **$getGuildVar[minigameGuildWins;$guildID;0]** minigames ganhos.
+]
+
+;$getGuildVar[color;$guildID;Red]]
+
+  `,
+data: {
+  "type": 1,
+  "description": "Veja informações sobre o servidor!",
+  "name": "info"
+},
+}
