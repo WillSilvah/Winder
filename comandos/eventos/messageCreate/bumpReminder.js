1a799692730375ad18d32d3f3c43585af5c205a5
WillSilvah <williamfelipe235@gmail.com>
2026-03-09 22:23:56 -0300

    bumpReminder.js

--- comandos/eventos/messageCreate/bumpReminder.js
+++ comandos/eventos/messageCreate/bumpReminder.js
@@ -9,7 +9,8 @@ $jsonLoad[data;$messageRawData[$channelID;$messageID]]
 
 $onlyIf[$checkContains[$env[data;embeds;0;description];Bump done!]]
 
-$sendMessage[1462796515578876079;<@$env[data;interaction;user]> Obrigado por ajudar!]
+$sendMessage[1462796515578876079;<@$env[data;interaction;user]> Obrigado por ajudar!
+Avisarei quando bater $discordTimestamp[$math[$getTimestamp+$parseString[2h]];LongTime]]
 
 $let[text;Hey! Tá na hora de dar boost na Disboard!\nUsa o comando </bump:947088344167366698>]
 $advancedTimeout[$esc[
