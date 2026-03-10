bbb0431b185f7767ad3464aa1c0aef2241d4ba7a
WillSilvah <williamfelipe235@gmail.com>
2026-03-10 00:10:41 -0300

    info.js

--- comandos/interactions/servidor/info.js
+++ comandos/interactions/servidor/info.js
@@ -11,10 +11,10 @@ $addThumbnail[$guildIcon]
 $addSeparator[Large;true]
 $addTextDisplay[
 ### $toUpperCase[TEMPORADA $env[session;number]: $env[session;name]]
-💬 **$separateNumber[$getGuildVar[guildMonthMessages;$guildID;0];.]** mensagens foram enviadas por **$getMemberLeaderboardLength[messageMonth;$guildID]** membros.
+💬 **$getMemberLeaderboardLength[messageMonth;$get[guildID]]** membros enviaram **$separateNumber[$getGuildVar[guildMonthMessages;$get[guildID];0];.]** mensagens neste mês.
 💬 **$getMemberLeaderboardLength[messageWeekly;$guildID]** membros enviaram **$separateNumber[$getGuildVar[guildWeeklyMessages;$guildID;0];.]** mensagens nessa semana.
 💬 **$getMemberLeaderboardLength[messageToday;$guildID]** membros enviaram **$separateNumber[$getGuildVar[guildTodayMessages;$guildID;0];.]** mensagens hoje.
-💬 A média de mensagens do mês anterior é **$getGuildVar[guildActivityMedia]** e atualmente é **$truncate[$math[$media[messageMonth;$guildMemberCount[1462224054676099094;;false]]/3]]**.
+💬 A média de mensagens do mês anterior é **$getGuildVar[guildActivityMedia;$guildID;0]** e atualmente é **$truncate[$math[$media[messageMonth;$guildMemberCount[1462224054676099094;;false]]/3]]**.
 
 ]
 ;$getGuildVar[color;$guildID;Red]]
