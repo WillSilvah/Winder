52edca127da9cc8e9fc1b87a395f824e3a9257db
WillSilvah <williamfelipe235@gmail.com>
2026-03-11 11:03:56 -0300

    timeouts.js

--- comandos/basic/desenvolvedor/timeouts.js
+++ comandos/basic/desenvolvedor/timeouts.js
@@ -1,8 +1,8 @@
 module.exports = {
-    name: "eval",
-    desc: "Permite o desenvolvedor executar funções a nível superior",
+    name: "timeouts",
+    desc: "Envia um arquivo json com todos os dados de timeouts avancados a ser executados",
     category: "Desenvolvedor",
-	usage: "eval code",
+	usage: "timeouts",
     type: "messageCreate",
     code: `
 $onlyIf[$hasRoles[1462224054676099094;$authorID;1462547405466636384]==true;<@$authorID> | Você precisa ser **$username[$clientID]** Developer para poder executar esse comando.]
