ab21d94a9eef049cca536721e45a931d282a7fbd
WillSilvah <williamfelipe235@gmail.com>
2026-03-11 11:13:46 -0300

    timeouts.js

--- comandos/basic/desenvolvedor/timeouts.js
+++ comandos/basic/desenvolvedor/timeouts.js
@@ -7,7 +7,7 @@ module.exports = {
     code: `
 $onlyIf[$hasRoles[1462224054676099094;$authorID;1462547405466636384]==true;<@$authorID> | Você precisa ser **$username[$clientID]** Developer para poder executar esse comando.]
 $jsonLoad[timeouts;$getGlobalVar[timeouts]]
-
+$reply
 **$argsCount[$jsonValues[timeouts; ]]** comandos esperando serem executados.
 $attachment[$getGlobalVar[timeouts];WinderDB_Timeouts.json;true]
 `
