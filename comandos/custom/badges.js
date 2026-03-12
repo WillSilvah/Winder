8949715379d1742e928fbde1aca11d6ef7d4ffb9
WillSilvah <williamfelipe235@gmail.com>
2026-03-12 16:11:38 -0300

    badges.js

--- comandos/custom/badges.js
+++ comandos/custom/badges.js
@@ -8,7 +8,7 @@ module.exports = [{
     }
    ],
    code: `
-    $let[badges;$if[$userGuildTag[$env[userID]]==PATS;$emoji[$emojiID[pats_logo]] ;]$if[$getMemberVar[memberIsVerified;$env[userID]]==true;✅;]$if[$getMemberVar[memberIsStaff;$env[userID]]==true;🛡️;]$if[$getMemberVar[memberIsActive;$env[userID]]==true;💬;]$if[$getMemberVar[badge_diadasmulheres;$env[userID]]==true;<:badge_diadasmulheres:1480234134742896842>;]]
+    $let[badges;$if[$userGuildTag[$env[userID]]==PATS;$emoji[$emojiID[pats_logo]] ;]$if[$getMemberVar[memberIsVerified;$env[userID]]==true;✅;]$if[$getMemberVar[memberIsStaff;$env[userID]]==true;🛡️;]$if[$getMemberVar[memberIsActive;$env[userID]]==true;💬;]$if[$getMemberVar[badge_diadasmulheres;$env[userID]]==true;<:badge_diadasmulheres:1480234134742896842>;]$if[$getMemberVar[badge_cobaia;$env[userID]]==true;⚠️;]]
 
         $return[$get[badges]]
 `
