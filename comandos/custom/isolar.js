c47ad6c4e438e406162aed97063472b4ce765619
WillSilvah <williamfelipe235@gmail.com>
2026-03-12 08:47:37 -0300

    isolar.js

--- comandos/custom/isolar.js
+++ comandos/custom/isolar.js
@@ -8,15 +8,45 @@ module.exports = [{
     }
    ],
     code: `
-    $if[$hasRoles[$hasRoles[$guildID;$env[userID];$getGuildVar[memberIsolatedRole]]==false;
+    $onlyIf[$getGuildVar[memberIsolatedRole]!=;]
+    $if[$hasRoles[$guildID;$env[userID];$getGuildVar[memberIsolatedRole]]==false;
      $!memberAddRoles[$guildID;$env[userID];$getGuildVar[memberIsolatedRole]]
      
      $arrayLoad[channels;,;$guildChannelIDs[$guildID;,]]
       $arrayForEach[channels;id;
-       $if[
-       $modifyChannelPerms[$getGuildVar[isolatedChannel];$getGuildVar[memberIsolatedRole];-ViewChannel]
+       $if[$env[id]!=$getGuildVar[isolatedChannel];
+       $modifyChannelPerms[$env[id];$getGuildVar[memberIsolatedRole];-ViewChannel]
       ]]
-         
+     
+     $setMemberVar[memberIsolated;true;$env[userID];$guildID]
+     
+     $if[$getMemberVar[memberIsVerified]==true;
+      $!memberRemoveRoles[$guildID;$env[userID];$getGuildVar[memberVerifiedRole]]
+     ]
+      $if[$getMemberVar[memberIsStaff]==true;
+       $!memberRemoveRoles[$guildID;$env[userID];$getGuildVar[allStaffRole]]
+      ]
+       $if[$getMemberVar[memberIsArtist]==true;
+        $!memberRemoveRoles[$guildID;$env[userID];$getGuildVar[memberArtistRole]]
+       ]
+        $if[$getMemberVar[memberIsOfficialArtist]==true;
+         $!memberRemoveRoles[$guildID;$env[userID];$getGuildVar[memberOfficialArtistRole]]
+        ]
+         $if[$getMemberVar[memberIsSupporter]==true;
+          $!memberRemoveRoles[$guildID;$env[userID];$getGuildVar[memberSupporterRole]]
+         ]
+          $if[$getMemberVar[memberIsActive]==true;
+           $!memberRemoveRoles[$guildID;$env[userID];$getGuildVar[memberActiveRole]]
+          ]
+     
+      
+      ;
+     $!memberRemoveRoles[$guildID;$env[userID];$getGuildVar[memberIsolatedRole]]
+     
+     $advancedTimeout[$esc[
+      $setMemberVar[memberIsolated;false;{0};{1}]
+     
+     ];24h;IsolatedStatus_$get[userID];$get[userID];$guildID]
     ]
 
 `
