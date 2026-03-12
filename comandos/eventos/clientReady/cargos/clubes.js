35d1dc31c98875243ce680aed3af6e18eda741c6
WillSilvah <williamfelipe235@gmail.com>
2026-03-12 17:23:09 -0300

    clubes.js

--- comandos/eventos/clientReady/cargos/clubes.js
+++ comandos/eventos/clientReady/cargos/clubes.js
@@ -3,33 +3,29 @@ module.exports = [{
     type: "clientReady",
     code: `
     $deleteMessage[1470875996356874292;$getGlobalVar[clubes_messageID]]
-    
     $wait[2s]
-    
-$let[msgID;$sendMessage[1470875996356874292;$disableAllMentions
+    $let[msgID;$sendMessage[1470875996356874292;$disableAllMentions
 
     $addContainer[
-    $addSection[
-    $addTextDisplay[## CLUBES!
-**😘! 🤫! 🏳️‍🌈**
+     $addSection[
+     $addTextDisplay[## CLUBES!
+**🐱 😘! 🤫! 🏳️‍🌈**
 
 Qual clube você se identifica ou quer participar?
 ]
     $addThumbnail[https://abs.twimg.com/emoji/v2/72x72/1f465.png]
 ]
-;Green]
+    ;Green]
 
     $addActionRow
     $addStringSelectMenu[clubes;Escolha um clube;false;1;1]
-    $addOption[BoyKisser;;boykisser;😘;false]
+     $addOption[Clube do Winder;;winder;🐱;false]
+     $addOption[BoyKisser;;boykisser;😘;false]
+     $addOption[Fofoqueiras;;fofoqueiras;🤫;false]
+     $addOption[Vale Animado;;valeanimado;🏳️‍🌈;false]
+     $addOption[Sair de todos os clubes;Até logo;remove;❌;false]
     
-    $addOption[Fofoqueiras;;fofoqueiras;🤫;false]
-    
-    $addOption[Vale Animado;;valeanimado;🏳️‍🌈;false]
-    
-    $addOption[Sair de todos os clubes;Até logo;remove;❌;false]
-    
-;true]]
+    ;true]]
 
     $setGlobalVar[clubes_messageID;$get[msgID]]
 `
@@ -40,7 +36,8 @@ Qual clube você se identifica ou quer participar?
     $jsonLoad[clubes;{
   "boykisser": "1465012850740302027",
   "fofoqueiras": "1481404732928168077",
-  "valeanimado": "1463156445276143707"
+  "valeanimado": "1463156445276143707",
+  "winder": "1481748351857725521"
 }
 ]
     $arrayLoad[clubesID;,;$jsonValues[clubes;,]]
