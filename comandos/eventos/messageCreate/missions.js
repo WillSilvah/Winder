4f120735dc98de714c1c852803c508543244cfdb
WillSilvah <williamfelipe235@gmail.com>
2026-03-12 09:17:18 -0300

    missions.js

--- comandos/eventos/messageCreate/missions.js
+++ comandos/eventos/messageCreate/missions.js
@@ -1,62 +1,56 @@
 module.exports = [{
     type: "messageCreate",
     code: `
-$if[$getMemberVar[messageTotal;$authorID;$guildID;0]==6;
-$reply[$channelID;$messageID]
-$startTyping[$channelID]
-$wait[2s]
-$sendMessage[$channelID;Olá, seja bem vindo(a) a patinhas! 👋 Vim te contar que para ter acesso a enviar mídia nos canais de texto você precisa enviar **200 mensagens**, é bem rápido. Veja suas informações em **/perfil**👀
-]
-$startTyping[$channelID]
-$wait[7s]
-$sendMessage[$channelID;Não é necessário usar o **/perfil** toda hora, irei te avisar no momento em que conseguir, relaxe! 😊
-]
-$startTyping[$channelID]
-$wait[4s]
-$sendMessage[$channelID;Dúvidas? Qualquer coisa o canal <#1463277652562411787> está disponível. Se divirta na patinhas! 
-]
-$startTyping[$channelID]
-$wait[2s]
-$sendMessage[$channelID;😘]
-]
+    $if[$getMemberVar[messageTotal;$authorID;$guildID;0]==6;
+     $reply[$channelID;$messageID]
+     $startTyping[$channelID]
+     $wait[2s]
+     $sendMessage[$channelID;Olá, seja bem vindo(a) a patinhas! 👋 Vim te contar que para ter acesso a enviar mídia nos canais de texto você precisa enviar **200 mensagens**, é bem rápido. Veja suas informações em **/perfil**👀]
+     $startTyping[$channelID]
+     $wait[7s]
+     $sendMessage[$channelID;Não é necessário usar o **/perfil** toda hora, irei te avisar no momento em que conseguir, relaxe! 😊]
+     $startTyping[$channelID]
+     $wait[4s]
+     $sendMessage[$channelID;Dúvidas? Qualquer coisa o canal <#1463277652562411787> está disponível. Se divirta na patinhas! ]
+     $startTyping[$channelID]
+     $wait[2s]
+     $sendMessage[$channelID;😘]
+    ]
 
-$if[$getGuildVar[allMemberRole]!=;
-$if[$getMemberVar[messageTotal;$authorID]>=6;
-$!memberAddRoles[$guildID;$authorID;$getGuildVar[allMemberRole];1481259180877811930;1481256988921954418;1481256655986360361;1481256478563237888;1481261082977763338]
-]
-]
+    $if[$getGuildVar[allMemberRole]!=;
+     $if[$getMemberVar[messageTotal;$authorID]>=6;
+      $!memberAddRoles[$guildID;$authorID;$getGuildVar[allMemberRole];1481259180877811930;1481256988921954418;1481256655986360361;1481256478563237888;1481261082977763338]
+     ]
+    ]
 
-$if[$getGuildVar[memberVerifiedRole]!=;
-$if[$getMemberVar[messageTotal;$authorID]>=200;
-$onlyIf[$getGlobalVar[sleepMode]==false;]
-$onlyIf[$hasRoles[$guildID;$authorID;$getGuildVar[memberVerifiedRole;$guildID]]==false;]
-$sendMessage[$channelID;<@$authorID> | Você agora é um **Peludo Verificado**! $emoji[$emojiID[pats_foxClapping]] Você está gostando da Patinhas? 👀;false]
-$!memberAddRoles[$guildID;$authorID;$getGuildVar[memberVerifiedRole;$guildID]]
-]
-]
+    $if[$getGuildVar[memberVerifiedRole]!=;
+     $if[$getMemberVar[messageTotal;$authorID]>=200;
+      $onlyIf[$getGlobalVar[sleepMode]==false;]
+      $onlyIf[$getMemberVar[memberIsolated]==false;]
+      $onlyIf[$hasRoles[$guildID;$authorID;$getGuildVar[memberVerifiedRole;$guildID]]==false;]
+      $sendMessage[$channelID;<@$authorID> | Você agora é um **Peludo Verificado**! $emoji[$emojiID[pats_foxClapping]] Você está gostando da Patinhas? 👀;false]
+      $!memberAddRoles[$guildID;$authorID;$getGuildVar[memberVerifiedRole;$guildID]]
+     ]
+    ]
 
 
 
-$if[$getGuildVar[guildMonthMessages;$guildID;0]==$getGuildVar[guildMonthMessagesMeta;$guildID];
-
-$setGuildVar[guildMonthMessagesMeta;$math[$getGuildVar[guildMonthMessagesMeta;$guildID]+10000];$guildID]
-]
-
-
+    $if[$getGuildVar[guildMonthMessages;$guildID;0]==$getGuildVar[guildMonthMessagesMeta;$guildID];
+     $setGuildVar[guildMonthMessagesMeta;$math[$getGuildVar[guildMonthMessagesMeta;$guildID]+10000];$guildID]
+    ]
 
 `
 },{
     type: "messageCreate",
     code: `
     $onlyIf[$guildID==1462224054676099094;]
-$if[$getMemberVar[messageMonth;$authorID]>=$getGuildVar[guildActivityMedia];
-$onlyIf[$hasRoles[$guildID;$authorID;$getGuildVar[memberActiveRole;$guildID]]==false;]
-$!memberAddRoles[$guildID;$authorID;$getGuildVar[memberActiveRole;$guildID]]
-
-$sendMessage[1467318550925410485;
-$title[O membro @$userTag é um membro ativo!]
-$description[
-Média de atividade do servidor atual: **$getGuildVar[guildActivityMedia]**
+    
+    $if[$getMemberVar[messageMonth;$authorID]>=$getGuildVar[guildActivityMedia];
+     $onlyIf[$hasRoles[$guildID;$authorID;$getGuildVar[memberActiveRole;$guildID]]==false;]
+     $!memberAddRoles[$guildID;$authorID;$getGuildVar[memberActiveRole;$guildID]]
+     $sendMessage[1467318550925410485;
+     $title[O membro @$userTag é um membro ativo!]
+     $description[Média de atividade do servidor atual: **$getGuildVar[guildActivityMedia]**
 Cálculo atual: **$truncate[$math[$media[messageMonth;$guildMemberCount[1462224054676099094;;false]]/3]]**
 
 Mensagens enviadas neste mês: **$getMemberVar[messageMonth]**
@@ -64,27 +58,24 @@ Mensagens enviadas nesta semana: **$getMemberVar[messageWeekly]**
 Mensagens enviadas hoje: **$getMemberVar[messageToday]**
 Mensagens enviadas antes: **$getMemberVar[messageTotal]**
 ]
-$thumbnail[$userAvatar]
-$footer[$authorID]
-$color[Green]
-]
-;
-$onlyIf[$hasRoles[$guildID;$authorID;$getGuildVar[memberActiveRole;$guildID]]==true;]
-$!memberRemoveRoles[$guildID;$authorID;$getGuildVar[memberActiveRole;$guildID]]
-$sendMessage[1467318550925410485;
-$title[O membro @$userTag não é um membro ativo.]
-$description[
-Média de atividade do servidor atual: **$getGuildVar[guildActivityMedia]**
+     $thumbnail[$userAvatar]
+     $footer[$authorID]
+     $color[Green]]
+    ;
+     $onlyIf[$hasRoles[$guildID;$authorID;$getGuildVar[memberActiveRole;$guildID]]==true;]
+     $!memberRemoveRoles[$guildID;$authorID;$getGuildVar[memberActiveRole;$guildID]]
+     $sendMessage[1467318550925410485;
+     $title[O membro @$userTag não é um membro ativo.]
+     $description[Média de atividade do servidor atual: **$getGuildVar[guildActivityMedia]**
 Cálculo atual: **$media[messageMonth;]**
 
 Mensagens enviadas neste mês: **$getMemberVar[messageMonth]**
 Mensagens enviadas $usertag nesta semana: **$getMemberVar[messageWeekly]**
 Mensagens enviadas $usertag hoje: **$getMemberVar[messageToday]**
-Mensagens enviadas $usertag antes: **$getMemberVar[messageTotal]**
-]
-$thumbnail[$userAvatar]
-$footer[$authorID]
-$color[Red]
-]]
+Mensagens enviadas $usertag antes: **$getMemberVar[messageTotal]**]
+     $thumbnail[$userAvatar]
+     $footer[$authorID]
+     $color[Red]]
+    ]
 `
 }]
