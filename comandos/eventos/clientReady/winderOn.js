e2e90d7420f003eaa2d57809d38615235327bd5c
WillSilvah <williamfelipe235@gmail.com>
2026-03-12 15:17:29 -0300

    winderOn.js

--- comandos/eventos/clientReady/winderOn.js
+++ comandos/eventos/clientReady/winderOn.js
@@ -25,7 +25,7 @@ $username[$clientID] está ligado! >w<
 $setStatus[Online;Playing;$username[$clientID] está ligado! >w<]
 
 $setInterval[
-$setStatus[Idle;Playing;$parseInt[$ram] MB | $truncate[$math[($cpu/1000)*100]]% | $parseDigital[$uptime] | $pingms | $getGlobalVar[version]]
+$setStatus[Idle;Playing;$parseInt[$ram] MB | $truncate[$math[($cpu/1000)*100]]% | $pingms | $getGlobalVar[version] | $parseDigital[$uptime]]
 
 ;12s;ClientStatus]
 `
