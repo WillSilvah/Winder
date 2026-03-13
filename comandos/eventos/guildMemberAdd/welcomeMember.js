fe7ed875e4dbb3753a313e1c5347cd93b13adadf
WillSilvah <williamfelipe235@gmail.com>
2026-03-12 21:19:38 -0300

    welcomeMember.js

--- comandos/eventos/guildMemberAdd/welcomeMember.js
+++ comandos/eventos/guildMemberAdd/welcomeMember.js
@@ -2,7 +2,7 @@ module.exports = {
     type: "guildMemberAdd",
     code: `
     $setMemberVar[memberJoinedHere?;true]
-    
-$setGuildVar[membersJoinedMonth;$sum[$getGuildVar[membersJoinedMonth];1]]
+    $setMemberVar[inviter;$if[$inviterID[$guildID;$authorID]!=;$inviterID[$guildID;$authorID];$clientID];$authorID;$guildID]
+    $setGuildVar[membersJoinedMonth;$sum[$getGuildVar[membersJoinedMonth];1]]
 `
 }
