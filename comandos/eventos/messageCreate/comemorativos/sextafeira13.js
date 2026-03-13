0e40f9facc1449ff5da71e5ad5d2d068d56b4af3
WillSilvah <williamfelipe235@gmail.com>
2026-03-13 08:33:16 -0300

    sextafeira13.js

--- comandos/eventos/messageCreate/comemorativos/sextafeira13.js
+++ comandos/eventos/messageCreate/comemorativos/sextafeira13.js
@@ -8,7 +8,32 @@ module.exports = [{
 
     $addMessageReactions[$channelID;$messageID;💀]
 $sendMessage[$channelID; ### O <@$authorID> TEVE AZAR.
--# $randomText[Morreu de uma batata quente;Caiu em um buraco;Foi atropelado por um unicórnio;Tomou um golpe de maca;Foi engulado por um buraco negro;Morreu de tédio;Foi picado por uma abelha assassina;Caiu de uma escada;Morreu de risada;Foi vítima de um ataque de gatinhos;Tomou um tiro de Nerf;Foi esmagado por um piano;Morreu de sono;Foi devorado por um dragão;Caiu em um poço de lava;Morreu de fome em uma ilha deserta;Foi atingido por um raio;Morreu de vergonha;Foi vítima de um ataque de memes;Caiu de um prédio;Morreu de overdose de açúcar]
+-# $randomText[
+  Morreu de uma batata quente;
+  Caiu em um buraco;
+  Foi atropelado por um unicórnio;
+  Tomou um golpe de maca;
+  Foi engulado por um buraco negro;
+  Morreu de tédio;
+  Foi picado por uma abelha assassina;
+  Caiu de uma escada;
+  Morreu de risada;
+  Foi vítima de um ataque de gatinhos;
+  Tomou um tiro de Nerf;
+  Foi esmagado por um piano;
+  Morreu de sono;
+  Foi devorado por um dragão;
+  Caiu em um poço de lava;
+  Morreu de fome em uma ilha deserta;
+  Foi atingido por um raio;
+  Morreu de vergonha;
+  Foi vítima de um ataque de memes;
+  Caiu de um prédio;
+  Morreu de overdose de açúcar;
+  Morreu de tristeza;
+  Talaricou a mulher do chefe do morro
+]
+
 ]
     
 $!memberAddRoles[$guildID;$authorID;1481960745582202991]
