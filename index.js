898ba2e2f6e1718f2fba779b022f97a03b83782e
WillSilvah <williamfelipe235@gmail.com>
2026-03-13 14:38:40 -0300

    index.js

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ const client = new ForgeClient({
         "webhooksUpdate"
     ],
     prefixes: [
-        "w+", "w/"
+        "w+", "w/", "<@$clientID> "
     ],
     prefixCaseInsensitive: true,
     trackers: {
