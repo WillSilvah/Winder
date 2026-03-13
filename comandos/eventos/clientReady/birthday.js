d3d868963dc9047f842689a7f801f2a25e7458e7
WillSilvah <williamfelipe235@gmail.com>
2026-03-13 05:26:38 -0300

    birthday.js, winderOn.js

--- comandos/eventos/clientReady/birthday.js
+++ comandos/eventos/clientReady/birthday.js
@@ -5,22 +5,22 @@ module.exports = [{
     $let[guildID;1462224054676099094]
 
     $arrayLoad[birthday;, ;$guildMemberIDs[$get[guildID]]]
+     $arrayForEach[birthday;id;
+     $if[$getMemberVar[birthday;$env[id];$get[guildID]]==$day[TwoDigit]/$month[TwoDigit];
 
-    $arrayForEach[birthday;id;
-$if[$getMemberVar[birthday;$env[id];$get[guildID]]==$day[TwoDigit]/$month[TwoDigit];
-
-    $sendMessage[$getGuildVar[batePapo;$get[guildID]];
-    $addSection[
-        $addTextDisplay[## 🎂<@$env[id]> ESTÁ FAZENDO $math[$year-$getMemberVar[birthdayYear;$env[id];$get[guildID]]] ANOS HOJE!]
-    $addThumbnail[$userAvatar[$env[id]]]
-]
-    $addSeparator[Large;true]
-]
+     $sendMessage[$getGuildVar[batePapo;$get[guildID]];
+     $addSection[
+      $addTextDisplay[## 🎂<@$env[id]> ESTÁ FAZENDO $math[$year-$getMemberVar[birthdayYear;$env[id];$get[guildID]]] ANOS HOJE!]
+      $addThumbnail[$userAvatar[$env[id]]]
+     ]
+      $addSeparator[Large;true]
+      $addTextDisplay[<@&1463138741441790014>]
+     ]
 
     $let[msgid;$sendMessage[$getGuildVar[birthdayChannel;$get[guildID]];
     $addSection[
-        $addTextDisplay[## 🎂 HOJE É ANIVERSÁRIO DO @$userTag[$env[id]]!
-Está fazendo $math[$year-$getMemberVar[birthdayYear;$env[id];$get[guildID]]] anos.
+        $addTextDisplay[## 🎂 HOJE É ANIVERSÁRIO DO @$userTag[$env[id]]!]
+        $addTextDisplay[Está fazendo $math[$year-$getMemberVar[birthdayYear;$env[id];$get[guildID]]] anos.]
 ]
     $addThumbnail[$userAvatar[$env[id]]]
 ]
