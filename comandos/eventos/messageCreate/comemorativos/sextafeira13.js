c61304195995760c530d84b8258c6006e785d2d8
WillSilvah <williamfelipe235@gmail.com>
2026-03-13 13:02:20 -0300

    sextafeira13.js

--- comandos/eventos/messageCreate/comemorativos/sextafeira13.js
+++ comandos/eventos/messageCreate/comemorativos/sextafeira13.js
@@ -3,6 +3,7 @@ module.exports = [{
      code: `
      $timezone[America/Recife]
      $onlyIf[$weekDay[Long]-$day==Friday-13;]
+      $setGuildVar[evento_sextafeira13;$math[$day[TwoDigit]+1];$guildID]
      $onlyIf[$randomNumber[1;100]<=5;]
      $onlyIf[$hasRoles[$guildID;$authorID;1481960745582202991]==false;]
 
@@ -10,9 +11,6 @@ module.exports = [{
 $sendMessage[$channelID;### O <@$authorID> TEVE AZAR.
 -# $randomText[Atropelado por uma bicleta;Simplesmente faleceu;Morreu de uma batata quente;Caiu em um buraco;Foi atropelado por um unicórnio;Tomou um golpe de faca;Foi engulido por um buraco negro;Morreu de tédio;Foi picado por uma abelha assassina;Caiu de uma escada;Morreu de risada;Foi vítima de um ataque de gatinhos;Tomou um tiro de Nerf;Foi esmagado por um piano;Morreu de sono;Foi devorado por um dragão;Caiu em um poço de lava;Morreu de fome em uma ilha deserta;Foi atingido por um raio;Morreu de vergonha;Foi vítima de um ataque de memes;Caiu de um prédio;Morreu de overdose de açúcar]
 ]
-
-
-$getGuildVar[evento_sextafeira13;$math[$day[TwoDigit]+1];$guildID]
     
 $!memberAddRoles[$guildID;$authorID;1481960745582202991]
 
