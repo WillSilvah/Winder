d36ae6a8debca0799572756001a59e2b0493152a
WillSilvah <williamfelipe235@gmail.com>
2026-03-13 21:23:11 -0300

    autoRankDaily.js

--- comandos/eventos/clientReady/autoRankDaily.js
+++ comandos/eventos/clientReady/autoRankDaily.js
@@ -13,7 +13,7 @@ $addSection[
 $addTextDisplay[### $toUpperCase[TEMPORADA $env[session;number]: $env[session;name]]]
 $addThumbnail[$guildIcon[$get[guildID]]]
 $addTextDisplay[
-$memberLeaderboard[messageMonth;$get[guildID];desc;11;1;\n;data;pos;$return[$env[pos]° **@$username[$env[data;id]]**
+$memberLeaderboard[messageMonth;$get[guildID];desc;50;1;\n;data;pos;$return[$env[pos]° **@$username[$env[data;id]]**
 > 💬 $separateNumber[$env[data;value];.] ✨️ $separateNumber[$getMemberVar[pdaMonth;$env[data;id];$get[guildID];0];.] ⬆️ $getMemberVar[votesMonth;$env[data;id];$get[guildID];0]]]
 ]
 ]
