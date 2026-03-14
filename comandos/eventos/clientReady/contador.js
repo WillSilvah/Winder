7d12d56f292e84f38f5827ad9a1b2fec2c69e2ee
WillSilvah <williamfelipe235@gmail.com>
2026-03-14 17:20:23 -0300

    contador.js

--- comandos/eventos/clientReady/contador.js
+++ comandos/eventos/clientReady/contador.js
@@ -3,12 +3,14 @@ module.exports = {
  code: `
  
     $advancedInterval[$esc[
+    $log[Voice_Statdocks atualizado]
      $!setChannelName[1465535257297027214;🐾 | 👤$separateNumber[$guildMemberCount[1462224054676099094;;false];.] 🟢$math[$guildMemberCount[1462224054676099094;online;false]+$guildMemberCount[1462224054676099094;dnd;false]+$guildMemberCount[1462224054676099094;idle;false]]]
      $!setChannelName[1463122813736063016;⭐️ | falta $sub[$getGuildVar[membersMeta;1462224054676099094];$guildMemberCount[1462224054676099094;;false]] peludos]
      $!setChannelName[1465534760620392722;🔥 | 💬 $separateNumber[$getGuildVar[guildMonthMessages;1462224054676099094];.] 👤$getMemberLeaderboardLength[messageMonth;1462224054676099094]]
     ];10m;Voice_Statdocks]
 
     $advancedInterval[$esc[
+     $log[BatePapo_Stats atualizado]
      $!setChannelTopic[$getGuildVar[batePapo;1462224054676099094];(👤💬 $getMemberLeaderboardLength[messageToday;1462224054676099094]) Onde a fauna se encontra.
 Leia as #👋│diretrizes para ficar de acordo com a ordem! :dizzy:]
 
