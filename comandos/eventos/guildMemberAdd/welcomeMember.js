8078446f55fd18490b92571e151be508674d07c2
WillSilvah <williamfelipe235@gmail.com>
2026-03-16 10:36:06 -0300

    joinedFirstTime.js, welcomeMember.js, tempVarsOnLeave.js, userLastOnTimestamp.js, perfil.js

--- comandos/eventos/guildMemberAdd/welcomeMember.js
+++ comandos/eventos/guildMemberAdd/welcomeMember.js
@@ -1,15 +1,16 @@
 module.exports = {
     type: "guildMemberAdd",
     code: `
-$setGuildVar[membersJoinedMonth;$sum[$getGuildVar[membersJoinedMonth;$guildID;0];1]]
-$setMemberVar[inviter;$if[$inviterID[$guildID;$authorID]!=;$inviterID[$guildID;$authorID];$clientID];$authorID;$guildID]
+    $setMemberVar[memberJoinedFirstTime;$memberJoinedAt[$guildID;$authorID];$authorID;$guildID]
+    $setGuildVar[membersJoinedMonth;$sum[$getGuildVar[membersJoinedMonth;$guildID;0];1]]
+    $setMemberVar[inviter;$if[$inviterID[$guildID;$authorID]!=;$inviterID[$guildID;$authorID];$clientID];$authorID;$guildID]
 
 $sendMessage[$getGuildVar[batePapo];
 $if[$isBot[$authorID]==false;
 $addContainer[
 $addSection[
 $addTextDisplay[$if[$getMemberVar[memberJoinedHere?;$authorID;$guildID;false]==false;### 👋 <@$authorID> BEM VINDO!;### 🎉 <@$authorID> BEM VINDO DE VOLTA!]
-**Sinta-se em casa, aproveite bem a nossa comunidade! **
+**Sinta-se em casa, aproveite bem a nossa comunidade!**
 ]
 $addTextDisplay[-# Veio pelo: $if[$inviterID[$guildID;$authorID]!=;@$username[$inviterID[$guildID;$authorID]];@Fulano] | ID: $authorID]
 $addThumbnail[$userAvatar]
