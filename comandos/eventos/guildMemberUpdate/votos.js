dc472c2ab783a22f5f2633debf783da17f198c5f
WillSilvah <williamfelipe235@gmail.com>
2026-03-17 08:38:21 -0300

    votos.js, chamadas.js

--- comandos/eventos/guildMemberUpdate/votos.js
+++ comandos/eventos/guildMemberUpdate/votos.js
@@ -15,12 +15,12 @@ module.exports = {
      ]
      $addSeparator[Large;true]
     ]
+    
+    $setMemberVar[nextVoteRemindTime;$math[$getTimestamp+$parseString[6h]]]
 
     $if[$isUserDMEnabled==true;
      $sendDM[$authorID;Obrigado por ter votado! Lembrarei você de votar às $discordTimestamp[$getMemberVar[nextVoteRemindTime;$authorID];ShortDateMediumTime]. Use o comando /lembrete voto para desativar ou ativar os lembretes na DM!]
     ]
-
-    $setMemberVar[nextVoteRemindTime;$math[$getTimestamp+$parseString[6h]]]
 
     $advancedTimeout[$esc[
      $sendDM[{0};
--- comandos/eventos/interactionCreate/cargos/chamadas.js
+++ comandos/eventos/interactionCreate/cargos/chamadas.js
@@ -5,15 +5,13 @@ module.exports = [{
     $ephemeral
     $interactionReply[
      $addContainer[
-      $addSection[
-       $addTextDisplay[## CHAMADAS!]
-       $addTextDisplay[**💬! 🔉!**]
-       $addTextDisplay[Sabia que você pode ser mencionado no bate papo para conversar sobre um assunto?]
-       $addTextDisplay[-# - Os cargos de chamadas podem ser mencionados por **qualquer pessoa** durante **06:00** até **22:00** no horário de Pernambuco.]
-       $addTextDisplay[> **⚠️ Lembre-se, pode acontecer de receber várias menções**]
-       $addThumbnail[https://abs.twimg.com/emoji/v2/72x72/1f44b.png]
-       ]
-      ;Yellow]
+     $addSection[
+     $addTextDisplay[## CHAMADAS!]
+     $addTextDisplay[**💬! 🔉!**]
+     $addTextDisplay[Sabia que você pode ser mencionado no bate papo para conversar sobre um assunto?]
+     $addTextDisplay[-# - Os cargos de chamadas podem ser mencionados por **qualquer pessoa** durante **06:00** até **22:00** no horário de Pernambuco.]
+     $addTextDisplay[> **⚠️ Lembre-se, pode acontecer de receber várias menções**]
+     $addThumbnail[https://abs.twimg.com/emoji/v2/72x72/1f44b.png]];Yellow]
      $addActionRow
      $addStringSelectMenu[chamadas;Escolha uma notificação;false;1;1]
        $addOption[Chamar para conversar;Seja chamado para conversar sobre algum assunto.;conversar;💬;false]
