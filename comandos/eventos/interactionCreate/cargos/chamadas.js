56271070240a678071b88c910cbb5bc076a4fabb
Williams Felipe <49321188+WillSilvah@users.noreply.github.com>
2026-03-17 10:30:22 -0300

    Refactor text display in chamadas.js

--- comandos/eventos/interactionCreate/cargos/chamadas.js
+++ comandos/eventos/interactionCreate/cargos/chamadas.js
@@ -8,18 +8,18 @@ module.exports = [{
      $addSection[
      $addTextDisplay[## CHAMADAS!]
      $addTextDisplay[**💬! 🔉!**]
-     $addTextDisplay[Sabia que você pode ser mencionado no bate papo para conversar sobre um assunto?\n-# Os cargos de chamadas podem ser mencionados por qualquer pessoa durante 06:00 até 22:00 no horário de Pernambuco.]
+     $addTextDisplay[Sabia que você pode ser mencionado no bate papo para conversar sobre um assunto?]
+     $addTextDisplay[-# Os cargos de chamadas podem ser mencionados por qualquer pessoa durante 06:00 até 22:00 no horário de Pernambuco.]
      $addTextDisplay[> ⚠️ Lembre-se, pode acontecer de receber várias menções]
      $addThumbnail[https://abs.twimg.com/emoji/v2/72x72/1f44b.png]
      ]
      ;Yellow]
-     ]
      $addActionRow
      $addStringSelectMenu[chamadas;Escolha uma notificação;false;1;1]
        $addOption[Chamar para conversar;Seja chamado para conversar sobre algum assunto.;conversar;💬;false]
        $addOption[Chamar para calls;Gosta de participar de Calls?;call;🔉;false]
        $addOption[Não quero receber chamadas;É incômodo?;remove;❌;false]
-       
+]
 `
 },{
     type: "interactionCreate",
