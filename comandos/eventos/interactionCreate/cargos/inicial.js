eb4ebd48fb0300380b478e6a0948075251812161
WillSilvah <williamfelipe235@gmail.com>
2026-03-16 21:01:26 -0300

    inicial.js

--- comandos/eventos/interactionCreate/cargos/inicial.js
+++ comandos/eventos/interactionCreate/cargos/inicial.js
@@ -6,11 +6,8 @@ module.exports = [{
     $let[msgID;$sendMessage[1470875996356874292;
     $disableAllMentions
     $addContainer[
-     $addSection[
       $addTextDisplay[## GERENCIAR CARGOS!]
       $addTextDisplay[Tem muitos cargos interessantes na Patinhas para poder pegar!]
-      $addThumbnail[https://abs.twimg.com/emoji/v2/72x72/1f514.png]
-     ]
     $addSeparator[Large;true]
     $addActionRow
      $addButton[bnotificar;Receber notificações;Success;🔔;false]
