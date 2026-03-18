72ef6fa5bca33358937001b1a93ee5e60a7d226a
Williams Felipe <49321188+WillSilvah@users.noreply.github.com>
2026-03-18 10:31:17 -0300

    Add reaction cooldown to messageCreate event

--- comandos/eventos/messageCreate/reactions.js
+++ comandos/eventos/messageCreate/reactions.js
@@ -2,6 +2,7 @@ module.exports = [{
   type: "messageCreate",
   code: `
   $if[$includes[$toLowerCase[$message];kkkk]==true;
+   $cooldown[$guildID;10s]
    $!addMessageReactions[$channelID;$messageID;$randomText[😂;🤣]]
   ]
   
