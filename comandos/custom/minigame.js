c6033d4a88a6d77879ee1abfa9a5a2fd3138c2ec
WillSilvah <williamfelipe235@gmail.com>
2026-03-19 17:16:49 -0300

    minigame.js, minigame.js

--- comandos/custom/minigame.js
+++ comandos/custom/minigame.js
@@ -47,6 +47,7 @@ module.exports = [{
       ]
      ]
     $setGuildVar[minigameExecuteCount;$math[$getGuildVar[minigameExecuteCount;$env[guildID];0]+1];$env[guildID]]
+    $setGuildVar[minigameTimestamp;$env[guildID]]
      $wait[1m]
       $onlyIf[$getGuildVar[minigameStatus;$env[guildID]]==true;]
       $setGuildVar[minigameStatus;false;$env[guildID]]
--- comandos/eventos/clientReady/minigame.js
+++ comandos/eventos/clientReady/minigame.js
@@ -12,34 +12,39 @@ module.exports = [{
     },{
     type: 'messageCreate',
     code: `
-$onlyIf[$channelID==$getGuildVar[batePapo];]
-$onlyIf[$getGuildVar[minigameStatus;$guildID;false]==true;]
-$onlyIf[$if[$includes[$getGuildVar[minigameType];fraseRepeat]==true;$message;$toLowerCase[$message]]==$if[$includes[$getGuildVar[minigameType];fraseRepeat]==true;$getGuildVar[minigameWord;$guildID];$toLowerCase[$getGuildVar[minigameWord;$guildID]]];]
-$startTyping[$channelID]
-$setGuildVar[minigameStatus;false;$guildID]
-$setGuildVar[minigameWord;;$guildID]
+    $onlyIf[$channelID==$getGuildVar[batePapo];]
+    $onlyIf[$getGuildVar[minigameStatus;$guildID;false]==true;]
+    $onlyIf[$if[$includes[$getGuildVar[minigameType];fraseRepeat]==true;$message;$toLowerCase[$message]]==$if[$includes[$getGuildVar[minigameType];fraseRepeat]==true;$getGuildVar[minigameWord;$guildID];$toLowerCase[$getGuildVar[minigameWord;$guildID]]];]
 
-$let[xpDrop;$if[$hasRoles[$guildID;$authorID;$getGuildVar[memberVerifiedRole]]==true;$randomNumber[$getGuildVar[minigameXPmin;$guildID];$getGuildVar[minigameXPmax;$guildID]];0]]
-
-$setMemberVar[pdaMonth;$sum[$getMemberVar[pdaMonth;$authorID;$guildID;0];$get[xpDrop]];$authorID]
-$setMemberVar[pdaTotal;$sum[$getMemberVar[pdaTotal;$authorID;$guildID;0];$get[xpDrop]];$authorID]
-
-$!addMessageReactions[$channelID;$messageID;🏆]
-
-$setMemberVar[minigameMonthWins;$sum[$getMemberVar[minigameMonthWins;$authorID;$guildID;0];1];$authorID]
-$setMemberVar[minigameTotalWins;$sum[$getMemberVar[minigameTotalWins;$authorID;$guildID;0];1];$authorID]
-
-$sendMessage[$channelID;<@$authorID> | Parabéns, você ganhou ✨**+$get[xpDrop] Pontos de atividade!**!
-> **Ganhou 🏆$getMemberVar[minigameMonthWins;$authorID] vezes neste mês!**
-]
-
-$setGuildVar[minigameGuildWins;$math[$getGuildVar[minigameGuildWins;$guildID;0]+1];$guildID]
-
-$!memberRemoveRoles[$guildID;$getGuildVar[minigameLastUser;$guildID];$getGuildVar[lastWinMemberRole]]
-$wait[1s]
-$!memberAddRoles[$guildID;$authorID;$getGuildVar[lastWinMemberRole]]
-$setGuildVar[minigameLastUser;$authorID;$guildID]
-
-$setGuildVar[minigameEmit;false;$guildID]
+    $startTyping[$channelID]
+    $setGuildVar[minigameStatus;false;$guildID]
+    $setGuildVar[minigameWord;;$guildID]
+    $let[xpDrop;$if[$hasRoles[$guildID;$authorID;$getGuildVar[memberVerifiedRole]]==true;$randomNumber[$getGuildVar[minigameXPmin;$guildID];$getGuildVar[minigameXPmax;$guildID]];0]]
+    $setMemberVar[pdaMonth;$sum[$getMemberVar[pdaMonth;$authorID;$guildID;0];$get[xpDrop]];$authorID]
+    $setMemberVar[pdaTotal;$sum[$getMemberVar[pdaTotal;$authorID;$guildID;0];$get[xpDrop]];$authorID]
+    $!addMessageReactions[$channelID;$messageID;🏆]
+    $setMemberVar[minigameMonthWins;$sum[$getMemberVar[minigameMonthWins;$authorID;$guildID;0];1];$authorID]
+    $setMemberVar[minigameTotalWins;$sum[$getMemberVar[minigameTotalWins;$authorID;$guildID;0];1];$authorID]
+    $let[time;$getTimestamp]
+    
+    $sendMessage[$channelID;
+     $addSection[
+       $addTextDisplay[## <@$authorID> VENCEU!]
+       $addTextDisplay[✨**+$get[xpDrop]**] 
+       $addThumbnail[$userAvatar]
+     ]
+    ]
+    $startTyping[$channelID]
+    $setGuildVar[minigameGuildWins;$math[$getGuildVar[minigameGuildWins;$guildID;0]+1];$guildID]
+    $!memberRemoveRoles[$guildID;$getGuildVar[minigameLastUser;$guildID];$getGuildVar[lastWinMemberRole]]
+    $wait[1s]
+    $!memberAddRoles[$guildID;$authorID;$getGuildVar[lastWinMemberRole]]
+    $setGuildVar[minigameLastUser;$authorID;$guildID]
+    $setGuildVar[minigameEmit;false;$guildID]
+    
+    $sendMessage[$channelID;
+     $addTextDisplay[### <@$authorID> ganhou 🏆 **$getMemberVar[minigameMonthWins;$authorID;$guildID;0]** vezes neste mês e está em **$getMemberLeaderboardValue[minigameMonthWins;desc;$authorID;$guildID]°** lugar]
+    ]
+    
 `
 }]
