544398f45d434fc77c47dada077dd6d99f984cf0
WillSilvah <williamfelipe235@gmail.com>
2026-03-19 11:59:46 -0300

    staff.js, verificado.js

--- comandos/eventos/messageCreate/autoreply/staff.js
+++ comandos/eventos/messageCreate/autoreply/staff.js
@@ -4,13 +4,11 @@ module.exports = [{
 $onlyIf[$mentioned[0]==$authorID;]
 $reply
 $if[$includes[$toLowercase[$message];como]|$includes[$toLowercase[$message];viro;entro;participo;participar]|$includes[$toLowercase[$message];staff;mod;adm;admiro;admin;moderador;administração;administrador]==true|true|true;
-$if[$hasRoles[$guildID;$authorID;$getGuildVar[memberVerifiedRole]]==true;
 $startTyping[$channelID]
 $wait[2s]
+$if[$hasRoles[$guildID;$authorID;$getGuildVar[memberVerifiedRole]]==true;
 $sendMessage[$channelID;Que ótimo que você quer participar da Equipe Patinhas! Primeiramente, responda o formulário que está no canal <#1466894271457136821> com **sinceridade** e **atenção**. Desejo boa sorte! 😄
 ];
-$startTyping[$channelID]
-$wait[2s]
 $sendMessage[$channelID;É de fato interessante participar da Equipe Patinhas, mas lhe aviso logo que **você não está apto a se entrar na equipe** pois não tem o cargo **Peludo Verificado**. Isso significa que você não enviou 200 mensagens aqui ainda, caso consiga, o canal <#1466894271457136821> estará disponível para você. 🫠
 ]
 ]
---
+++ comandos/eventos/messageCreate/autoreply/verificado.js
@@ -0,0 +1,15 @@
+module.exports = [{
+    type: "messageCreate",
+    code: `
+    $reply $disableRoleMentions
+    $if[$includes[$toLowercase[$message];como;enviar;mandar;envio]|$includes[$toLowercase[$message];image;mídia;arte;anexo;vídeo]==true|true;
+    $startTyping[$channelID]
+    $wait[2s]
+    $if[$hasRoles[$guildID;$authorID;$getGuildVar[memberVerifiedRole]]==true;
+$sendMessage[$channelID;Para conseguir a permissão de enviar mídia nos canais de texto, você precisa ter o cargo <@&$getGuildVar[memberVerifiedRole]>. Você ganha com **200 mensagens** (Falta **$math[200-$getMemberVar[messageTotal]]** mensagens), lembrarei quando você ganhar o cargo.];
+$sendMessage[$channelID;Você **já pode** enviar mídias nos canais.]
+$sendMessage[$channelID;$sticker[1483454059447976108]]
+]
+]    
+`
+}]
