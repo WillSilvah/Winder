e479677512e419fd9ff4aa73eef9075a845931ae
WillSilvah <williamfelipe235@gmail.com>
2026-03-18 22:04:26 -0300

    backup.js, memberAwaitForm.js

--- comandos/eventos/clientReady/backup.js
+++ comandos/eventos/clientReady/backup.js
@@ -6,7 +6,7 @@ $sendMessage[788131713418002463;
 WinderDB_$day[TwoDigit]-$month[TwoDigit]_$hour[TwoDigit]$minute[TwoDigit]
 $attachment[database/forge.db;forge.db]
 ]
-$advanedInterval[
+$advancedInterval[
 $timezone[America/Recife]
 $sendMessage[788131713418002463;
 WinderDB_$day[TwoDigit]-$month[TwoDigit]_$hour[TwoDigit]$minute[TwoDigit]
--- comandos/eventos/guildMemberUpdate/memberAwaitForm.js
+++ comandos/eventos/guildMemberUpdate/memberAwaitForm.js
@@ -1,15 +1,21 @@
 module.exports = {
  type: 'guildMemberUpdate',
- code: `$stop
+ code: `
  $onlyIf[$checkContains[$oldMember[addedRoles];1467496885664092201]==true;]
 
  $if[$isUserDMEnabled[$authorID]==true;
   $sendDM[$authorID;
    $addContainer[
-   $addTextDisplay[### RECEBEMOS SEU FORMULÁRIO!]
-]
-]
-]
+    $addSection[
+    $addTextDisplay[## RECEBEMOS O SEU FORMULÁRIO!]
+    $addTextDisplay[Em breve, você poderá receber uma resposta sobre a sua candidatura, não vai demorar muito.]
+    $addThumbnail[https://i.ibb.co/F47676pR/pats-orange-light.png]
+    ]
+   ;Orange]
+  ]
+ ]
+ 
+ $memberRemoveRoles[$guildID;$authorID;1467496885664092201]
 
 `
 }
