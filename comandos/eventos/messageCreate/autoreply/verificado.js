1a42e7fad533da9e3c027fa675cd2a492c6fd5e6
WillSilvah <williamfelipe235@gmail.com>
2026-03-20 08:52:59 -0300

    verificado.js

--- comandos/eventos/messageCreate/autoreply/verificado.js
+++ comandos/eventos/messageCreate/autoreply/verificado.js
@@ -2,13 +2,13 @@ module.exports = [{
     type: "messageCreate",
     code: `
     $reply $disableRoleMentions
-    $if[$includes[$toLowercase[$message];como;enviar;mandar;envio]|$includes[$toLowercase[$message];image;mídia;arte;foto;photo;anexo;vídeo]==true|true;
+    $if[$includes[$toLowercase[$message];como;enviar;mandar;envio]|$includes[$toLowercase[$message];image;mídia;midia;arte;foto;photo;anexo;vídeo]==true|true;
     $startTyping[$channelID]
     $wait[2s]
     $if[$hasRoles[$guildID;$authorID;$getGuildVar[memberVerifiedRole]]==false;
-$sendMessage[$channelID;Para conseguir a permissão de enviar mídia nos canais de texto, você precisa ter o cargo <@&$getGuildVar[memberVerifiedRole]>. Você ganha com **200 mensagens** (Falta **$math[200-$getMemberVar[messageTotal]]** mensagens), lembrarei quando você ganhar o cargo.];
-$sendMessage[$channelID;Mas você **já pode** enviar mídias nos canais.]
-$sendMessage[$channelID;$sticker[1483454059447976108]]
+    $sendMessage[$channelID;Para conseguir a permissão de enviar mídia nos canais de texto, você precisa ter o cargo <@&$getGuildVar[memberVerifiedRole]>. Você ganha com **200 mensagens** (Falta **$math[200-$getMemberVar[messageTotal]]** mensagens), lembrarei quando você ganhar o cargo.];
+    $sendMessage[$channelID;Mas você **já pode** enviar mídias nos canais.]
+    $sendMessage[$channelID;$sticker[1483454059447976108]]
 ]
 ]    
 `
