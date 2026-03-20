ba8029ab8f5f667691b29c77ea6f13e2ab6d7967
WillSilvah <williamfelipe235@gmail.com>
2026-03-20 08:56:17 -0300

    perfil.js

--- comandos/slashCommands/perfil.js
+++ comandos/slashCommands/perfil.js
@@ -16,7 +16,7 @@ $addThumbnail[$userAvatar[$get[userID]]]
 $if[$memberExists[$guildID;$get[userID]]==true;$addSeparator[Large;true]
 $addTextDisplay[
 👋 Entrou na patinhas $discordTimestamp[$memberJoinedAt[$guildID;$get[userID]];RelativeTime] em $memberJoinPosition[$guildID;$get[userID]]° lugar
-💌 Veio pelo: **$if[$inviterID[$guildID;$get[userID]]!=;@$username[$inviterID[$guildID;$get[userID]]];@Fulano]**
+💌 Veio pelo: **$userTag[$getMemberVar[inviter;$get[userID]]]**
 💬 Enviou mensagem $discordTimestamp[$getMemberVar[lastMessageTimestamp;$get[userID]];RelativeTime]
 ]
 ;
