75bf442b5b8ccb4f71d0dbdb4fa5ddc03ed17c44
WillSilvah <williamfelipe235@gmail.com>
2026-03-21 22:01:31 -0300

    Update 1 file: messageLogger.js

--- comandos/eventos/messageCreate/messageLogger.js
+++ comandos/eventos/messageCreate/messageLogger.js
@@ -4,7 +4,8 @@ module.exports = [{
     $onlyIf[$getMemberVar[privacyMode;$authorID;$guildID;false]==false;]
     $timezone[America/Recife]
     $setMemberVar[messageLogToday;$hour[TwoDigit]:$minute[TwoDigit]:$second[TwoDigit] - $day[TwoDigit]/$month[TwoDigit]/$year[TwoDigit] | M: $messageID | C: $channelID\n$message$if[$messageAttachments[$channelID;$messageID]!=;\n$messageAttachments[$channelID;$messageID;\n];]\n$getMemberVar[messageLogToday]]
-    $setMemberVar[messageLogWeekly;$hour[TwoDigit]:$minute[TwoDigit]:$second[TwoDigit] - $day[TwoDigit]/$month[TwoDigit]/$year[TwoDigit] | M: $messageID | C: $channelID\n$message$if[$messageAttachments[$channelID;$messageID]!=;\n$messageAttachments[$channelID;$messageID;\n];]\n$getMemberVar[messageLogWeek]]
+    $setMemberVar[messageLogWeekly;$hour[TwoDigit]:$minute[TwoDigit]:$second[TwoDigit] - $day[TwoDigit]/$month[TwoDigit]/$year[TwoDigit] | M: $messageID | C: $channelID\n$message$if[$messageAttachments[$channelID;$messageID]!=;\n$messageAttachments[$channelID;$messageID;\n];]\n$getMemberVar[messageLogWeekly]]
+    
     $setMemberVar[messageLogMonth;$hour[TwoDigit]:$minute[TwoDigit]:$second[TwoDigit] - $day[TwoDigit]/$month[TwoDigit]/$year[TwoDigit] | M: $messageID | C: $channelID\n$message$if[$messageAttachments[$channelID;$messageID]!=;\n$messageAttachments[$channelID;$messageID;\n];]\n$getMemberVar[messageLogMonth]]
 `
 }]
