b29fba3507eb99b75d6ac75d6651686a4a1e6536
WillSilvah <williamfelipe235@gmail.com>
2026-03-22 09:27:59 -0300

    Update 1 file: desavisar.js

--- comandos/slashCommands/desavisar.js
+++ comandos/slashCommands/desavisar.js
@@ -5,7 +5,7 @@ module.exports = {
     $let[staffID;$authorID]
     $let[type;desavisado]
     
-    $onlyIf[$getMemberVar[warnList;$get[userID]]=={};<@$authorID> O **@$userTag** não tem nenhum aviso.]
+    $onlyIf[$getMemberVar[warnList;$get[userID]]!={};<@$authorID> O **@$userTag[$get[userID]]** não tem nenhum aviso.]
 
     $let[memberWarns;$getMemberVar[warns;$get[userID];$get[guild];1]]
     
