717ac6c7e4e11a625d961de3e6465cc360193716
WillSilvah <williamfelipe235@gmail.com>
2026-03-22 08:10:47 -0300

    Update 1 file: roleMentionsCooldown.js

--- comandos/eventos/messageCreate/defensor/roleMentionsCooldown.js
+++ comandos/eventos/messageCreate/defensor/roleMentionsCooldown.js
@@ -1,19 +1,20 @@
 module.exports = [{
     type: "messageCreate",
     code: `
-    $let[talkRole;$getGuildVar[guildCallTalkRole]]
-    $let[voiceRole;$getGuildVar[guildCallVoiceRole]]
-    
-    $if[$isRoleMentioned[$channelID;$messageID;$get[talkRole]]==true;
-     $!editRole[1462224054676099094;$get[talkRole];;;;;false;]
-     $!memberAddRoles[$guildID;$authorID;$get[talkRole]]
-     $advancedTimeout[$esc[$!editRole[1462224054676099094;{0};;;;;true;]];30m;talkRole_Cooldown;$get[talkRole]]
-    ]
-    
-    $if[$isRoleMentioned[$channelID;$messageID;$get[voiceRole]]==true;
-     $!editRole[1462224054676099094;$get[voiceRole];;;;;false;]
-     $!memberAddRoles[$guildID;$authorID;$get[voiceRole]]
-     $advancedTimeout[$esc[$!editRole[1462224054676099094;{0};;;;;true;]];30m;voiceRole_Cooldown;$get[voiceRole]]
-    ]
+$let[talkRole;$getGuildVar[guildCallTalkRole]]
+$let[voiceRole;$getGuildVar[guildCallVoiceRole]]
+
+$if[$isRoleMentioned[$channelID;$messageID;$get[talkRole]]==true;
+  $!editRole[$guildID;$get[talkRole];;;;;false]
+  $!memberAddRoles[$guildID;$authorID;$get[talkRole]]
+  $advancedTimeout[$esc[$!editRole[{1};{0};;;;;true]];30m;talkRole_Cooldown;$get[talkRole];$guildID]
+]
+
+$if[$isRoleMentioned[$channelID;$messageID;$get[voiceRole]]==true;
+  $!editRole[$guildID;$get[voiceRole];;;;;false]
+  $!memberAddRoles[$guildID;$authorID;$get[voiceRole]]
+  $advancedTimeout[$esc[$!editRole[{1};{0};;;;;true]];30m;voiceRole_Cooldown;$get[voiceRole];$guildID]
+]
+
 `
 }]    
