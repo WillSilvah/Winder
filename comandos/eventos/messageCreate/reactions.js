b5f69c69b5ca9ecaf6b40a73bb83bf0196c74a95
WillSilvah <williamfelipe235@gmail.com>
2026-03-21 22:18:22 -0300

    Update 1 file: reactions.js

--- comandos/eventos/messageCreate/reactions.js
+++ comandos/eventos/messageCreate/reactions.js
@@ -10,10 +10,6 @@ module.exports = [{
    
    $!addMessageReactions[$channelID;$messageID;💀]
   ]
-  $if[$includes[$toLowerCase[$message];bom;boa]-$includes[$toLowerCase[$message];dia;tarde;noite]==true-true;
-   
-   $!addMessageReactions[$channelID;$messageID;👋]
-  ]
   $if[$includes[$toLowerCase[$message];olá;oi;hello;hi;howdy]==true;
    $onlyIf[$argsCount[$message]<3;]
    
