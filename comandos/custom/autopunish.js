e6ef93ac54920a30a112bb6c242921619a70ee3b
WillSilvah <williamfelipe235@gmail.com>
2026-03-23 21:41:40 -0300

    Update 1 file: autopunish.js

--- comandos/custom/autopunish.js
+++ comandos/custom/autopunish.js
@@ -3,6 +3,25 @@ module.exports = [{
     code: `
     $ifx[
      $if[$getMemberVar[warnsTotal;$get[userID]]==1;
+      $let[time;2h]
+      $winderMute
      ]
-]`
+     $elseIf[$getMemberVar[warnsTotal;$get[userID]]==2;
+      $let[time;12h]
+      $winderMute
+     ]
+     $elseIf[$getMemberVar[warnsTotal;$get[userID]]==3;
+      $let[time;24h]
+      $winderMute
+     ]
+     $elseIf[$getMemberVar[warnsTotal;$get[userID]]==4;
+      $let[time;72h]
+      $winderMute
+      $winderKick
+     ]
+     $elseIf[$getMemberVar[warnsTotal;$get[userID]]>=5;
+      $winderBan
+     ]
+    ]
+`
 }]
