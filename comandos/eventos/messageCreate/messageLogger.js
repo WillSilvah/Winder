e029c6b7666f73e7c3bfac05a7f696c9ded30abc
Williams Felipe <49321188+WillSilvah@users.noreply.github.com>
2026-03-24 11:05:08 -0300

    Update messageLogger.js

--- comandos/eventos/messageCreate/messageLogger.js
+++ comandos/eventos/messageCreate/messageLogger.js
@@ -4,7 +4,7 @@ module.exports = [{
     $timezone[America/Recife] $c[Fuso horário de Pernambuco, padrão em todo o sistema]
     $onlyIf[$getMemberVar[privacyMode;$authorID;$guildID;false]==false;] $c[Por enquanto não utilizado, mas permite que membros especificos possam não ser registrados, permitindo que não tenha tanto dado salvo na database no Winder]
 
-    $c[Registra todas as mensagens do membro baseando si em dia, semana e mês, sendo resetado todo dia de meia noite, domingo de meia noite e no dia 1 de todo mês de meia noite]
+    $c[Registra todas as mensagens do membro baseando em dia, semana e mês, sendo resetado todo dia de meia noite, domingo de meia noite e no dia 1 de todo mês de meia noite]
     $setMemberVar[messageLogToday;$hour[TwoDigit]:$minute[TwoDigit]:$second[TwoDigit] - $day[TwoDigit]/$month[TwoDigit]/$year[TwoDigit] | M: $messageID | C: $channelID\n$message$if[$messageAttachments[$channelID;$messageID]!=;\n$messageAttachments[$channelID;$messageID;\n];]\n$getMemberVar[messageLogToday]]
     $setMemberVar[messageLogWeekly;$hour[TwoDigit]:$minute[TwoDigit]:$second[TwoDigit] - $day[TwoDigit]/$month[TwoDigit]/$year[TwoDigit] | M: $messageID | C: $channelID\n$message$if[$messageAttachments[$channelID;$messageID]!=;\n$messageAttachments[$channelID;$messageID;\n];]\n$getMemberVar[messageLogWeekly]]
     $setMemberVar[messageLogMonth;$hour[TwoDigit]:$minute[TwoDigit]:$second[TwoDigit] - $day[TwoDigit]/$month[TwoDigit]/$year[TwoDigit] | M: $messageID | C: $channelID\n$message$if[$messageAttachments[$channelID;$messageID]!=;\n$messageAttachments[$channelID;$messageID;\n];]\n$getMemberVar[messageLogMonth]]
