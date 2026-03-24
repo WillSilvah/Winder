080a69733fdc74c7e70e6ae282ea2a741b70592f
Williams Felipe <49321188+WillSilvah@users.noreply.github.com>
2026-03-24 10:51:54 -0300

    Update counting.js

--- comandos/eventos/messageCreate/counting.js
+++ comandos/eventos/messageCreate/counting.js
@@ -1,11 +1,11 @@
 module.exports = {
     type: "messageCreate",
     code: `
-$onlyIf[$channelID==1478505120525324541;]
-$onlyIf[$isInteger[$message]==true;$deleteCommand]
-$onlyIf[$message==$getGuildVar[countingGame;$guildID;0];$deleteCommand]
-
-$setGuildVar[countingGame;$math[$getGuildVar[countingGame;$guildID;0]+1];$guildID]
+    $c[O jogo de contagem no canal #contando do servidor]
+    $onlyIf[$channelID==1478505120525324541;]
+    $onlyIf[$isInteger[$message]==true;$deleteCommand]
+    $onlyIf[$message==$getGuildVar[countingGame;$guildID;0];$deleteCommand]
+    $setGuildVar[countingGame;$math[$getGuildVar[countingGame;$guildID;0]+1];$guildID]
     
 `
 }
