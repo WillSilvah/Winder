5f1040024ef830d6d3f19d32b47d771eb5bdd325
Williams Felipe <49321188+WillSilvah@users.noreply.github.com>
2026-03-24 10:49:38 -0300

    Update bumpReminder.js

--- comandos/eventos/messageCreate/bumpReminder.js
+++ comandos/eventos/messageCreate/bumpReminder.js
@@ -1,27 +1,26 @@
 module.exports = [{
-type: "messageCreate",
-allowBots: true,
-code: `
-$onlyIf[$authorID==302050872383242240;]
-$onlyIf[$channelID==1462796515578876079;]
-
-$jsonLoad[data;$messageRawData[$channelID;$messageID]]
-
-$onlyIf[$checkContains[$env[data;embeds;0;description];Bump done!]]
-
-$sendMessage[1462796515578876079;<@$env[data;interaction;user]> Obrigado por ajudar!
-Avisarei quando bater $discordTimestamp[$math[$getTimestamp+$parseString[2h]];LongTime]]
-
-$!advancedTimeout[$esc[
-  $sendMessage[{channelID};
-    <@&1462953076091785370>
-    $title[🎉 Hora de dar /bump!]
-    $description[Hey! Tá na hora de dar boost na Disboard!\nUsa o comando </bump:947088344167366698>]
-    $color[Green]
-    $addActionRow
-    $addButton[bumpReminderRole;Receber notificações de bump;Success;🔔;false]
+  type: "messageCreate",
+  allowBots: true,
+  code: `
+  $c[Permite que apenas o bot DISBOARD execute o comando]
+   $onlyIf[$authorID==302050872383242240;]
+  $c[Permite que funcione APENAS no canal #bump da Patinhas] 
+   $onlyIf[$channelID==1462796515578876079;]
+  $c[Carrega os dados de todas as mensagens que o Disboard envia e se conter "bump done" executa o resto do comando]
+   $jsonLoad[data;$messageRawData[$channelID;$messageID]]
+   $onlyIf[$checkContains[$env[data;embeds;0;description];Bump done!]]
+  $sendMessage[1462796515578876079;<@$env[data;interaction;user]> Obrigado por ajudar!\nAvisarei quando bater $discordTimestamp[$math[$getTimestamp+$parseString[2h]];LongTime]]
+  
+ $c[Seta um tempo de duas horas para o lembrete de bump funcionar e adiciona um botão para o usuário ganhar um cargo de lembrete]
+  $!advancedTimeout[$esc[
+   $sendMessage[{channelID};
+    $addContainer[
+     $addTextDisplay[<@&1462953076091785370>\n### TÁ NA HORA DE BUMPAR!\nUse o comando </bump:947088344167366698> do Disboard!]
+     $addActionRow
+     $addButton[bumpReminderRole;Receber notificações de bump;Success;🔔;false]
+    ;#e6adad]
   ]
-];2h;BumpReminder;{"channelID": "$channelID"}]
+ ];2h;BumpReminder;{"channelID": "$channelID"}]
 
 `
 },{
