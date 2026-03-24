dd5754434c52dfe371a91900514e4fb9329d1b71
WillSilvah <williamfelipe235@gmail.com>
2026-03-24 20:22:02 -0300

    Update 1 file: resetMsgsPerMinute.js

--- comandos/eventos/clientReady/resetMsgsPerMinute.js
+++ comandos/eventos/clientReady/resetMsgsPerMinute.js
@@ -2,7 +2,7 @@ module.exports = {
  type: 'clientReady',
  code: `
 $setInterval[
-$setGuildVar[oldMsgPerMinute;$getGuildVar[msgPerMinute;$getGlobalVar[guildID]];$getGlobalVar[guildID]]
+$setGuildVar[oldMsgPerMinute;$default[$getGuildVar[msgPerMinute;$getGlobalVar[guildID]];0];$getGlobalVar[guildID]]
 $wait[5s]
 $deleteRecords[msgPerMinute;;guild];1m;resetMsgsPerMinute]
 
