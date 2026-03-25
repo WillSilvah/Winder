0880956a0e9719be7c5d3950e267612cde564231
WillSilvah <williamfelipe235@gmail.com>
2026-03-25 15:44:18 -0300

    Update 1 file: hasSpecialChars.js

--- comandos/eventos/experiments/hasSpecialChars.js
+++ comandos/eventos/experiments/hasSpecialChars.js
@@ -7,4 +7,21 @@ module.exports = [{
      $!memberSetNickname[$guildID;$authorID;$usertag]
     ]
 `
-}]
+},{
+    type: "guildMemberUpdate",
+    code: `
+    $arrayLoad[alphabet;;abcdefghijklmnopqrstuvwxyz]
+    $let[hasNormalLetters;$arraySome[alphabet;letter;$includes[$userDisplayName / $nickname;$env[letter]]]]
+    $if[$get[hasNormalLetters]==false;
+     $!memberSetNickname[$guildID;$authorID;$usertag]
+    ]
+`
+},{
+    type: "guildMemberAdd",
+    code: `
+    $arrayLoad[alphabet;;abcdefghijklmnopqrstuvwxyz]
+    $let[hasNormalLetters;$arraySome[alphabet;letter;$includes[$userDisplayName / $nickname;$env[letter]]]]
+    $if[$get[hasNormalLetters]==false;
+     $!memberSetNickname[$guildID;$authorID;$usertag]
+    ]
+`}]
