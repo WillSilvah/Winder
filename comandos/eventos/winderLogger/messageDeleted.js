e23610e8d1187bd0182f756142b75e5a86557d10
WillSilvah <williamfelipe235@gmail.com>
2026-03-25 14:27:43 -0300

    Update 1 file: messageDeleted.js

--- comandos/eventos/winderLogger/messageDeleted.js
+++ comandos/eventos/winderLogger/messageDeleted.js
@@ -1,10 +1,10 @@
 module.exports = [{
     type: "messageDelete",
     code: `
-    $sendMessage[$getGuildVar[eventLogChannel];
+    $webhookSend[$getGuildVar[webhookEventLogger];
      $author[$userDisplayName (@$userTag): Mensagem deletada;$userAvatar]
      $description[$codeBlock[$message]\nCanal: <#$channelID>]
      $color[Orange]
-    ]
+    ;false;$username[$clientID];$userAvatar[$clientID]]
 `
 }]    
