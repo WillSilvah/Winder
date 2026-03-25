3aec85e691891a308be35c869cc2efa5c94a2c55
WillSilvah <williamfelipe235@gmail.com>
2026-03-25 15:29:19 -0300

    Update 1 file: hasSpecialChars.js

--- comandos/eventos/experiments/hasSpecialChars.js
+++ comandos/eventos/experiments/hasSpecialChars.js
@@ -4,7 +4,7 @@ module.exports = [{
     $arrayLoad[alphabet;;abcdefghijklmnopqrstuvwxyz]
     $let[hasNormalLetters;$arraySome[alphabet;letter;$includes[$userDisplayName / $nickname;$env[letter]]]]
     $if[$get[hasNormalLetters]==false;
-     $memberSetNickname[$guildID;$authorID;$usertag]
+     $!memberSetNickname[$guildID;$authorID;$usertag]
     ]
 `
 }]
