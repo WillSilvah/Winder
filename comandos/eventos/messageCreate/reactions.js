9b73857693c805b07242659bbee1b1b4a6a9420c
WillSilvah <williamfelipe235@gmail.com>
2026-03-25 23:45:34 -0300

    Update 1 file: reactions.js

--- comandos/eventos/messageCreate/reactions.js
+++ comandos/eventos/messageCreate/reactions.js
@@ -3,11 +3,11 @@ module.exports = [{
   code: `
   $onlyIf[$mentioned[0;true]==$authorID;]
   $if[$includes[$toLowerCase[$message];kkkk]==true;
-   
+   $cooldown[$authorID;10s]
    $!addMessageReactions[$channelID;$messageID;$randomText[😂;🤣]]
   ]
   $if[$includes[$toLowerCase[$message];morte;caveira;morri;morre;💀;☠]==true;
-   
+   $cooldown[$authorID;10s]
    $!addMessageReactions[$channelID;$messageID;💀]
   ]
   
