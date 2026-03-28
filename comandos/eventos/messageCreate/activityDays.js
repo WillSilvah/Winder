a16aa77c9c2a5fe3f75616363913e03ee13d612b
WillSilvah <williamfelipe235@gmail.com>
2026-03-28 18:02:34 -0300

    Update 1 file: activityDays.js

--- comandos/eventos/messageCreate/activityDays.js
+++ comandos/eventos/messageCreate/activityDays.js
@@ -1,7 +1,7 @@
 module.exports = [{
     type: "messageCreate",
     code: `$timezone[America/Recife] $c[Fuso horário de Pernambuco, padrão em todo o sistema] 
-    $onlyIf[$getMemberVar[activityLastDay;$authorID;$guildID;00]!=$day[TwoDigit]/$month[TwoDigit]]
+    $onlyIf[$getMemberVar[activityLastDay;$authorID;$guildID]!=$day[TwoDigit]/$month[TwoDigit]]
     
     $setMemberVar[activityLastDay;$day[TwoDigit]/$month[TwoDigit]]
     $let[activityMonth;$getMemberVar[activityMonth;$authorID;$guildID;0]]
