74feb853af11a968a51deb7811e5cf1ca312f66f
WillSilvah <williamfelipe235@gmail.com>
2026-03-30 06:03:52 -0300

    Update 1 file: verificado.js

--- comandos/eventos/messageCreate/autoreply/verificado.js
+++ comandos/eventos/messageCreate/autoreply/verificado.js
@@ -7,10 +7,8 @@ module.exports = [{
     $startTyping[$channelID]
     $wait[1s]
     $if[$hasRoles[$guildID;$authorID;$getGuildVar[memberVerifiedRole]]==false;
-    $sendMessage[$channelID;Para conseguir a permissão de enviar mídia nos canais de texto, você precisa ter o cargo <@&$getGuildVar[memberVerifiedRole]>. Você ganha com **200 mensagens** (Falta **$math[200-$getMemberVar[messageTotal]]** mensagens), lembrarei quando você ganhar o cargo.];
-    $sendMessage[$channelID;Mas você **já pode** enviar mídias nos canais.]
-    $sendMessage[$channelID;$sticker[1483454059447976108]]
-]
-]    
+     $sendMessage[$channelID;Para conseguir a permissão de enviar mídia nos canais de texto, você precisa ter o cargo <@&$getGuildVar[memberVerifiedRole]>. Você ganha com **200 mensagens** (Falta **$math[200-$getMemberVar[messageTotal]]** mensagens), lembrarei quando você ganhar o cargo.];
+    ]
+    ]    
 `
 }]
