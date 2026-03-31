ab2e7fc3d3c6542df47db7378c33e5ac76dd1ef3
WillSilvah <williamfelipe235@gmail.com>
2026-03-31 08:06:19 -0300

    Update 1 file: daily.js

--- comandos/slashCommands/paws/daily.js
+++ comandos/slashCommands/paws/daily.js
@@ -1,11 +1,20 @@
 module.exports = {
-  code: `
-
-  `,
-data: {
+    code: `
+    $timezone[America/Recife] $timezone[America/Recife] $c[Fuso horário de Pernambuco, padrão em todo o sistema]
+    $onlyIf[$getMemberVar[dailyLast;$authorID;$guildID;00/00]!=$day[TwoDigit]/$month[TwoDigit];Você já pegou o prêmio diário hoje!]
+    $setMemberVar[dailyLast;$day[TwoDigit]/$month[TwoDigit]]
+    $let[money;$getMemberVar[money;$authorID;$guildID;0]]
+    $let[daily;$randomNumber[50;100]]
+    
+    $letSum[money;$get[daily]]
+    $setMemberVar[money;$get[money]]
+    
+    $interactionReply[Parabéns, vocêganhou **$get[daily]** furrásticos!]
+`,
+  data: {
   "type": 1,
-  "description": "Trabalhe em algum emprego e ganhe Paws!",
-  "name": "trabalhar",
+  "description": "Ganhe um bônus!",
+  "name": "diário",
   "name_localizations": {
     "en-GB": "work",
     "en-US": "work",
