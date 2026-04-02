6448d42e3dd1851f1abdcdc2ebd0884822d540c6
WillSilvah <williamfelipe235@gmail.com>
2026-04-02 18:16:27 -0300

    Update 1 file: perfil.js

--- comandos/slashCommands/perfil.js
+++ comandos/slashCommands/perfil.js
@@ -26,8 +26,7 @@ $addTextDisplay[$if[$getMemberVar[memberJoinedHere?;$get[userID];$guildID;false]
 ]
 $addTextDisplay[👤 Criou conta $discordTimestamp[$userCreatedAt[$get[userID]];RelativeTime]]
 $if[$getMemberVar[warnsTotal;$get[userID]]>0;$addTextDisplay[⚠ Tem **$getMemberVar[warnsTotal;$get[userID]]** advertências!]]
-$if[$isTimedOut[$guildID;$get[userID]]==true;$addTextDisplay[🤫 Silenciado até $discordTimestamp[$memberTimeoutDuration[$guildID;$get[userID]];FullDateShortTime]]]
-$addSeparator[Small;true]
+$if[$memberExists[$guildID;$get[userID]]==true;$if[$isTimedOut[$guildID;$get[userID]]==true;$addTextDisplay[🤫 Silenciado até $discordTimestamp[$memberTimeoutDuration[$guildID;$get[userID]];FullDateShortTime]]]]$addSeparator[Small;true]
 $if[$isBanned[$guildID;$get[userID]]==false;
 $addTextDisplay[
 ### $toUpperCase[TEMPORADA $env[session;number]: $env[session;name]]
