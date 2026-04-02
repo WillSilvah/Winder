3ff4bc1c15254d616982008f09ef30568a2d692f
Williams Felipe <49321188+WillSilvah@users.noreply.github.com>
2026-04-02 11:43:54 -0300

    Update avisos.js

--- comandos/slashCommands/mod/avisos.js
+++ comandos/slashCommands/mod/avisos.js
@@ -12,13 +12,12 @@ module.exports = {
       ]
       
       $arrayForEach[warnKeys;warn;
-        $addSeparator[Large]
-
-        $addTextDisplay[
+        $addSeparator[Small]
+        $addTextDisplay[$trim[
           **Aviso $inlineCode[#$env[warn]]: **$discordTimestamp[$env[warns;$env[warn];timestamp];FullDateShortTime]
           Aplicador: **@$userTag[$env[warns;$env[warn];staffID]]**
           Motivo: $codeBlock[$env[warns;$env[warn];reason]]
-        ]
+        ]]
       ]
     ;$guildColor[orange]]
   `,
