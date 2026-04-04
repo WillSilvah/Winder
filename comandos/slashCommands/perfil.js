52cb11741a91fd6f207bca28bfecf669448a6dbd
WillSilvah <williamfelipe235@gmail.com>
2026-04-04 11:32:51 -0300

    Update 1 file: perfil.js

--- comandos/slashCommands/perfil.js
+++ comandos/slashCommands/perfil.js
@@ -40,9 +40,11 @@ $addSeparator[Small;true]
 $addTextDisplay[
 ### MISSÕES
 $if[$hasRoles[$guildID;$get[userID];$getGuildVar[memberVerifiedRole]]==false;❌️ falta **$math[200-$getMemberVar[messageTotal;$get[userID]]] ($truncate[$math[$getMemberVar[messageTotal;$get[userID]]/200*100]]%)** mensagens para ganhar verificado.;✅️ É um peludo verificado]
+$if[$memberExists[$guildID;$get[userID];
 $if[$hasRoles[$guildID;$get[userID];1481734062270844968]==false;❌️ Não é um assombrado\n-# Consiga essa proeza conversando em certos horários na comunidade.;👻 É um peludo assombrado]
 $if[$getMemberVar[talkingMonth;$get[userID]]>=7;🗣 Conversou por mais de sete dias na comunidade neste mês!;❌️ Não conversou por sete dias na comunidade neste mês.]
 $if[$getMemberVar[talkingTotal;$get[userID]]>=30;🗣 É um fofoqueiro!\nConversou por mais de 30 dias sem parar!]
+]
 ]
 $addSeparator[Small;true]
 $addTextDisplay[### ANTERIORMENTE
