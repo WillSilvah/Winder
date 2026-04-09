fa7da89a34a7a15baf2d5335e36be72f213ee77c
WillSilvah <williamfelipe235@gmail.com>
2026-04-09 12:05:30 -0300

    Update 1 file: birthday.js

--- comandos/eventos/clientReady/birthday.js
+++ comandos/eventos/clientReady/birthday.js
@@ -8,7 +8,7 @@ module.exports = [{
      $arrayForEach[birthday;id;
      $if[$getMemberVar[birthday;$env[id];$get[guildID]]==$day[TwoDigit]/$month[TwoDigit];
      
-     $!memberAddRoles[$guildID;$env[id];$getGuildVar[birthdayRole;$get[guildID]]]
+     $!memberAddRoles[$get[guildID];$env[id];$getGuildVar[birthdayRole;$get[guildID]]]
      
      $sendMessage[$getGuildVar[batePapo;$get[guildID]];
      $addSection[
@@ -36,7 +36,7 @@ module.exports = [{
      ]
     ]
 
-;54 11 * * *;America/Recife]
+;56 12 * * *;America/Recife]
     
     
 `
