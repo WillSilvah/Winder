0393c1d87f748a4a212e4c9deaf1797925374dd5
WillSilvah <williamfelipe235@gmail.com>
2026-04-11 18:04:35 -0300

    Update 1 file: anunciar-chamadas.js

--- comandos/prefixCommands/textos/anunciar-chamadas.js
+++ comandos/prefixCommands/textos/anunciar-chamadas.js
@@ -14,7 +14,7 @@ $addTextDisplay[# MENSAGEM IMPORTANTE PARA VOCÊ!
 -# Você recebeu isso pois tem o cargo "💬 - Chamar pra conversar" na Patinhas
 
 Olá <@$env[id]> tudo bem? Sabemos que no Discord menções desnecessárias podem ser um incômodo e afastar membros das comunidades, então na Patinhas nós temos o cuidado de não ser muito chato nessa questão deixando que o membro escolha o que deseja receber facilmente no canal https://discord.com/channels/1462224054676099094/1470875996356874292 com avisos sobre cada um.
-Temos 77 membros que pegaram o cargo que você tem e apenas poucos deles aparecem ao ser mencionado o que podemos suspeitar que tem acumulo de menções já que muitos membros pegaram o cargo logo no começo ao entrar no servidor sem ter uma noção da atividade.
+Temos $arrayLength[roleMembers] membros que pegaram o cargo que você tem e apenas poucos deles aparecem ao ser mencionado o que podemos suspeitar que tem acumulo de menções já que muitos membros pegaram o cargo logo no começo ao entrar no servidor sem ter uma noção da atividade.
 ## NOVAS REGRAS AOS CARGOS DE CHAMADAS
 - Os cargos são públicos, isso significa que qualquer membro pode marcar eles para fazer alguma coisa e claro, tem regra de anti spam.
 - Os cargos de chamadas só podem ser mencionados durante o horário comercial da Patinhas, das 06:30 até 22:00.
