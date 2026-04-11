059cedf479f2b4da718d21a2aa54c2d1e543d569
WillSilvah <williamfelipe235@gmail.com>
2026-04-10 22:38:16 -0300

    Update 2 files: logoUpdate.js, moments.js

--- comandos/eventos/clientReady/moments.js
+++ comandos/eventos/clientReady/moments.js
@@ -123,7 +123,7 @@ $cron[$sendMessage[$getGuildVar[batePapo;$get[guildID]];
 ]
 $setGlobalVar[sleepMode;true]
 
-;0 22 * * *;America/Recife]
+;0 23 * * *;America/Recife]
 `
 },{
     type: "clientReady",
