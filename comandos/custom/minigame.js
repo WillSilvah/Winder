e8c0993297f64151218cd8df9c8279587339587d
WillSilvah <williamfelipe235@gmail.com>
2026-04-12 15:47:06 -0300

    Update 1 file: minigame.js

--- comandos/custom/minigame.js
+++ comandos/custom/minigame.js
@@ -62,7 +62,7 @@ module.exports = [{
     ] 
     $setGuildVar[minigameExecuteCount;$math[$getGuildVar[minigameExecuteCount;$env[guildID];0]+1];$env[guildID]]
     $setGuildVar[minigameTimestamp;$getTimestamp;$env[guildID]]
-     $wait[1m]
+     $wait[35s]
       $onlyIf[$getGuildVar[minigameStatus;$env[guildID]]==true;]
       $setGuildVar[minigameStatus;false;$env[guildID]]
       $setGuildVar[minigameWord;;$env[guildID]]
