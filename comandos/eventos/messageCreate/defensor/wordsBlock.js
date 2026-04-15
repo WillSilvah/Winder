d30455e469b7e62d135753a880b75bd1320c7c60
WillSilvah <williamfelipe235@gmail.com>
2026-04-15 15:29:55 -0300

    Update 1 file: wordsBlock.js

--- comandos/eventos/messageCreate/defensor/wordsBlock.js
+++ comandos/eventos/messageCreate/defensor/wordsBlock.js
@@ -1,6 +1,16 @@
 module.exports = [{
     type: "messageCreate",
-    code: `$stop
+    code: `
+    $if[$getMemberVar[messageTotal;$authorID;$guildID;0]<100;
+     $if[$includes[$toLowercase[$message];卐;卍;࿕;࿖;࿗;࿘;ꖦ;ᛋ;ϟ;ᛋᛋ;clatebit;trading;zeta;ζ;phi;φ;$USD;sieg heil;cavetodex]==true;
+      $let[userID;$authorID]
+     $let[reason;Comportamento indevido e ofensivo.]
+     $let[staffID;$clientID]
+     $let[type;banido]
     
+     $sendMessage[$getGuildVar[batePapo];w!ban <@$authorID> $get[reason]] $c[Enfeite, para dizer que foi o próprio bot que executou a função]
+     $winderBan
+     ]
+    ]
 `
 }]
