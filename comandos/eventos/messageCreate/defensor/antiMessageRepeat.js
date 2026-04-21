b78ae46527f6254d3aab284caf42ae787b450786
WillSilvah <williamfelipe235@gmail.com>
2026-04-21 09:34:06 -0300

    Update 1 file: antiMessageRepeat.js

--- comandos/eventos/messageCreate/defensor/antiMessageRepeat.js
+++ comandos/eventos/messageCreate/defensor/antiMessageRepeat.js
@@ -4,7 +4,7 @@ module.exports = [{
     $if[$toLowercase[$message]==$toLowercase[$getMemberVar[defensorLastMessage;$authorID;$guildID;vazio]];
      $deleteCommand
      $let[v;$getMemberVar[defensorInfractions;$authorID;$guildID;0]]
-     $letSum[v:1]
+     $letSum[v;1]
      $setMemberVar[defensorInfractions;$get[v];$authorID]
      
      $if[$getMemberVar[defensorInfractions;$authorID;$guildID;0]>3;
