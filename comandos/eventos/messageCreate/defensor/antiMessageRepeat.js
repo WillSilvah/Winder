b0a81118ca015965b6eec7e8d4f5bb224e261338
WillSilvah <williamfelipe235@gmail.com>
2026-04-21 17:39:21 -0300

    Update 1 file: antiMessageRepeat.js

--- comandos/eventos/messageCreate/defensor/antiMessageRepeat.js
+++ comandos/eventos/messageCreate/defensor/antiMessageRepeat.js
@@ -9,25 +9,7 @@ module.exports = [{
      $setMemberVar[defensorInfractions;$get[v];$authorID]
      
      $if[$getMemberVar[defensorInfractions;$authorID;$guildID;0]>3;
-      $if[$getMemberVar[messageTotal;$authorID;$guildID;0]>=$getGuildVar[memberVerifiedQuantity;$guildID;0];
-       $if[$userJoinedAt[$guildID;$authorID]>$math[$getTimestamp-$parseString[15d]];
-       $let[userID;$authorID]
-       $let[reason;Spam/Flood]
-       $let[staffID;$authorID]
-       $let[type;silenciado]
-       $let[time;30m]
-       $winderMute
-       $sendMessage[$channelID;w!silenciar <@$authorID> $get[reason]]
-       $sendMessage[$channelID;<@$clientID> **$username[$get[userID]]** foi **$get[type]**!\n> $bold[$get[reason]]];
-       $let[userID;$authorID]
-       $let[reason;Spam/Flood]
-       $let[staffID;$authorID]
-       $let[type;advertido]
-       $winderWarn
-       $autopunish
-       $sendMessage[$channelID;w!advertir <@$authorID> $get[reason]]
-       $sendMessage[$channelID;<@$clientID> **$username[$get[userID]]** foi **$get[type]**!\n> $bold[$get[reason]]]
-      ]
+      
      ]
      
     $setMemberVar[defensorLastMessage;$message;$authorID]
