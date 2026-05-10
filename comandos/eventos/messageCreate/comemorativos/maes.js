2b5dac5af8514c01bcff996e4f98c5d150ff6f75
WillSilvah <williamfelipe235@gmail.com>
2026-05-10 10:06:19 -0300

    Update 1 file: maes.js

---
+++ comandos/eventos/messageCreate/comemorativos/maes.js
@@ -0,0 +1,46 @@
+module.exports = [{
+    type: "messageCreate",
+    code: `
+    $onlyIf[$getGlobalVar[sleepMode]==false;]
+    $onlyIf[$getGuildVar[evento_diaDasMaesStatus;$guildID;false]==true]
+    $let[roleID;1503017802351513771]
+    $timezone[America/Recife]
+    $onlyIf[$day/$month[TwoDigit]==10/05]
+    $onlyIf[$hasRoles[$guildID;$authorID;$getGuildVar[memberVerifiedRole]]==true]
+    $onlyIf[$hasRoles[$guildID;$authorID;$get[roleID]]==false]
+    $let[chance;$randomNumber[1;100]]
+    $onlyIf[$includes[$toLowercase[$message];feliz]|$includes[$toLowercase[$message];dia]|$includes[$toLowercase[$message];das]|$includes[$toLowercase[$message];mães;mãe;mae;mainha]==true|true|true|true]
+
+    $!addMessageReactions[$channelID;$messageID;❤️]
+
+    $!memberAddRoles[$guildID;$authorID;$get[roleID]]
+`
+},{
+    type: "clientReady",
+    code: `
+    $let[roleID;1503017802351513771]
+    $cron[
+    $timezone[America/Recife]
+    $let[roleID;1503017802351513771]
+    $onlyIf[$day[TwoDigit]/$month[TwoDigit]==22/03;]
+    $setGuildVar[evento_diaDasMaesStatus;false;1462224054676099094]
+    $arrayLoad[roleMembers;,;$roleMembers[1462224054676099094;$get[roleID];,]]
+    $arrayMap[roleMembers;id;
+    $return[<@$env[id]>]
+    ;roleUsernames]
+
+    $sendMessage[$getGuildVar[anunciosChat;1462224054676099094];
+     $addContainer[
+      $addSection[
+       $addTextDisplay[### ❤️ FELIZ DIA DAS MÃES\nTodos os membros que deram feliz dia das mães hoje:]
+       $addTextDisplay[$arrayJoin[roleUsernames;\n]]
+       $addTextDisplay[\n Próximo evento: Domingo de maio de 2027]
+      ]
+     ;#005DBA]
+    ]
+    $setGuildVar[evento_diaDasMaes;false;1462224054676099094]
+    $!setGuildIcon[1462224054676099094;$getGuildVar[logo_sleep;1462224054676099094];Hora de dormir]
+    $setGuildVar[guildColor;#ddf3fe;1462224054676099094]
+    $setGuildVar[guildIconDefault;true;1462224054676099094]
+    ;7 22 22 3 *;America/Recife;diaDasMaes]
+`}]
