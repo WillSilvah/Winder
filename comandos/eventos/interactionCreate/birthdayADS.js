f75240614cf5653978e9c4873af98b17d9889579
WillSilvah <williamfelipe235@gmail.com>
2026-05-11 20:30:10 -0300

    Update 1 file: birthdayADS.js

--- comandos/eventos/interactionCreate/birthdayADS.js
+++ comandos/eventos/interactionCreate/birthdayADS.js
@@ -1,7 +1,6 @@
 module.exports = [{
     type: "interactionCreate",
     code: `$ephemeral
-    $wait[2s]
     $onlyIf[$getMemberVar[birthdayYear]==;]
     $interactionReply[Opa mano! Percebi que você não registrou o seu aniversário, como que vamos te parabenizar? Use o comando **/aniversário adicionar** ai pow!\nO aniversário também serve para a demografia, saber qual a média de idade dos membros, eles aparecem no **/perfil**]
 `
