46730383d3db4218d5abc6a6663ba153fd6f7823
WillSilvah <williamfelipe235@gmail.com>
2026-05-12 05:37:28 -0300

    Update 1 file: resetMsgsPerMinute.js

--- comandos/eventos/clientReady/resetMsgsPerMinute.js
+++ comandos/eventos/clientReady/resetMsgsPerMinute.js
@@ -7,7 +7,7 @@ module.exports = {
     $setInterval[
      $setGuildVar[oldMsgPerMinute;$default[$getGuildVar[msgPerMinute;$get[guildID]];0];$get[guildID]]
      
-     $setGuildVar[msgPerMinuteMonth;$hour[TwoDigit]:$minute[TwoDigit]:$second[TwoDigit] - $day[TwoDigit]/$month[TwoDigit]/$year[TwoDigit]: $default[$getGuildVar[msgPerMinute;$get[guildID]];0] mensagens por minuto e $getMemberLeaderboardLength[memberIsTalking?] membros estão conversando na hora | $math[$guildMemberCount[$get[guildID];online;false]+$guildMemberCount[$get[guildID];dnd;false]+$guildMemberCount[$get[guildID];idle;false]] usuários estão online no Discord \n$getGuildVar[msgPerMinuteMonth;$get[guildID]];$get[guildID]]
+     $setGuildVar[msgPerMinuteMonth;$hour[TwoDigit]:$minute[TwoDigit]:$second[TwoDigit] - $day[TwoDigit]/$month[TwoDigit]/$year[TwoDigit]: $default[$getGuildVar[msgPerMinute;$get[guildID]];0] mensagens por minuto e $getMemberLeaderboardLength[memberIsTalking?;$get[guildID]] membros estão conversando na hora | $math[$guildMemberCount[$get[guildID];online;false]+$guildMemberCount[$get[guildID];dnd;false]+$guildMemberCount[$get[guildID];idle;false]] usuários estão online no Discord \n$getGuildVar[msgPerMinuteMonth;$get[guildID]];$get[guildID]]
      
     $wait[5s]
     $deleteRecords[msgPerMinute]
