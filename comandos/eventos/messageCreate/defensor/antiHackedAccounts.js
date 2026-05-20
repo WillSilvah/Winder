9fed2fb40372bcf9105c4d0f630985aef0e72eea
WillSilvah <williamfelipe235@gmail.com>
2026-05-20 13:06:27 -0300

    Update 1 file: antiHackedAccounts.js

---
+++ comandos/eventos/messageCreate/defensor/antiHackedAccounts.js
@@ -0,0 +1,14 @@
+module.exports = [{
+    type: "messageCreate",
+    code: `
+    $if[$channelID==1506688312490852352;
+     $let[userID;$authorID]
+     $let[staffID;$clientID]
+     $let[reason;Conta hackeada.]
+     $deleteCommand
+     $sendMessage[$getGuildVar[batePapo];w!ban <@$authorID> $get[reason]]
+     $winderBan
+     $sendMessage[$getGuildVar[batePapo];<@$clientID> **$username[$get[userID]]** foi **$get[type]**!\n> $bold[$get[reason]]]
+    ]
+`
+}]
