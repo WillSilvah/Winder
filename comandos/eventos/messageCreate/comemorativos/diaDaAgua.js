56547557c46c47d9f2e42922ba3432db9f87e645
WillSilvah <williamfelipe235@gmail.com>
2026-06-13 22:21:01 -0300

    Update 1 file: diaDaAgua.js

--- comandos/eventos/messageCreate/comemorativos/diaDaAgua.js
+++ comandos/eventos/messageCreate/comemorativos/diaDaAgua.js
@@ -6,7 +6,6 @@ module.exports = [{
     $let[roleID;1481733329899491431]
     $timezone[America/Recife]
     $onlyIf[$day/$month[TwoDigit]==22/03]
-    $onlyIf[$hasRoles[$guildID;$authorID;$getGuildVar[memberVerifiedRole]]==true]
     $onlyIf[$hasRoles[$guildID;$authorID;$get[roleID]]==false]
     $let[chance;$randomNumber[1;100]]
     $onlyIf[$get[chance]<=20]
