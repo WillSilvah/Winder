568d24ddcd90a5e18c0de0ab7162220cba4619fa
WillSilvah <williamfelipe235@gmail.com>
2026-06-13 21:18:13 -0300

    Update 1 file: copa2026.js

--- comandos/eventos/messageCreate/comemorativos/copa2026.js
+++ comandos/eventos/messageCreate/comemorativos/copa2026.js
@@ -2,15 +2,12 @@ module.exports = [{
     type: "messageCreate",
     code: `
     $onlyIf[$getGlobalVar[sleepMode]==false;]
-    $onlyIf[$getGuildVar[evento_cobaBRAStatus;$guildID;false]==true]
     $let[roleID;1515463857768890518]
     $timezone[America/Recife]
     $onlyIf[$day/$month[TwoDigit]==13/06]
     $onlyIf[$hour[TwoDigit]>=19;]
     $onlyIf[$hasRoles[$guildID;$authorID;$getGuildVar[memberVerifiedRole]]==true]
     $onlyIf[$hasRoles[$guildID;$authorID;$get[roleID]]==false]
-    $let[chance;$randomNumber[1;100]]
-    $onlyIf[$get[chance]<=25]
 
     $!addMessageReactions[$channelID;$messageID;⚽️]
 
