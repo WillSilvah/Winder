41b660f6269a3899ebce3a0c4c575aefe2fda685
WillSilvah <williamfelipe235@gmail.com>
2026-06-29 15:34:02 -0300

    Update 1 file: copa2026.js

--- comandos/eventos/messageCreate/comemorativos/copa2026.js
+++ comandos/eventos/messageCreate/comemorativos/copa2026.js
@@ -4,8 +4,8 @@ module.exports = [{
     $onlyIf[$getGlobalVar[sleepMode]==false;]
     $let[roleID;1515463857768890518]
     $timezone[America/Recife]
-    $onlyIf[$day/$month[TwoDigit]==13/06]
-    $onlyIf[$hour[TwoDigit]>=19;]
+    $onlyIf[$day/$month[TwoDigit]==29/06]
+    $onlyIf[$hour[TwoDigit]>=14;]
     $onlyIf[$hasRoles[$guildID;$authorID;$getGuildVar[memberVerifiedRole]]==true]
     $onlyIf[$hasRoles[$guildID;$authorID;$get[roleID]]==false]
 
